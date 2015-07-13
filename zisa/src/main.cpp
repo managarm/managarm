a1478224fa5be22dd1ca42d55f2569fb058272be
avdgrinten <alexander.vandergrinten@gmail.com>
2015-07-13 21:31:39 +0200

    Implement basic ATA driver

--- zisa/src/main.cpp
+++ zisa/src/main.cpp
@@ -8,16 +8,45 @@
 
 uint8_t ioInByte(uint16_t port) {
 	register uint16_t in_port asm("dx") = port;
-	register uint16_t out_value asm("al");
+	register uint8_t out_value asm("al");
 	asm volatile ( "inb %%dx, %%al" : "=r" (out_value) : "r" (in_port) );
+	return out_value;
+}
+uint16_t ioInShort(uint16_t port) {
+	register uint16_t in_port asm("dx") = port;
+	register uint16_t out_value asm("ax");
+	asm volatile ( "inw %%dx, %%ax" : "=r" (out_value) : "r" (in_port) );
 	return out_value;
 }
 
 void ioOutByte(uint16_t port, uint8_t value) {
 	register uint16_t in_port asm("dx") = port;
-	register uint16_t in_value asm("al") = value;
+	register uint8_t in_value asm("al") = value;
 	asm volatile ( "outb %%al, %%dx" : : "r" (in_port), "r" (in_value) );
 }
+
+enum AtaPorts {
+	kPortReadData = 0,
+	kPortWriteSectorCount = 2,
+	kPortWriteLba1 = 3,
+	kPortWriteLba2 = 4,
+	kPortWriteLba3 = 5,
+	kPortWriteDevice = 6,
+	kPortWriteCommand = 7,
+	kPortReadStatus = 7,
+};
+
+enum AtaCommands {
+	kCommandReadSectorsExt = 0x24
+};
+
+enum AtaFlags {
+	kStatusDrq = 0x08,
+	kStatusBsy = 0x80,
+
+	kDeviceSlave = 0x10,
+	kDeviceLba = 0x40
+};
 
 int main() {
 	uintptr_t ports[] = { 0x1F0, 0x1F1, 0x1F2, 0x1F3, 0x1F4, 0x1F5, 0x1F6, 0x1F7, 0x3F6 };
@@ -26,8 +55,41 @@ int main() {
 	helAccessIo(ports, 9, &io_space);
 	helEnableIo(io_space);
 
-	uint8_t status = ioInByte(0x3F6);
+	uint16_t port_base = 0x1F0;
 	
-	printf("Status port: %x\n", status);
+	uint16_t count = 1;
+	uint64_t lba = 0;
+
+	ioOutByte(port_base + kPortWriteDevice, kDeviceLba);
+	
+	ioOutByte(port_base + kPortWriteSectorCount, (count >> 8) & 0xFF);
+	ioOutByte(port_base + kPortWriteLba1, (lba >> 24) & 0xFF);
+	ioOutByte(port_base + kPortWriteLba2, (lba >> 32) & 0xFF);
+	ioOutByte(port_base + kPortWriteLba3, (lba >> 40) & 0xFF);	
+	
+	ioOutByte(port_base + kPortWriteSectorCount, count & 0xFF);
+	ioOutByte(port_base + kPortWriteLba1, lba & 0xFF);
+	ioOutByte(port_base + kPortWriteLba2, (lba >> 8) & 0xFF);
+	ioOutByte(port_base + kPortWriteLba3, (lba >> 16) & 0xFF);
+	
+	ioOutByte(port_base + kPortWriteCommand, kCommandReadSectorsExt);
+
+	while(true) {
+		uint8_t status = ioInByte(port_base + kPortReadStatus);
+		if((status & kStatusBsy) != 0)
+			continue;
+		if((status & kStatusDrq) == 0)
+			continue;
+		break;
+	}
+
+	uint8_t buffer[512];
+	for(int i = 0; i < 256; i++) {
+		uint16_t word = ioInShort(port_base + kPortReadData);
+		buffer[2 * i] = word & 0xFF;
+		buffer[2 * i + 1] = (word >> 16) & 0xFF;
+	}
+
+	printf("%x\n", buffer[0]);
 }
 
