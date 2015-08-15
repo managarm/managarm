19cef5440c08987f49c938f67739838e8fee43e5
Andreas Hampicke <ahampicke@gmail.com>
2015-08-15 17:40:43 +0200

    Basic ext2 filesystem implementation

--- zisa/src/main.cpp
+++ zisa/src/main.cpp
@@ -334,21 +334,288 @@ void GptParser::Partition::readSectors(int64_t sector, uint8_t *buffer,
 }
 
 GptParser parser;
-uint8_t sectorbuffer[512]; 
 
-void onSectorRead(void *object) {
-	printf("inhalt: %x \n", sectorbuffer[56]);
+
+// --------------------------------------------------------
+// Superblock testing code
+// --------------------------------------------------------
+
+struct Superblock {
+	uint32_t inodesCount;
+	uint32_t blocksCount;
+	uint32_t rBlocksCount;
+	uint32_t freeBlocksCount;
+	uint32_t freeInodesCount;
+	uint32_t firstDataBlock;
+	uint32_t logBlockSize;
+	uint32_t logFragSize;
+	uint32_t blocksPerGroup;
+	uint32_t fragsPerGroup;
+	uint32_t inodesPerGroup;
+	uint32_t mtime;
+	uint32_t wtime;
+	uint16_t mntCount;
+	uint16_t maxMntCount;
+	uint16_t magic;
+	uint16_t state;
+	uint16_t errors;
+	uint16_t minorRevLevel;
+	uint32_t lastcheck;
+	uint32_t checkinterval;
+	uint32_t creatorOs;
+	uint32_t revLevel;
+	uint16_t defResuid;
+	uint16_t defResgid;
+	//-- EXT2_DYNAMIC_REV Specific --
+	uint32_t firstIno;
+	uint16_t inodeSize;
+	uint16_t blockGroupNr;
+	uint32_t featureCompat;
+	uint32_t featureIncompat;
+	uint32_t featureRoCompat;
+	uint8_t uuid[16];
+	uint8_t volumeName[16];
+	uint8_t lastMounted[64];
+	uint32_t algoBitmap;
+	//-- Performance Hints --
+	uint8_t preallocBlocks;
+	uint8_t preallocDirBlocks;
+	uint16_t alignment;
+	//-- Journaling Support --
+	uint8_t journalUuid[16];
+	uint32_t journalInum;
+	uint32_t journalDev;
+	uint32_t lastOrphan;
+	//-- Directory Indexing Support --
+	uint32_t hashSeed[4];
+	uint8_t defHashVersion;
+	uint8_t padding[3];
+	//-- Other options --
+	uint32_t defaultMountOptions;
+	uint32_t firstMetaBg;
+	uint8_t unused[760];
+};
+static_assert(sizeof(Superblock) == 1024, "Bad Superblock struct size");
+
+struct BlockGroupDescriptor {
+	uint32_t blockBitmap;
+	uint32_t inodeBitmap;
+	uint32_t inodeTable;
+	uint16_t freeBlocksCount;
+	uint16_t freeInodesCount;
+	uint16_t usedDirsCount;
+	uint16_t pad;
+	uint8_t reserved[12];
+};
+static_assert(sizeof(BlockGroupDescriptor) == 32, "Bad BlockGroupDescriptor struct size");
+
+struct BlockBitmap {
+
+};
+
+struct InodeBitmap {
+
+};
+
+struct Inode {
+	uint16_t mode;
+	uint16_t uid;
+	uint32_t size;
+	uint32_t atime;
+	uint32_t ctime;
+	uint32_t mtime;
+	uint32_t dtime;
+	uint16_t gid;
+	uint16_t linksCount;
+	uint32_t blocks;
+	uint32_t flags;
+	uint32_t osdl;
+	uint32_t directBlocks[12];
+	uint32_t indirectBlock;
+	uint32_t indirect2Block;
+	uint32_t indirect3Block;
+	uint32_t generation;
+	uint32_t fileAcl;
+	uint32_t dirAcl;
+	uint32_t faddr;
+	uint8_t osd2[12];
+};
+static_assert(sizeof(Inode) == 128, "Bad Inode struct size");
+
+
+class Ext2Driver {
+friend class GptParser;
+public:
+	void work(helx::Callback<> callback);
+	void onSuperblockRead();
+	void onBlockGroupDescriptorRead();
+	
+	class ReadInodeClosure {
+	public:
+		ReadInodeClosure(Ext2Driver &driver, uint32_t inode_number);
+
+		void read();
+
+	private:
+		void onReadComplete();
+
+		Ext2Driver &driver;
+		uint32_t inodeNumber;
+		uint8_t inodeBuffer[512];
+	};
+
+	class ReadDataClosure { 
+	public:
+		ReadDataClosure(Ext2Driver &driver, Inode inode, void *buffer,
+				uintptr_t first_block, size_t num_blocks);
+		void read();
+	private:
+		void onReadComplete();
+
+		Ext2Driver &driver;
+		Inode inode;
+		void *buffer;
+		uintptr_t firstBlock;
+		size_t numBlocks;
+		size_t blocksRead;
+	};
+private:
+	uint8_t superblockBuffer[1024];
+	uint8_t *blockGroupDescriptorBuffer;
+
+	uint32_t blockSize;
+	uint32_t sectorsPerBlock;
+	uint32_t numBlockGroups;
+
+	helx::Callback<> p_callback;
+};
+
+void Ext2Driver::work(helx::Callback<> callback) {
+	p_callback = callback;
+
+	parser.getPartition(1).readSectors(2, superblockBuffer, 2,
+			HELX_MEMBER(this, &Ext2Driver::onSuperblockRead));
+}
+void Ext2Driver::onSuperblockRead() {
+	Superblock *sb = (Superblock *)superblockBuffer;
+	blockSize = 1024 << sb->logBlockSize;
+	sectorsPerBlock = blockSize / 512;
+	numBlockGroups = sb->blocksCount / sb->blocksPerGroup;
+
+	printf("magic: %x \n", sb->magic);
+	printf("numBlockGroups: %d \n", numBlockGroups);
+	printf("blockSize: %d \n", blockSize);
+
+	size_t bgdt_size = numBlockGroups * sizeof(BlockGroupDescriptor);
+	if((bgdt_size % 512) != 0)
+		bgdt_size += 512 - (bgdt_size % 512);
+	blockGroupDescriptorBuffer = new uint8_t[bgdt_size];
+
+	parser.getPartition(1).readSectors(2 * sectorsPerBlock,
+			blockGroupDescriptorBuffer, bgdt_size / 512,
+			HELX_MEMBER(this, &Ext2Driver::onBlockGroupDescriptorRead));
+}
+void Ext2Driver::onBlockGroupDescriptorRead() {
+	BlockGroupDescriptor *bgdt = (BlockGroupDescriptor *)blockGroupDescriptorBuffer;
+	printf("bgBlockBitmap: %x \n", bgdt->blockBitmap);
+
+	ReadInodeClosure *readInode = new ReadInodeClosure(*this, 2);
+	readInode->read();
+}
+
+Ext2Driver::ReadInodeClosure::ReadInodeClosure(Ext2Driver &driver, uint32_t inode_number)
+: driver(driver), inodeNumber(inode_number) { }
+
+void Ext2Driver::ReadInodeClosure::read() {
+	Superblock *sb = (Superblock *)driver.superblockBuffer;
+	uint32_t block_group = (inodeNumber - 1) / sb->inodesPerGroup;
+	uint32_t index = (inodeNumber - 1) % sb->inodesPerGroup;
+
+	BlockGroupDescriptor *bgdt = (BlockGroupDescriptor *)driver.blockGroupDescriptorBuffer;
+	uint32_t inode_table_block = bgdt[block_group].inodeTable;
+	uint32_t offset = index * sb->inodeSize;
+	
+	uint32_t sector = inode_table_block * driver.sectorsPerBlock + (offset / 512);
+	parser.getPartition(1).readSectors(sector, inodeBuffer, 1,
+			HELX_MEMBER(this, &Ext2Driver::ReadInodeClosure::onReadComplete));
+}
+void Ext2Driver::ReadInodeClosure::onReadComplete() {
+	Superblock *sb = (Superblock *)driver.superblockBuffer;
+	uint32_t index = (inodeNumber - 1) % sb->inodesPerGroup;
+	uint32_t offset = index * sb->inodeSize;
+
+	Inode *inode = (Inode *)(inodeBuffer + (offset % 512));
+	printf("mode: %x , offset: %d , inodesize: %d \n", inode->mode, offset, sb->inodeSize);
+
+	size_t num_blocks = inode->size / driver.blockSize;
+	if((inode->size % driver.blockSize) != 0)
+		num_blocks++;
+
+	void *buffer = malloc(num_blocks * driver.blockSize);
+	ReadDataClosure *readDataClosure = new ReadDataClosure(driver, *inode, buffer, 0, num_blocks);
+	readDataClosure->read();
+}
+
+Ext2Driver::ReadDataClosure::ReadDataClosure(Ext2Driver &driver, Inode inode, void *buffer,
+		uintptr_t first_block, size_t num_blocks)
+: driver(driver), inode(inode), buffer(buffer), firstBlock(first_block), numBlocks(num_blocks),
+		blocksRead(0) { }
+
+void Ext2Driver::ReadDataClosure::read() {
+	uint32_t direct_index = firstBlock + blocksRead;
+	uint32_t block = inode.directBlocks[direct_index];
+
+	printf("reading block %d, %d of %d complete!\n", block, blocksRead, numBlocks);
+	parser.getPartition(1).readSectors(block * driver.sectorsPerBlock,
+			(uint8_t *)buffer + blocksRead * driver.blockSize, driver.sectorsPerBlock,
+			HELX_MEMBER(this, &Ext2Driver::ReadDataClosure::onReadComplete));	
+}
+
+struct DirEntry {
+	uint32_t inode;
+	uint16_t recordLength;
+	uint8_t nameLength;
+	uint8_t fileType;
+	char name[];
+};
+
+void Ext2Driver::ReadDataClosure::onReadComplete() {
+	blocksRead++;
+
+	if(blocksRead < numBlocks) {
+		read();
+	}else{
+		uintptr_t offset = 0;
+		while(offset < numBlocks * driver.blockSize) {
+			DirEntry *entry = (DirEntry *)((uintptr_t)buffer + offset);
+
+			printf("File: %.*s\n", entry->nameLength, entry->name);
+
+			offset += entry->recordLength;
+		}
+
+		printf("buffer: %x \n", buffer);		
+	}
+}
+
+Ext2Driver ext2driver;
+
+void test(void *object){
+
+}
+
+void testExt2Driver(){
+	ext2driver.work(helx::Callback<>(nullptr, &test));
 }
 
 void onTableComplete(void *object) {
-	GptParser::Partition &partition = parser.getPartition(1);
-	partition.readSectors(2, sectorbuffer, 1, helx::Callback<>(nullptr, &onSectorRead));
+	testExt2Driver();
 }
 
 void testAta() {
 	parser.parse(helx::Callback<>(nullptr, &onTableComplete));
-
 }
+
 
 // --------------------------------------------------------
 // IPC testing code
