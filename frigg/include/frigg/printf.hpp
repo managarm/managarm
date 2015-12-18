99164a8ae91865d3e14412a89414a251e1ca838c
avdgrinten <alexander.vandergrinten@gmail.com>
2015-12-18 13:21:20 +0100

    Fix frigg printf

--- frigg/include/frigg/printf.hpp
+++ frigg/include/frigg/printf.hpp
@@ -16,7 +16,7 @@ void printf(P &printer, const char *format, va_list args) {
 		}
 
 		++format;
-		assert(*format != 0);
+		assert(*format);
 		
 		bool left_justify = false;
 		bool always_sign = false;
@@ -28,44 +28,62 @@ void printf(P &printer, const char *format, va_list args) {
 			if(*format == '-') {
 				left_justify = true;
 				++format;
-				assert(*format != 0);
+				assert(*format);
 			}else if(*format == '+') {
 				always_sign = true;
 				++format;
-				assert(*format != 0);
+				assert(*format);
 			}else if(*format == ' ') {
 				plus_becomes_space = true;
 				++format;
-				assert(*format != 0);
+				assert(*format);
 			}else if(*format == '#') {
 				alt_conversion = true;
 				++format;
-				assert(*format != 0);
+				assert(*format);
 			}else if(*format == '0') {
 				fill_zeros = true;
 				++format;
-				assert(*format != 0);
+				assert(*format);
 			}else{
 				break;
 			}
 		}
 
+		assert(!left_justify);
+		assert(!always_sign);
+		assert(!plus_becomes_space);
+		assert(!alt_conversion);
+		assert(!fill_zeros);
+
 		int minimum_width = 0;
 		while(*format >= '0' && *format <= '9') {
 			minimum_width = minimum_width * 10 + (*format - '0');
 			++format;
-			assert(*format != 0);
+			assert(*format);
 		}
+
+		assert(minimum_width == 0);
 		
+		bool has_precision = false;
 		int precision = 0;
 		if(*format == '.') {
 			++format;
-			assert(*format != 0);
+			assert(*format);
 
-			while(*format >= '0' && *format <= '9') {
-				precision = precision * 10 + (*format - '0');
+			has_precision = true;
+			
+			if(*format == '*') {
 				++format;
-				assert(*format != 0);
+				assert(*format);
+				precision = va_arg(args, int);
+			}else{
+				assert(*format >= '0' && *format <= '9');
+				while(*format >= '0' && *format <= '9') {
+					precision = precision * 10 + (*format - '0');
+					++format;
+					assert(*format);
+				}
 			}
 		}
 		
@@ -76,14 +94,28 @@ void printf(P &printer, const char *format, va_list args) {
 		}
 
 		switch(*format) {
-		case 'u': {
-			if(l_prefix) {
-				printUInt(printer, va_arg(args, unsigned long), 10);
+		case '%':
+			printer.print('%');
+			break;
+		case 'c':
+			assert(!l_prefix);
+			assert(precision == 0);
+			printer.print((char)va_arg(args, int));
+			break;
+		case 's':
+			assert(!l_prefix);
+			if(has_precision) {
+				const char *s = va_arg(args, const char *);
+				for(int i = 0; i < precision && s[i]; i++)
+					printer.print(s[i]);
 			}else{
-				printUInt(printer, va_arg(args, unsigned int), 10);
+				printer.print(va_arg(args, const char *));
 			}
-		} break;
-		case 'd': {
+			break;
+		case 'd':
+		case 'i': {
+			assert(!l_prefix);
+			assert(precision == 0);
 			int number = va_arg(args, int);
 			if(number < 0) {
 				printer.print('-');
@@ -92,9 +124,26 @@ void printf(P &printer, const char *format, va_list args) {
 				printUInt(printer, number, 10);
 			}
 		} break;
-		case 's':
-			printer.print(va_arg(args, const char *));
+		case 'o': assert(!"Not implemented");
+		case 'x':
+			// TODO: Implement this correctly
+			assert(!l_prefix);
+			assert(precision == 0);
+			printUInt(printer, va_arg(args, unsigned long), 10);
 			break;
+		case 'X':
+			assert(!l_prefix);
+			assert(precision == 0);
+			printUInt(printer, va_arg(args, unsigned int), 16);
+			break;
+		case 'u': {
+			assert(precision == 0);
+			if(l_prefix) {
+				printUInt(printer, va_arg(args, unsigned long), 10);
+			}else{
+				printUInt(printer, va_arg(args, unsigned int), 10);
+			}
+		} break;
 		default:
 			assert(!"Illegal printf modifier");
 		}
