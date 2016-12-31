d4e3e3dcf3b20e4135f6c22d20221107260a6932
avdgrinten <alexander.vandergrinten@gmail.com>
2016-12-31 16:39:07 +0100

    thor: Fix physical memory alignment

--- thor/kernel/src/generic/physical.cpp
+++ thor/kernel/src/generic/physical.cpp
@@ -18,28 +18,38 @@ PhysicalChunkAllocator::PhysicalChunkAllocator(PhysicalAddr bootstrap_base,
 void PhysicalChunkAllocator::bootstrap() {
 	assert(!_buddyPointer);
 
+	// Align the base pointer to 2 MiB (the size of a large page).
+	// This ensures that we can hand out up to 2 MiB of contiguous physical memory.
+	// TODO: Check for overflow.
+	_physicalBase = _bootstrapBase;
+	auto limit = _bootstrapBase + _bootstrapLength;
+	_physicalBase = (_physicalBase + ((size_t(1) << 21) - 1)) & ~((size_t(1) << 21) - 1);
+	assert(_physicalBase <= limit);
+
 	// Now we proceed to initialize the buddy allocator.
-	_buddyOrder = frigg::buddy_tools::suitable_order(_bootstrapLength >> kPageShift);
+	_buddyOrder = frigg::buddy_tools::suitable_order((limit - _physicalBase) >> kPageShift);
 
 	// Subtract the buddy tree from the size of the chunk
 	// and determine the number of roots for the buddy tree.
-	auto pre_roots = _bootstrapLength >> (kPageShift + _buddyOrder);
-	auto overhead = frigg::buddy_tools::determine_size(pre_roots, _buddyOrder) >> kPageShift;
-	assert(overhead < _bootstrapLength);
-	
-	frigg::infoLogger() << "thor: Using an order "
-			<< _buddyOrder << " buddy allocator"
-			<< " (" << overhead << " pages overhead)"<< frigg::endLog;
-
-	_buddyRoots = (_bootstrapLength - overhead) >> (kPageShift + _buddyOrder);
+	auto pre_roots = (limit - _physicalBase) >> (kPageShift + _buddyOrder);
+	auto overhead = frigg::buddy_tools::determine_size(pre_roots, _buddyOrder);
+	assert(overhead < limit - _physicalBase);
+	_buddyRoots = (limit - _physicalBase - overhead) >> (kPageShift + _buddyOrder);
 	assert(_buddyRoots >= 32);
 
 	// Finally initialize the buddy tree.
-	_physicalBase = _bootstrapBase + (overhead << kPageShift);
-	_buddyPointer = reinterpret_cast<int8_t *>(physicalToVirtual(_bootstrapBase));
+	auto table = (limit - overhead) & ~((size_t(1) << kPageShift) - 1);
+	_buddyPointer = reinterpret_cast<int8_t *>(physicalToVirtual(table));
 	frigg::buddy_tools::initialize(_buddyPointer, _buddyRoots, _buddyOrder);
+	
+	frigg::infoLogger() << "Memory region from " << (void *)_physicalBase
+			<< " to " << (void *)limit << frigg::endLog;
+	frigg::infoLogger() << "    Using an order " << _buddyOrder << " buddy allocator"
+			<< " with " << _buddyRoots << " roots"
+			<< " (" << (overhead >> kPageShift) << " pages overhead)"<< frigg::endLog;
+	frigg::infoLogger() << "    Buddy table starts at " << (void *)table << frigg::endLog;
 
-	_freePages += (_bootstrapLength - overhead) >> kPageShift;
+	_freePages += (limit - _physicalBase - overhead) >> kPageShift;
 }
 
 PhysicalAddr PhysicalChunkAllocator::allocate(Guard &guard, size_t size) {
@@ -52,7 +62,9 @@ PhysicalAddr PhysicalChunkAllocator::allocate(Guard &guard, size_t size) {
 	int target = 0;
 	while(size > (size_t(kPageSize) << target))
 		target++;
-	assert(size == (size_t(kPageSize) << target));
+	if(size != (size_t(kPageSize) << target))
+		frigg::infoLogger() << "\e[31mPhysical allocation of size " << (void *)size
+				<< " rounded up to power of 2\e[39m" << frigg::endLog;
 
 	auto physical = _physicalBase + (frigg::buddy_tools::allocate(_buddyPointer,
 			_buddyRoots, _buddyOrder, target) << kPageShift);
@@ -66,7 +78,6 @@ void PhysicalChunkAllocator::free(Guard &guard, PhysicalAddr address, size_t siz
 	int target = 0;
 	while(size > (size_t(kPageSize) << target))
 		target++;
-	assert(size == (size_t(kPageSize) << target));
 
 	auto index = (address - _physicalBase) >> kPageShift;
 	frigg::buddy_tools::free(_buddyPointer, _buddyRoots, _buddyOrder,
