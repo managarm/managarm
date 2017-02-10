a24a5d182c63ecf43b5de7ad198751f5a0153d38
avdgrinten <alexander.vandergrinten@gmail.com>
2017-02-10 19:48:46 +0100

    gfx_intel: Display a sample image

--- drivers/gfx/intel/src/main.cpp
+++ drivers/gfx/intel/src/main.cpp
@@ -34,6 +34,9 @@ namespace regs {
 	static constexpr arch::bit_register<uint32_t> pipeConfig(0x70008);
 
 	static constexpr arch::bit_register<uint32_t> planeControl(0x70180);
+	static constexpr arch::scalar_register<uint32_t> planeOffset(0x70184);
+	static constexpr arch::scalar_register<uint32_t> planeStride(0x70188);
+	static constexpr arch::scalar_register<uint32_t> planeAddress(0x7019C);
 
 	static constexpr arch::bit_register<uint32_t> vgaControl(0x71400);
 }
@@ -86,9 +89,15 @@ namespace pipe_config {
 	static constexpr arch::field<uint32_t, bool> enablePipe(31, 1);
 }
 
+enum class PrimaryFormat : unsigned int {
+	INDEXED = 2,
+	BGRX8888 = 6,
+	RGBX8888 = 14
+};
+
 namespace plane_control {
 	static constexpr arch::field<uint32_t, bool> enablePlane(31, 1);
-	static constexpr arch::field<uint32_t, unsigned int> pixelFormat(26, 4);
+	static constexpr arch::field<uint32_t, PrimaryFormat> pixelFormat(26, 4);
 }
 
 namespace vga_control {
@@ -233,8 +242,8 @@ struct Mode {
 };
 
 struct Controller {
-	Controller(arch::mem_space space)
-	: _space{space} { }
+	Controller(arch::mem_space ctrl, void *memory)
+	: _ctrl{ctrl}, _memory{memory} { }
 
 	void run();
 
@@ -256,47 +265,52 @@ struct Controller {
 	void dumpPipe();
 
 private:
-	arch::mem_space _space;
+	arch::mem_space _ctrl;
+	void *_memory;
 };
 
 void Controller::disableDac() {
-	auto bits = _space.load(regs::dacPort);
+	auto bits = _ctrl.load(regs::dacPort);
 	std::cout << "DAC Port: " << static_cast<uint32_t>(bits) << std::endl;
 	assert(bits & dac_port::enableDac);
-	_space.store(regs::dacPort, bits & ~dac_port::enableDac);
+	_ctrl.store(regs::dacPort, bits & ~dac_port::enableDac);
 }
 
 void Controller::enableDac() {
-	auto bits = _space.load(regs::dacPort);
+	auto bits = _ctrl.load(regs::dacPort);
 	assert(!(bits & dac_port::enableDac));
-	_space.store(regs::dacPort, bits | dac_port::enableDac(true));
+	_ctrl.store(regs::dacPort, bits | dac_port::enableDac(true));
 }
 
 void Controller::disablePlane() {
-	auto bits = _space.load(regs::planeControl);
+	auto bits = _ctrl.load(regs::planeControl);
 	assert(bits & plane_control::enablePlane);
-	_space.store(regs::planeControl, bits & ~plane_control::enablePlane);
+	_ctrl.store(regs::planeControl, bits & ~plane_control::enablePlane);
 }
 
 void Controller::enablePlane() {
-	auto bits = _space.load(regs::planeControl);
+	_ctrl.store(regs::planeOffset, 0);
+	_ctrl.store(regs::planeStride, 640 * 4);
+	_ctrl.store(regs::planeAddress, 0);
+
+	auto bits = _ctrl.load(regs::planeControl);
 	std::cout << "Plane control: " << static_cast<uint32_t>(bits) << std::endl;
 	assert(!(bits & plane_control::enablePlane));
-	_space.store(regs::planeControl, (bits & ~plane_control::pixelFormat)
-			| plane_control::pixelFormat(2)
+	_ctrl.store(regs::planeControl, (bits & ~plane_control::pixelFormat)
+			| plane_control::pixelFormat(PrimaryFormat::RGBX8888)
 			| plane_control::enablePlane(true));
 }
 
 void Controller::disablePipe() {
-	auto bits = _space.load(regs::pipeConfig);
+	auto bits = _ctrl.load(regs::pipeConfig);
 	std::cout << "Pipe config: " << static_cast<uint32_t>(bits) << std::endl;
 	assert(bits & pipe_config::enablePipe);
 	assert(bits & pipe_config::pipeStatus);
-	_space.store(regs::pipeConfig, bits & ~pipe_config::enablePipe);
+	_ctrl.store(regs::pipeConfig, bits & ~pipe_config::enablePipe);
 	
-	std::cout << "After disable: " << (_space.load(regs::pipeConfig)
+	std::cout << "After disable: " << (_ctrl.load(regs::pipeConfig)
 			& pipe_config::pipeStatus) << std::endl;
-	while(_space.load(regs::pipeConfig) & pipe_config::pipeStatus) {
+	while(_ctrl.load(regs::pipeConfig) & pipe_config::pipeStatus) {
 		// Busy wait until the pipe is shut off.
 	}
 
@@ -305,30 +319,30 @@ void Controller::disablePipe() {
 
 void Controller::programPipe(Mode mode) {
 	// Program the display timings.
-	_space.store(regs::htotal, hvtotal::active(mode.horizontal.active - 1)
+	_ctrl.store(regs::htotal, hvtotal::active(mode.horizontal.active - 1)
 			| hvtotal::total(mode.horizontal.total - 1));
-	_space.store(regs::hblank, hvblank::start(mode.horizontal.blankingStart() - 1)
+	_ctrl.store(regs::hblank, hvblank::start(mode.horizontal.blankingStart() - 1)
 			| hvblank::end(mode.horizontal.blankingEnd() - 1));
-	_space.store(regs::hsync, hvsync::start(mode.horizontal.syncStart - 1)
+	_ctrl.store(regs::hsync, hvsync::start(mode.horizontal.syncStart - 1)
 			| hvsync::end(mode.horizontal.syncEnd - 1));
 	
-	_space.store(regs::vtotal, hvtotal::active(mode.vertical.active - 1)
+	_ctrl.store(regs::vtotal, hvtotal::active(mode.vertical.active - 1)
 			| hvtotal::total(mode.vertical.total - 1));
-	_space.store(regs::vblank, hvblank::start(mode.vertical.blankingStart() - 1)
+	_ctrl.store(regs::vblank, hvblank::start(mode.vertical.blankingStart() - 1)
 			| hvblank::end(mode.vertical.blankingEnd() - 1));
-	_space.store(regs::vsync, hvsync::start(mode.vertical.syncStart - 1)
+	_ctrl.store(regs::vsync, hvsync::start(mode.vertical.syncStart - 1)
 			| hvsync::end(mode.vertical.syncEnd - 1));
 	
-	_space.store(regs::sourceSize, source_size::vertical(mode.vertical.active - 1)
+	_ctrl.store(regs::sourceSize, source_size::vertical(mode.vertical.active - 1)
 			| source_size::horizontal(mode.horizontal.active - 1));
 	
 	// Enable the pipe.
-	auto bits = _space.load(regs::pipeConfig);
+	auto bits = _ctrl.load(regs::pipeConfig);
 	assert(!(bits & pipe_config::enablePipe));
 	assert(!(bits & pipe_config::pipeStatus));
-	_space.store(regs::pipeConfig, bits | pipe_config::enablePipe(true));
+	_ctrl.store(regs::pipeConfig, bits | pipe_config::enablePipe(true));
 	
-	while(!(_space.load(regs::pipeConfig) & pipe_config::pipeStatus)) {
+	while(!(_ctrl.load(regs::pipeConfig) & pipe_config::pipeStatus)) {
 		// Busy wait until the pipe is ready.
 	}
 
@@ -336,24 +350,24 @@ void Controller::programPipe(Mode mode) {
 }
 
 void Controller::disableDpll() {
-	auto bits = _space.load(regs::pllControl);
+	auto bits = _ctrl.load(regs::pllControl);
 	assert(bits & pll_control::enablePll);
-	_space.store(regs::pllControl, bits & ~pll_control::enablePll);
+	_ctrl.store(regs::pllControl, bits & ~pll_control::enablePll);
 }
 
 void Controller::programDpll(PllParams params) {
-	_space.store(regs::pllDivisor1, pll_divisor::m2(params.m2)
+	_ctrl.store(regs::pllDivisor1, pll_divisor::m2(params.m2)
 			| pll_divisor::m1(params.m1) | pll_divisor::n(params.n));
-	_space.store(regs::pllDivisor2, pll_divisor::m2(params.m2)
+	_ctrl.store(regs::pllDivisor2, pll_divisor::m2(params.m2)
 			| pll_divisor::m1(params.m1) | pll_divisor::n(params.n));
 	
-	_space.store(regs::pllControl, pll_control::enablePll(false));
+	_ctrl.store(regs::pllControl, pll_control::enablePll(false));
 
-	_space.store(regs::pllControl, pll_control::phase(6)
+	_ctrl.store(regs::pllControl, pll_control::phase(6)
 			| pll_control::encodedP1(1 << (params.p1 - 1))
 			| pll_control::modeSelect(1) | pll_control::disableVga(true)
 			| pll_control::enablePll(true));
-	_space.load(regs::pllControl);
+	_ctrl.load(regs::pllControl);
 
 	uint64_t ticks, now;
 	HEL_CHECK(helGetClock(&ticks));
@@ -361,18 +375,18 @@ void Controller::programDpll(PllParams params) {
 		HEL_CHECK(helGetClock(&now));
 	} while(now - ticks <= 150000);
 	
-	std::cout << "State: " << (_space.load(regs::pllControl) & pll_control::enablePll)
+	std::cout << "State: " << (_ctrl.load(regs::pllControl) & pll_control::enablePll)
 			<< std::endl;
 
-	_space.store(regs::busMultiplier, bus_multiplier::vgaMultiplier(3)
+	_ctrl.store(regs::busMultiplier, bus_multiplier::vgaMultiplier(3)
 			| bus_multiplier::dacMultiplier(3));
 	
 	for(int i = 0; i < 3; i++) {
-		_space.store(regs::pllControl, pll_control::phase(6)
+		_ctrl.store(regs::pllControl, pll_control::phase(6)
 				| pll_control::encodedP1(1 << (params.p1 - 1))
 				| pll_control::modeSelect(1) | pll_control::disableVga(true)
 				| pll_control::enablePll(true));
-		_space.load(regs::pllControl);
+		_ctrl.load(regs::pllControl);
 
 		uint64_t ticks, now;
 		HEL_CHECK(helGetClock(&ticks));
@@ -380,14 +394,14 @@ void Controller::programDpll(PllParams params) {
 			HEL_CHECK(helGetClock(&now));
 		} while(now - ticks <= 150000);
 	
-		std::cout << "State: " << (_space.load(regs::pllControl) & pll_control::enablePll)
+		std::cout << "State: " << (_ctrl.load(regs::pllControl) & pll_control::enablePll)
 				<< std::endl;
 	}
 }
 
 void Controller::dumpDpll() {
-	auto control = _space.load(regs::pllControl);
-	auto divisor1 = _space.load(regs::pllDivisor1);
+	auto control = _ctrl.load(regs::pllControl);
+	auto divisor1 = _ctrl.load(regs::pllDivisor1);
 
 	if(control & pll_control::enablePll) {
 		std::cout << "gfx_intel: DPLL is running." << std::endl;
@@ -405,19 +419,19 @@ void Controller::dumpDpll() {
 }
 
 void Controller::relinquishVga() {
-	auto bits = _space.load(regs::vgaControl);
+	auto bits = _ctrl.load(regs::vgaControl);
 	assert(!(bits & vga_control::disableVga));
-	_space.store(regs::vgaControl, (bits & ~vga_control::centeringMode)
+	_ctrl.store(regs::vgaControl, (bits & ~vga_control::centeringMode)
 			| vga_control::disableVga(true));
 }
 
 void Controller::dumpPipe() {
-	auto htotal = _space.load(regs::htotal);
-	auto hblank = _space.load(regs::hblank);
-	auto hsync = _space.load(regs::hsync);
-	auto vtotal = _space.load(regs::vtotal);
-	auto vblank = _space.load(regs::vblank);
-	auto vsync = _space.load(regs::vsync);
+	auto htotal = _ctrl.load(regs::htotal);
+	auto hblank = _ctrl.load(regs::hblank);
+	auto hsync = _ctrl.load(regs::hsync);
+	auto vtotal = _ctrl.load(regs::vtotal);
+	auto vblank = _ctrl.load(regs::vblank);
+	auto vsync = _ctrl.load(regs::vsync);
 
 	Timings horizontal;
 	Timings vertical;
@@ -458,6 +472,11 @@ void Controller::run() {
 	dumpPipe();
 	enablePlane();
 	enableDac();
+
+	auto plane = reinterpret_cast<uint32_t *>(_memory);
+	for(size_t x = 0; x < 640; x++)
+		for(size_t y = 0; y < 480; y++)
+			plane[y * 640 + x] = (x / 3) | ((y / 2) << 8);
 }
 
 // ----------------------------------------------------------------
@@ -468,14 +487,17 @@ COFIBER_ROUTINE(cofiber::no_future, bindController(mbus::Entity entity), ([=] {
 	protocols::hw::Device device(COFIBER_AWAIT entity.bind());
 	auto info = COFIBER_AWAIT device.getPciInfo();
 	assert(info.barInfo[0].ioType == protocols::hw::IoType::kIoTypeMemory);
-	auto bar = COFIBER_AWAIT device.accessBar(0);
+	auto ctrl_bar = COFIBER_AWAIT device.accessBar(0);
+	auto memory_bar = COFIBER_AWAIT device.accessBar(2);
 //	auto irq = COFIBER_AWAIT device.accessIrq();
 	
-	void *actual_pointer;
-	HEL_CHECK(helMapMemory(bar.getHandle(), kHelNullHandle, nullptr,
-			0, 0x80000, kHelMapReadWrite | kHelMapShareAtFork, &actual_pointer));
+	void *ctrl_window, *memory_window;
+	HEL_CHECK(helMapMemory(ctrl_bar.getHandle(), kHelNullHandle, nullptr,
+			0, 0x8'0000, kHelMapReadWrite | kHelMapShareAtFork, &ctrl_window));
+	HEL_CHECK(helMapMemory(memory_bar.getHandle(), kHelNullHandle, nullptr,
+			0, 0x1000'0000, kHelMapReadWrite | kHelMapShareAtFork, &memory_window));
 
-	Controller controller{arch::mem_space(actual_pointer)};
+	Controller controller{arch::mem_space(ctrl_window), memory_window};
 	controller.run();
 }))
 
