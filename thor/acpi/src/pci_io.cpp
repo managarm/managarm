64dc4f063239bb2bb80a503429f3c308537f0bd8
avdgrinten <alexander.vandergrinten@gmail.com>
2017-02-13 20:33:07 +0100

    ACPI: Use fine grained I/O for PCI config access

--- thor/acpi/src/pci_io.cpp
+++ thor/acpi/src/pci_io.cpp
@@ -9,31 +9,40 @@
 #include "pci.hpp"
 
 uint32_t readPciWord(uint32_t bus, uint32_t slot, uint32_t function, uint32_t offset) {
-	assert(bus < 256 && slot < 32 && function < 8);
-	assert((offset % 4) == 0 && offset < 256);
-	uint32_t address = (bus << 16) | (slot << 11) | (function << 8) | offset | 0x80000000;
-	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
-
+	assert(bus < 256 && slot < 32 && function < 8 && offset < 256);
+	assert(!(offset & 3));
 	uint32_t result;
-	asm volatile ( "inl %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC)) );
+	auto address = (bus << 16) | (slot << 11) | (function << 8)
+			| (offset & ~uint32_t(3)) | 0x80000000;
+	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
+	asm volatile ( "inl %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC + (offset & 3))) );
 	return result;
 }
 
 uint16_t readPciHalf(uint32_t bus, uint32_t slot, uint32_t function, uint32_t offset) {
-	assert((offset % 2) == 0);
-	uint32_t word = readPciWord(bus, slot, function, offset & ~uint32_t(3));
-	return (word >> ((offset & 3)) * 8) & 0xFFFF;
+	assert(bus < 256 && slot < 32 && function < 8 && offset < 256);
+	assert(!(offset & 1));
+	uint16_t result;
+	auto address = (bus << 16) | (slot << 11) | (function << 8)
+			| (offset & ~uint32_t(3)) | 0x80000000;
+	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
+	asm volatile ( "inw %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC + (offset & 3))) );
 }
 
 uint8_t readPciByte(uint32_t bus, uint32_t slot, uint32_t function, uint32_t offset) {
-	uint32_t word = readPciWord(bus, slot, function, offset & ~uint32_t(3));
-	return (word >> ((offset & 3)) * 8) & 0xFF;
+	assert(bus < 256 && slot < 32 && function < 8 && offset < 256);
+	uint8_t result;
+	auto address = (bus << 16) | (slot << 11) | (function << 8)
+			| (offset & ~uint32_t(3)) | 0x80000000;
+	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
+	asm volatile ( "inb %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC + (offset & 3))) );
 }
 
 void writePciWord(uint32_t bus, uint32_t slot, uint32_t function, uint32_t offset, uint32_t value) {
-	assert(bus < 256 && slot < 32 && function < 8);
-	assert((offset % 4) == 0 && offset < 256);
-	uint32_t address = (bus << 16) | (slot << 11) | (function << 8) | offset | 0x80000000;
+	assert(bus < 256 && slot < 32 && function < 8 && offset < 256);
+	assert(!(offset % 4));
+	auto address = (bus << 16) | (slot << 11) | (function << 8)
+			| (offset & ~uint32_t(3)) | 0x80000000;
 	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
 	asm volatile ( "outl %0, %1" : : "a" (value), "d" (uint16_t(0xCFC)) );
 }
