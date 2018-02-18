8aa13c4db2465d8c047ed6a4faa690623ff141fa
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-02-18 23:12:24 +0100

    posix: Fix interval for timerfd()

--- posix/subsystem/src/timerfd.cpp
+++ posix/subsystem/src/timerfd.cpp
@@ -12,6 +12,57 @@
 namespace {
 
 struct OpenFile : ProxyFile {
+private:
+	struct Timer {
+		uint64_t initial;
+		uint64_t interval;
+	};
+	
+	COFIBER_ROUTINE(cofiber::no_future, arm(Timer *timer), ([=] {
+		assert(timer->initial || timer->interval);
+//		std::cout << "posix: Timer armed" << std::endl;
+
+		uint64_t tick;
+		HEL_CHECK(helGetClock(&tick));
+
+		if(timer->initial) {
+			helix::AwaitClock await_initial;
+			auto &&submit = helix::submitAwaitClock(&await_initial, tick + timer->initial,
+					helix::Dispatcher::global());
+			COFIBER_AWAIT submit.async_wait();
+			HEL_CHECK(await_initial.error());
+			tick += timer->initial;
+
+			if(_activeTimer == timer) {
+				_expirations++;
+				_theSeq++;
+				_seqBell.ring();
+			}else{
+				COFIBER_RETURN();
+			}
+		}
+
+		if(!timer->interval)
+			COFIBER_RETURN();
+
+		while(true) {
+			helix::AwaitClock await_interval;
+			auto &&submit = helix::submitAwaitClock(&await_interval, tick + timer->interval,
+					helix::Dispatcher::global());
+			COFIBER_AWAIT submit.async_wait();
+			HEL_CHECK(await_interval.error());
+			tick += timer->interval;
+
+			if(_activeTimer == timer) {
+				_expirations++;
+				_theSeq++;
+				_seqBell.ring();
+			}else{
+				COFIBER_RETURN();
+			}
+		}
+	}))
+
 	// ------------------------------------------------------------------------
 	// File protocol adapters.
 	// ------------------------------------------------------------------------
@@ -36,7 +87,7 @@ public:
 	}
 
 	OpenFile()
-	: ProxyFile{nullptr}, _expirations{0}, _theSeq{1} { }
+	: ProxyFile{nullptr}, _activeTimer{nullptr}, _expirations{0}, _theSeq{1} { }
 
 	COFIBER_ROUTINE(FutureMaybe<size_t>, readSome(void *data, size_t max_length) override, ([=] {
 		assert(max_length == sizeof(uint64_t));
@@ -58,28 +109,23 @@ public:
 	helix::BorrowedDescriptor getPassthroughLane() override {
 		return _passthrough;
 	}
-	
-	COFIBER_ROUTINE(cofiber::no_future, setTime(struct timespec time), ([=] {
-		uint64_t tick;
-		HEL_CHECK(helGetClock(&tick));
 
-//		std::cout << "posix: Timer armed" << std::endl;
-
-		helix::AwaitClock await_clock;
-		auto &&submit = helix::submitAwaitClock(&await_clock, tick + time.tv_nsec,
-				helix::Dispatcher::global());
-		COFIBER_AWAIT submit.async_wait();
-		HEL_CHECK(await_clock.error());
-
-//		std::cout << "posix: Timer expiration" << std::endl;
-		_expirations++;
-
-		_theSeq++;
-		_seqBell.ring();
-	}))
+	void setTime(uint64_t initial, uint64_t interval) {
+		if(initial || interval) {
+			_activeTimer = new Timer;
+			_activeTimer->initial = initial;
+			_activeTimer->interval = interval;
+			arm(_activeTimer);
+		}else{
+			_activeTimer = nullptr;
+		}
+	}
 
 private:
 	helix::UniqueLane _passthrough;
+
+	// Currently active timer.
+	Timer *_activeTimer;
 
 	// Number of expirations since last read().
 	uint64_t _expirations;
@@ -99,11 +145,23 @@ std::shared_ptr<ProxyFile> createFile() {
 }
 
 void setTime(File *file, struct timespec initial, struct timespec interval) {
-	assert(!interval.tv_sec);
-	assert(!interval.tv_nsec);
+	assert(initial.tv_sec >= 0 && initial.tv_nsec >= 0);
+	assert(interval.tv_sec >= 0 && interval.tv_nsec >= 0);
+
+	uint64_t initial_nanos;
+	if(__builtin_mul_overflow(initial.tv_sec, 1000000000, &initial_nanos)
+			|| __builtin_add_overflow(initial.tv_nsec, initial_nanos, &initial_nanos))
+		throw std::runtime_error("Overflow in timerfd setup");
+
+	uint64_t interval_nanos;
+	if(__builtin_mul_overflow(interval.tv_sec, 1000000000, &interval_nanos)
+			|| __builtin_add_overflow(interval.tv_nsec, interval_nanos, &interval_nanos))
+		throw std::runtime_error("Overflow in timerfd setup");
 
 	auto timerfd = static_cast<OpenFile *>(file);
-	timerfd->setTime(initial);
+	// TODO: Check for overflow.
+//	timerfd->setTime(initial_nanos, interval_nanos);
 }
 
 } // namespace timerfd
+
