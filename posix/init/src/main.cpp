a2be26e0f04871f1bd782ae1e33f1d386d42fada
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-03-02 22:26:07 +0100

    posix: Tidy up and refactor init process

--- posix/init/src/main.cpp
+++ posix/init/src/main.cpp
@@ -1,25 +1,15 @@
 
 #include <assert.h>
 #include <fcntl.h>
-#include <linux/input.h>
-#include <netinet/in.h> // FIXME: for testing
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/mount.h>
-#include <sys/socket.h> // FIXME: for testing
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 #include <iostream>
-#include <vector>
-
-//FIXME:
-#include <string.h>
-#include <hel.h>
-#include <hel-syscalls.h>
 
 int main() {
 	int fd = open("/dev/helout", O_WRONLY);
@@ -27,39 +17,25 @@ int main() {
 	dup2(fd, STDERR_FILENO);
 	printf("Starting posix-init\n");
 	
-	std::vector<char *> args;
-	args.push_back(const_cast<char *>("acpi"));
-	args.push_back("--use-pixman");
-	args.push_back(nullptr);
-
-	std::vector<char *> env;
-	env.push_back(const_cast<char *>("XDG_RUNTIME_DIR=/run"));
-	env.push_back(const_cast<char *>("MESA_GLSL_CACHE_DISABLE=1"));
-	env.push_back(const_cast<char *>("MESA_DEBUG=1"));
-//	env.push_back(const_cast<char *>("TGSI_PRINT_SANITY=1"));
-//	env.push_back(const_cast<char *>("SOFTPIPE_NO_RAST=1"));
-//	env.push_back(const_cast<char *>("SOFTPIPE_DUMP_FS=1"));
-	env.push_back(nullptr);
-
 	// Start essential bus and storage drivers.
 	auto uhci = fork();
 	if(!uhci) {
-		execve("/sbin/uhci", args.data(), env.data());
+		execl("/sbin/uhci", "uhci", nullptr);
 	}else assert(uhci != -1);
 
 	auto ehci = fork();
 	if(!ehci) {
-		execve("/sbin/ehci", args.data(), env.data());
+		execl("/sbin/ehci", "ehci", nullptr);
 	}else assert(ehci != -1);
 
 	auto virtio = fork();
 	if(!virtio) {
-		execve("/sbin/virtio-block", args.data(), env.data());
+		execl("/sbin/virtio-block", "virtio-block", nullptr);
 	}else assert(virtio != -1);
 
 	auto storage = fork();
 	if(!storage) {
-		execve("/sbin/storage", args.data(), env.data());
+		execl("/sbin/storage", "storage", nullptr);
 	}else assert(storage != -1);
 
 	// Spin until /dev/sda0 becomes available. Then mount the rootfs and prepare it.
@@ -86,17 +62,17 @@ int main() {
 	/*
 	auto gfx_bochs = fork();
 	if(!gfx_bochs) {
-		execve("/usr/bin/gfx_bochs", args.data(), env.data());
+		execl("/usr/bin/gfx_bochs", "gfx_bochs", nullptr);
 	}else assert(gfx_bochs != -1);
 	*/
 	auto gfx_virtio = fork();
 	if(!gfx_virtio) {
-		execve("/usr/bin/gfx_virtio", args.data(), env.data());
+		execl("/usr/bin/gfx_virtio", "gfx_virtio", nullptr);
 	}else assert(gfx_virtio != -1);
 	
 	auto input_hid = fork();
 	if(!input_hid) {
-		execve("/usr/bin/hid", args.data(), env.data());
+		execl("/usr/bin/hid", "hid", nullptr);
 	}else assert(input_hid != -1);
 
 	while(access("/dev/dri/card0", F_OK)) {
@@ -111,131 +87,19 @@ int main() {
 
 	auto modeset = fork();
 	if(!modeset) {
+		putenv("XDG_RUNTIME_DIR=/run");
+		putenv("MESA_GLSL_CACHE_DISABLE=1");
+		putenv("MESA_DEBUG=1");
+//		putenv("TGSI_PRINT_SANITY=1");
+//		putenv("SOFTPIPE_NO_RAST=1");
+//		putenv("SOFTPIPE_DUMP_FS=1");
+
 		//execve("/root/unixsock", args.data(), env.data());
 		//execve("/root/test-libudev", args.data(), env.data());
 		//execve("/usr/bin/kmscube", args.data(), env.data());
 		//execve("/root/modeset-render", args.data(), env.data());
 		//execve("/root/modeset-double-buffered", args.data(), env.data());
-		execve("/usr/bin/weston", args.data(), env.data());
+		execl("/usr/bin/weston", "weston", "--use-pixman", nullptr);
 	}else assert(modeset != -1);
-
-/*
-	// UART
-	auto uart = fork();
-	if(!uart) {
-		execve("/usr/bin/uart", args.data(), env.data());
-	}else assert(uart != -1);
-
-	// Spin until /dev/ttyS0 becomes available.
-	while(access("/dev/ttyS0", F_OK)) {
-		assert(errno == ENOENT);
-		sleep(1);
-	}
-	
-	std::cout << "init: /dev/ttyS0 is available" << std::endl;
-	
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	close(STDERR_FILENO);
-	int tty = open("/dev/ttyS0", O_WRONLY);
-	dup2(tty, STDOUT_FILENO);
-	dup2(tty, STDERR_FILENO);
-	
-	std::cout << "init: On serial console" << std::endl;
-*/
-/*
-	auto hid = fork();
-	if(!hid) {
-		execve("/realfs/usr/bin/hid", args.data(), env.data());
-	}else assert(hid != -1);
-*/
-/*
-	auto bash = fork();
-	if(!bash) {
-		execve("/realfs/usr/bin/bash", args.data(), env.data());
-	}else assert(bash != -1);
-*/
-/*
-	while(true) {
-		char buffer[1];
-		auto read_sz = read(STDOUT_FILENO, buffer, 1);
-		assert(read_sz > 0);
-		std::cout << buffer[0] << std::endl;
-	}
-*/
-/*
-	auto gfx_intel = fork();
-	if(!gfx_intel) {
-		execve("/usr/bin/gfx_intel", args.data(), env.data());
-	}else assert(gfx_intel != -1);
-*/
-
-/*
-	auto hid = fork();
-	if(!hid) {
-		execve("/usr/bin/hid", args.data(), env.data());
-	}else assert(hid != -1);
-
-	// Spin until /dev/event0 becomes available.
-	while(access("/dev/event0", F_OK)) {
-		assert(errno == ENOENT);
-		for(int i = 0; i < 100; i++)
-			sched_yield();
-	}
-
-	// dump input events
-	const char *path = "/dev/event1";
-	int file = open(path, 0);
-	assert(!(file == -1));
-
-	while(true) {
-		input_event event;
-		size_t read_bytes = read(file, &event, sizeof(input_event));
-		assert(!(read_bytes == -1));
-		
-		printf("type: %i\n", event.type);
-		printf("code: %i\n", event.code);
-		printf("val: %i\n", event.value);
-		printf("-----------\n");
-	}
-*/
-/*
-	auto vga_terminal = fork();
-	if(!vga_terminal) {
-		execve("/realfs/usr/bin/vga_terminal", args.data(), env.data());
-	}else assert(vga_terminal != -1);
-*/
-
-/*	
-	// TODO: this is a very ugly hack to wait until the fs is ready
-	for(int i = 0; i < 10000; i++)
-		sched_yield();
-
-	printf("Second fork, here we go!\n");
-
-	pid_t terminal_child = fork();
-	assert(terminal_child != -1);
-	if(!terminal_child) {
-//		execve("/usr/bin/kbd", args.data(), env.data());
-		execve("/usr/bin/uhci", args.data(), env.data());
-//		execve("/usr/bin/virtio-net", args.data(), env.data());
-//		execve("/usr/bin/bochs_vga", args.data(), env.data());
-//		execve("/usr/bin/zisa", args.data(), env.data());
-	}
-*/	
-	// TODO: this is a very ugly hack to wait until the fs is ready
-/*	for(int i = 0; i < 10000; i++)
-		sched_yield();
-	
-	printf("Testing network API!\n");
-	
-	int socket = open("/dev/network/ip+udp", O_RDWR);
-	
-	struct sockaddr_in address;
-	address.sin_family = AF_INET;
-	address.sin_port = 7;
-	address.sin_addr.s_addr = (10 << 24) | (85 << 16) | (1 << 8) | 1;
-	connect(socket, (struct sockaddr *)&address, sizeof(struct sockaddr_in));
-	write(socket, "hello", 5);*/
 }
 
