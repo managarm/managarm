423bafeb289ca29422a49c67beefb80ba3191cd8
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-05-13 00:50:27 +0200

    drivers/block-ata: Make IRQ handling more robust

--- drivers/block/ata/src/main.cpp
+++ drivers/block/ata/src/main.cpp
@@ -12,6 +12,11 @@
 
 #include <blockfs.hpp>
 
+namespace {
+	constexpr bool logIrqs = false;
+	constexpr bool logRequests = false;
+}
+
 // --------------------------------------------------------
 // Controller class
 // --------------------------------------------------------
@@ -26,6 +31,10 @@ namespace regs {
 	inline constexpr arch::scalar_register<uint8_t> outLba3{5};
 	inline constexpr arch::scalar_register<uint8_t> outDevice{6};
 	inline constexpr arch::scalar_register<uint8_t> outCommand{7};
+}
+
+namespace alt_regs {
+	inline constexpr arch::scalar_register<uint8_t> inStatus{0};
 }
 
 class Controller : public blockfs::BlockDevice {
@@ -74,12 +83,13 @@ private:
 	helix::UniqueDescriptor _irq;
 	HelHandle _ioHandle;
 	arch::io_space _ioSpace;
+	arch::io_space _altSpace;
 
 	bool _inRequest;
 };
 
 Controller::Controller()
-: BlockDevice{512}, _ioSpace{0x1F0}, _inRequest{false} {
+: BlockDevice{512}, _ioSpace{0x1F0}, _altSpace{0x3F6}, _inRequest{false} {
 	HelHandle irq_handle;
 	HEL_CHECK(helAccessIrq(14, &irq_handle));
 	_irq = helix::UniqueDescriptor{irq_handle};
@@ -98,41 +108,51 @@ void Controller::run() {
 COFIBER_ROUTINE(cofiber::no_future, Controller::_handleIrqs(), ([=] {
 	uint64_t sequence = 0;
 	while(true) {
-//		std::cout << "block-ata: Awaiting IRQ." << std::endl;
+		if(logIrqs)
+			std::cout << "block-ata: Awaiting IRQ." << std::endl;
 		helix::AwaitEvent await_irq;
 		auto &&submit = helix::submitAwaitEvent(_irq, &await_irq, sequence,
 				helix::Dispatcher::global());
 		COFIBER_AWAIT submit.async_wait();
 		HEL_CHECK(await_irq.error());
 		sequence = await_irq.sequence();
-//		std::cout << "block-ata: IRQ fired." << std::endl;
+		if(logIrqs)
+			std::cout << "block-ata: IRQ fired." << std::endl;
+
+		// Check if the device is ready without clearing the IRQ.
+		auto status = _altSpace.load(alt_regs::inStatus);
+		if(status & kStatusBsy)
+			continue;
+		assert(!(status & (kStatusErr | kStatusDf)));
+		assert(status & kStatusRdy);
+		assert(status & kStatusDrq);
+		
+		// Clear and acknowledge the IRQ.
+		auto cleared = _ioSpace.load(regs::inStatus);
+		HEL_CHECK(helAcknowledgeIrq(_irq.getHandle(), 0, 0));
+		assert(!(cleared & (kStatusErr | kStatusDf)));
+		assert(cleared & kStatusRdy);
+		assert(cleared & kStatusDrq);
+		status = cleared;
 
 		assert(_inRequest);
 		assert(!_requestQueue.empty());
 		auto request = &_requestQueue.front();
-		
-		// acknowledge the interrupt
-		auto status = _ioSpace.load(regs::inStatus);
-		assert(!(status & kStatusBsy));
-		assert(!(status & kStatusErr));
-		assert(!(status & kStatusDf));
-		assert(status & kStatusRdy);
-		assert(status & kStatusDrq);
-		
+	
 		auto dest = reinterpret_cast<uint8_t *>(request->buffer)
 				+ request->sectorsRead * 512;
 		for(int i = 0; i < 256; i++) {
+			// TODO: Be careful with endianess here.
 			uint16_t data = _ioSpace.load(regs::inData);
 			memcpy(dest + 2 * i, &data, sizeof(uint16_t));
 		}
-		
-		HEL_CHECK(helAcknowledgeIrq(_irq.getHandle(), 0, 0));
 
 		request->sectorsRead++;
 		assert(request->sectorsRead <= request->numSectors);
 		if(request->sectorsRead == request->numSectors) {
-//			std::cout << "block-ata: Reading from " << request->sector
-//					<< " complete" << std::endl;
+			if(logRequests)
+				std::cout << "block-ata: Reading from " << request->sector
+						<< " complete" << std::endl;
 			request->promise.set_value();
 			_requestQueue.pop();
 			_inRequest = false;
@@ -144,8 +164,6 @@ COFIBER_ROUTINE(cofiber::no_future, Controller::_handleIrqs(), ([=] {
 }))
 
 async::result<void> Controller::readSectors(uint64_t sector, void *buffer, size_t num_sectors) {
-//	std::cout << "block-ata: Reading " << num_sectors
-//			<< " sectors from " << sector << std::endl;
 	_requestQueue.emplace();
 	auto request = &_requestQueue.back();
 	request->sector = sector;
@@ -165,9 +183,14 @@ void Controller::_performRequest() {
 
 	assert(!_requestQueue.empty());
 	auto request = &_requestQueue.front();
+	
+	if(logRequests)
+		std::cout << "block-ata: Reading " << request->numSectors
+				<< " sectors from " << request->sector << std::endl;
 
 	assert(!(request->sector & ~((size_t(1) << 48) - 1)));
 	_ioSpace.store(regs::outDevice, kDeviceLba);
+	// TODO: There should be a delay after drive selection.
 	
 	_ioSpace.store(regs::outSectorCount, (request->numSectors >> 8) & 0xFF);
 	_ioSpace.store(regs::outLba1, (request->sector >> 24) & 0xFF);
