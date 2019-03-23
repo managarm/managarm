bbb3a98bcd518ebde1f5c06f606e05ea554a3e04
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-03-23 13:09:04 +0100

    drivers/kernletcc: Disable hexdump

--- drivers/kernletcc/src/main.cpp
+++ drivers/kernletcc/src/main.cpp
@@ -9,6 +9,8 @@
 #include <kernlet.pb.h>
 #include "common.hpp"
 
+static bool dumpHex = false;
+
 // ----------------------------------------------------------------------------
 // kernletctl handling.
 // ----------------------------------------------------------------------------
@@ -125,14 +127,16 @@ COFIBER_ROUTINE(cofiber::no_future, serveCompiler(helix::UniqueLane lane),
 			auto elf = compileFafnir(reinterpret_cast<const uint8_t *>(recv_code.data()),
 					recv_code.length(), bind_types);
 
-			for(size_t i = 0; i < elf.size(); i++) {
-				printf("%02x", elf[i]);
-				if((i % 32) == 31)
-					putchar('\n');
-				else if((i % 8) == 7)
-					putchar(' ');
+			if(dumpHex) {
+				for(size_t i = 0; i < elf.size(); i++) {
+					printf("%02x", elf[i]);
+					if((i % 32) == 31)
+						putchar('\n');
+					else if((i % 8) == 7)
+						putchar(' ');
+				}
+				putchar('\n');
 			}
-			putchar('\n');
 
 			auto object = COFIBER_AWAIT upload(elf.data(), elf.size(), bind_types);
 
