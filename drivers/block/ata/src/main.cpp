205ffa09c409453cf9052de013f04d1a57a40963
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-05-05 11:40:16 +0200

    drivers/ata: Refactor IRQ handling
    This fixes ATA on VirtualBox.

--- drivers/block/ata/src/main.cpp
+++ drivers/block/ata/src/main.cpp
@@ -6,6 +6,7 @@
 #include <queue>
 
 #include <async/result.hpp>
+#include <async/doorbell.hpp>
 #include <arch/io_space.hpp>
 #include <arch/register.hpp>
 #include <helix/ipc.hpp>
@@ -45,14 +46,11 @@ public:
 	void run();
 
 private:
-	cofiber::no_future _handleIrqs();
+	cofiber::no_future _doRequestLoop();
 
 public:
 	async::result<void> readSectors(uint64_t sector, void *buffer,
 			size_t num_sectors) override;
-
-private:
-	void _performRequest();
 
 private:
 	enum Commands {
@@ -73,23 +71,25 @@ private:
 	struct Request {
 		uint64_t sector;
 		size_t numSectors;
-		size_t sectorsRead;
 		void *buffer;
 		async::promise<void> promise;
 	};
 
-	std::queue<Request> _requestQueue;
+	async::result<void> _performRequest(Request *request);
+
+	std::queue<std::unique_ptr<Request>> _requestQueue;
+	async::doorbell _doorbell;
 
 	helix::UniqueDescriptor _irq;
 	HelHandle _ioHandle;
 	arch::io_space _ioSpace;
 	arch::io_space _altSpace;
 
-	bool _inRequest;
+	uint64_t _irqSequence = 0;
 };
 
 Controller::Controller()
-: BlockDevice{512}, _ioSpace{0x1F0}, _altSpace{0x3F6}, _inRequest{false} {
+: BlockDevice{512}, _ioSpace{0x1F0}, _altSpace{0x3F6} {
 	HelHandle irq_handle;
 	HEL_CHECK(helAccessIrq(14, &irq_handle));
 	_irq = helix::UniqueDescriptor{irq_handle};
@@ -100,112 +100,106 @@ Controller::Controller()
 }
 
 void Controller::run() {
-	_handleIrqs();
+	_doRequestLoop();
 
 	blockfs::runDevice(this);
 }
 
-COFIBER_ROUTINE(cofiber::no_future, Controller::_handleIrqs(), ([=] {
-	uint64_t sequence = 0;
+COFIBER_ROUTINE(cofiber::no_future, Controller::_doRequestLoop(), ([=] {
 	while(true) {
-		if(logIrqs)
-			std::cout << "block-ata: Awaiting IRQ." << std::endl;
-		helix::AwaitEvent await_irq;
-		auto &&submit = helix::submitAwaitEvent(_irq, &await_irq, sequence,
-				helix::Dispatcher::global());
-		COFIBER_AWAIT submit.async_wait();
-		HEL_CHECK(await_irq.error());
-		sequence = await_irq.sequence();
-		if(logIrqs)
-			std::cout << "block-ata: IRQ fired." << std::endl;
-
-		// Check if the device is ready without clearing the IRQ.
-		auto status = _altSpace.load(alt_regs::inStatus);
-		if(status & kStatusBsy) {
-			HEL_CHECK(helAcknowledgeIrq(_irq.getHandle(), kHelAckNack, sequence));
+		if(_requestQueue.empty()) {
+			COFIBER_AWAIT _doorbell.async_wait();
 			continue;
 		}
-		assert(!(status & (kStatusErr | kStatusDf)));
-		assert(status & kStatusRdy);
-		assert(status & kStatusDrq);
-		
-		// Clear and acknowledge the IRQ.
-		auto cleared = _ioSpace.load(regs::inStatus);
-		HEL_CHECK(helAcknowledgeIrq(_irq.getHandle(), kHelAckAcknowledge, sequence));
-		assert(!(cleared & (kStatusErr | kStatusDf)));
-		assert(cleared & kStatusRdy);
-		assert(cleared & kStatusDrq);
-		status = cleared;
 
-		assert(_inRequest);
-		assert(!_requestQueue.empty());
-		auto request = &_requestQueue.front();
-	
-		auto dest = reinterpret_cast<uint8_t *>(request->buffer)
-				+ request->sectorsRead * 512;
-		for(int i = 0; i < 256; i++) {
-			// TODO: Be careful with endianess here.
-			uint16_t data = _ioSpace.load(regs::inData);
-			memcpy(dest + 2 * i, &data, sizeof(uint16_t));
-		}
-
-		request->sectorsRead++;
-		assert(request->sectorsRead <= request->numSectors);
-		if(request->sectorsRead == request->numSectors) {
-			if(logRequests)
-				std::cout << "block-ata: Reading from " << request->sector
-						<< " complete" << std::endl;
-			request->promise.set_value();
-			_requestQueue.pop();
-			_inRequest = false;
-
-			if(!_requestQueue.empty())
-				_performRequest();
-		}
+		auto request = _requestQueue.front().get();
+		COFIBER_AWAIT _performRequest(request);
+		request->promise.set_value();
+		_requestQueue.pop();
 	}
 }))
 
 async::result<void> Controller::readSectors(uint64_t sector, void *buffer, size_t num_sectors) {
-	_requestQueue.emplace();
-	auto request = &_requestQueue.back();
+	auto request = std::make_unique<Request>();
+	auto future = request->promise.async_get();
 	request->sector = sector;
 	request->numSectors = num_sectors;
-	request->sectorsRead = 0;
 	request->buffer = buffer;
-	auto future = request->promise.async_get();
 
-	if(!_inRequest)
-		_performRequest();
+	_requestQueue.push(std::move(request));
+	_doorbell.ring();
+
 	return future;
 }
 
-void Controller::_performRequest() {
-	assert(!_inRequest);
-	_inRequest = true;
-
-	assert(!_requestQueue.empty());
-	auto request = &_requestQueue.front();
-	
+COFIBER_ROUTINE(async::result<void>, Controller::_performRequest(Request *request), ([=] {
 	if(logRequests)
-		std::cout << "block-ata: Reading " << request->numSectors
+		std::cout << "block/ata: Reading " << request->numSectors
 				<< " sectors from " << request->sector << std::endl;
 
 	assert(!(request->sector & ~((size_t(1) << 48) - 1)));
 	_ioSpace.store(regs::outDevice, kDeviceLba);
 	// TODO: There should be a delay after drive selection.
-	
+
 	_ioSpace.store(regs::outSectorCount, (request->numSectors >> 8) & 0xFF);
 	_ioSpace.store(regs::outLba1, (request->sector >> 24) & 0xFF);
 	_ioSpace.store(regs::outLba2, (request->sector >> 32) & 0xFF);
-	_ioSpace.store(regs::outLba3, (request->sector >> 40) & 0xFF);	
-	
+	_ioSpace.store(regs::outLba3, (request->sector >> 40) & 0xFF);
+
 	_ioSpace.store(regs::outSectorCount, request->numSectors & 0xFF);
 	_ioSpace.store(regs::outLba1, request->sector & 0xFF);
 	_ioSpace.store(regs::outLba2, (request->sector >> 8) & 0xFF);
 	_ioSpace.store(regs::outLba3, (request->sector >> 16) & 0xFF);
-	
+
 	_ioSpace.store(regs::outCommand, kCommandReadSectorsExt);
-}
+
+	// Receive the result for each sector.
+	for(size_t k = 0; k < request->numSectors; k++) {
+		while(true) {
+			if(logIrqs)
+				std::cout << "block/ata: Awaiting IRQ." << std::endl;
+			helix::AwaitEvent await_irq;
+			auto &&submit = helix::submitAwaitEvent(_irq, &await_irq, _irqSequence,
+					helix::Dispatcher::global());
+			COFIBER_AWAIT submit.async_wait();
+			HEL_CHECK(await_irq.error());
+			_irqSequence = await_irq.sequence();
+			if(logIrqs)
+				std::cout << "block/ata: IRQ fired." << std::endl;
+
+			// Check if the device is ready without clearing the IRQ.
+			auto alt_status = _altSpace.load(alt_regs::inStatus);
+			if(alt_status & kStatusBsy) {
+				HEL_CHECK(helAcknowledgeIrq(_irq.getHandle(), kHelAckNack, _irqSequence));
+				continue;
+			}
+			assert(!(alt_status & (kStatusErr | kStatusDf)));
+
+			// Clear and acknowledge the IRQ.
+			auto status = _ioSpace.load(regs::inStatus);
+			HEL_CHECK(helAcknowledgeIrq(_irq.getHandle(), kHelAckAcknowledge, _irqSequence));
+			assert(!(status & (kStatusErr | kStatusDf)));
+			if(!(status & kStatusRdy))
+				std::cout << "\e[31m" "block/ata: RDY is not set after IRQ" "\e[39m" << std::endl;
+			if(!(status & kStatusDrq))
+				std::cout << "\e[31m" "block/ata: DRQ is not set after IRQ" "\e[39m" << std::endl;
+			if((status & kStatusRdy) && (status & kStatusDrq))
+				break;
+		}
+
+		// Read the data.
+		auto dest = reinterpret_cast<uint8_t *>(request->buffer) + k * 512;
+		for(int i = 0; i < 256; i++) {
+			// TODO: Be careful with endianess here.
+			uint16_t data = _ioSpace.load(regs::inData);
+			memcpy(dest + 2 * i, &data, sizeof(uint16_t));
+		}
+	}
+
+	if(logRequests)
+		std::cout << "block/ata: Reading from " << request->sector
+				<< " complete" << std::endl;
+}))
 
 Controller globalController;
 
@@ -214,7 +208,7 @@ Controller globalController;
 // --------------------------------------------------------
 
 int main() {
-	printf("block-ata: Starting driver\n");
+	printf("block/ata: Starting driver\n");
 
 	{
 		async::queue_scope scope{helix::globalQueue()};
