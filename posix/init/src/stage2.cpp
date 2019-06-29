882e95a7501629ef1dac0c37465e7e67964407e5
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-06-29 10:53:14 +0200

    init: Split into two stages

--- posix/init/src/stage2.cpp
+++ posix/init/src/stage2.cpp
@@ -1,82 +1,16 @@
 
 #include <assert.h>
-#include <fcntl.h>
-#include <sched.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <sys/mman.h>
-#include <sys/mount.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <iostream>
 
+// This second stages assumes that:
+// - stdin, stdout and stderr are already set up correctly
+// - the real file system is mounted to /
+
 int main() {
-	int fd = open("/dev/helout", O_WRONLY);
-	dup2(fd, STDOUT_FILENO);
-	dup2(fd, STDERR_FILENO);
-	printf("Starting posix-init\n");
-
-	// Start essential bus and storage drivers.
-	auto ehci = fork();
-	if(!ehci) {
-		execl("/bin/runsvr", "/bin/runsvr", "runsvr", "/sbin/ehci", nullptr);
-	}else assert(ehci != -1);
-
-	auto virtio = fork();
-	if(!virtio) {
-		execl("/bin/runsvr", "/bin/runsvr", "runsvr", "/sbin/virtio-block", nullptr);
-	}else assert(virtio != -1);
-
-/*
-	auto block_ata = fork();
-	if(!block_ata) {
-		execl("/bin/runsvr", "/bin/runsvr", "runsvr", "/sbin/block-ata", nullptr);
-	}else assert(block_ata != -1);
-*/
-
-	auto block_usb = fork();
-	if(!block_usb) {
-		execl("/bin/runsvr", "/bin/runsvr", "runsvr", "/sbin/storage", nullptr);
-	}else assert(block_usb != -1);
-
-	// Spin until /dev/sda0 becomes available. Then mount the rootfs and prepare it.
-	while(access("/dev/sda0", F_OK)) {
-		assert(errno == ENOENT);
-		std::cout << "Waiting for /dev/sda0" << std::endl;
-		sleep(1);
-	}
-
-	// Hack: Start UHCI only after EHCI devices are ready.
-	auto uhci = fork();
-	if(!uhci) {
-		execl("/bin/runsvr", "/bin/runsvr", "runsvr", "/sbin/uhci", nullptr);
-	}else assert(uhci != -1);
-
-	printf("init: Mounting /dev/sda0\n");
-	if(mount("/dev/sda0", "/realfs", "ext2", 0, ""))
-		throw std::runtime_error("mount() failed");
-
-	if(mount("", "/realfs/sys", "sysfs", 0, ""))
-		throw std::runtime_error("mount() failed");
-	if(mount("", "/realfs/dev", "devtmpfs", 0, ""))
-		throw std::runtime_error("mount() failed");
-	if(mount("", "/realfs/run", "tmpfs", 0, ""))
-		throw std::runtime_error("mount() failed");
-
-	if(mkdir("/dev/pts", 0620))
-		throw std::runtime_error("mkdir() failed");
-	if(mount("", "/realfs/dev/pts", "devpts", 0, ""))
-		throw std::runtime_error("mount() failed");
-
-	if(chroot("/realfs"))
-		throw std::runtime_error("chroot() failed");
-	// Some programs, e.g. bash with its builtin getcwd() cannot deal with CWD outside of /.
-	if(chdir("/"))
-		throw std::runtime_error("chdir() failed");
-
-	std::cout << "init: On /realfs" << std::endl;
+	std::cout << "init: Entering stage2" << std::endl;
 
 	auto upload = [] (const char *name) {
 		auto svrctl = fork();
@@ -161,8 +95,10 @@ int main() {
 		putenv("MESA_GLSL_CACHE_DISABLE=1");
 //		putenv("MESA_DEBUG=1");
 
-		execl("/usr/bin/weston", "weston", nullptr);
+		//execl("/usr/bin/weston", "weston", nullptr);
 		//execl("/usr/bin/weston", "weston", "--use-pixman", nullptr);
+		execl("/usr/bin/kmscon", "kmscon", nullptr);
+		//execl("/usr/bin/kmscon", "kmscon", "--debug", "--verbose", nullptr);
 		//execl("/usr/bin/kmscube", "kmscube", nullptr);
 	}else assert(modeset != -1);
 
