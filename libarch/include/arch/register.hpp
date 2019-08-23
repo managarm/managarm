08ad46f0ca5ca6447913eb33a887517157f541e5
Kacper Słomiński <kacper.slominski72@gmail.com>
2019-08-23 19:58:14 +0200

    libarch: add freestanding scalar_load and scalar_store

--- libarch/include/arch/register.hpp
+++ libarch/include/arch/register.hpp
@@ -30,6 +30,18 @@ using scalar_register = basic_register<T, T, P>;
 template<typename B, typename P = ptrdiff_t>
 using bit_register = basic_register<bit_value<B>, B, P>;
 
+// Space is second because otherwise you can't do scalar_load<uint32_t> etc
+template<typename T, typename Space>
+T scalar_load(Space &s, ptrdiff_t offset) {
+	return s.load(scalar_register<T>(offset));
+}
+
+// Same as above
+template<typename T, typename Space>
+void scalar_store(Space &s, ptrdiff_t offset, T val) {
+	return s.store(scalar_register<T>(offset), val);
+}
+
 } // namespace arch
 
 #endif // LIBARCH_REGISTER_HPP
