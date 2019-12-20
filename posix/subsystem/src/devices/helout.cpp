b1be946ac157a980947eaced20c85472598b939c
Kacper Słomiński <kacper.slominski72@gmail.com>
2019-12-20 23:43:59 +0100

    posix/subsystem: switch devices/helout to C++20 coroutines

--- posix/subsystem/src/devices/helout.cpp
+++ posix/subsystem/src/devices/helout.cpp
@@ -4,28 +4,32 @@
 #include "../common.hpp"
 #include "helout.hpp"
 
+#include <experimental/coroutine>
+
 HelHandle __mlibc_getPassthrough(int fd);
 
 namespace {
 
 struct HeloutFile final : File {
 private:
-	COFIBER_ROUTINE(expected<size_t>,
-	readSome(Process *, void *data, size_t max_length) override, ([=] {
+	expected<size_t> readSome(Process *, void *data, size_t max_length) override {
 		(void)data;
 		(void)max_length;
 		assert(!"Not implemented");
-	}))
+		__builtin_unreachable();
+	}
 	
-	COFIBER_ROUTINE(expected<PollResult>, poll(Process *, uint64_t sequence,
-			async::cancellation_token) override, ([=] {
+	expected<PollResult> poll(Process *, uint64_t sequence, async::cancellation_token) override {
 		// TODO: Signal that we are ready to accept output.
 		std::cout << "posix: poll() on helout" << std::endl;
 		if(!sequence) {
 			PollResult result{1, 0, 0};
-			COFIBER_RETURN(result);
+			co_return result;
 		}
-	}))
+
+		co_await std::experimental::suspend_always{};
+		__builtin_unreachable();
+	}
 
 	helix::BorrowedDescriptor getPassthroughLane() override {
 		return helix::BorrowedDescriptor(__mlibc_getPassthrough(1));
@@ -47,14 +51,13 @@ struct HeloutDevice final : UnixDevice {
 		return "helout";
 	}
 	
-	COFIBER_ROUTINE(FutureMaybe<SharedFilePtr>,
-	open(std::shared_ptr<MountView> mount, std::shared_ptr<FsLink> link,
-			SemanticFlags semantic_flags) override, ([=] {
+	FutureMaybe<SharedFilePtr> open(std::shared_ptr<MountView> mount, std::shared_ptr<FsLink> link,
+			SemanticFlags semantic_flags) override {
 		assert(!semantic_flags);
 		auto file = smarter::make_shared<HeloutFile>(std::move(mount), std::move(link));
 		file->setupWeakFile(file);
-		COFIBER_RETURN(File::constructHandle(std::move(file)));
-	}))
+		co_return File::constructHandle(std::move(file));
+	}
 };
 
 } // anonymous namespace
