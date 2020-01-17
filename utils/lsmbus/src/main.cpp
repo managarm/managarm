2268f06b20f5c1605c226bc5a4b987d94766d370
Arsen <arsenarsentmc@outlook.com>
2020-01-17 18:52:00 +0100

    utils: add lsmbus: a tool to enumerate mbus

---
+++ utils/lsmbus/src/main.cpp
@@ -0,0 +1,45 @@
+#include <typeinfo>
+#include <type_traits>
+#include <iostream>
+
+#include <protocols/mbus/client.hpp>
+
+namespace {
+struct PrintVisitor {
+	void operator()(mbus::StringItem &item) {
+		std::cout << item.value;
+	}
+
+	template<typename T>
+	void operator()(T&& val) {
+		std::cout << "WARNING: Unimplemented type: " << typeid(std::decay_t<T>).name();
+	}
+};
+
+async::detached enumerateBus() {
+	auto root = co_await mbus::Instance::global().getRoot();
+
+	auto filter = mbus::Conjunction({});
+
+	auto handler = mbus::ObserverHandler {}
+	.withAttach([] (mbus::Entity, mbus::Properties props) {
+		std::cout << "found mbus entry:\n";
+		for (auto &[name, value] : props) {
+			std::cout << "\tproperty: \"" << name << "\": ";
+			std::visit(PrintVisitor {}, value);
+			std::cout << std::endl;
+		}
+	});
+
+	co_await root.linkObserver(std::move(filter), std::move(handler));
+}
+}
+
+int main() {
+	{
+		async::queue_scope scope { helix::globalQueue() };
+		enumerateBus();
+	}
+
+	helix::globalQueue()->run();
+}
