26311deef5f204acba6ad61bf81e01927ffedca7
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2020-03-26 20:22:50 +0100

    init: Set HOME

--- posix/init/src/stage2.cpp
+++ posix/init/src/stage2.cpp
@@ -128,6 +128,7 @@ int main() {
 	if(!desktop) {
 //		putenv("MLIBC_DEBUG_MALLOC=1");
 		putenv("PATH=/usr/local/bin:/usr/bin:/bin");
+		putenv("HOME=/root");
 		putenv("XDG_RUNTIME_DIR=/run");
 		putenv("MESA_GLSL_CACHE_DISABLE=1");
 //		putenv("MESA_DEBUG=1");
