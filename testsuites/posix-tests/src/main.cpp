1b93aeafd12cd865d2afe4f78f5c49e2c874afbc
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2020-04-29 19:46:38 +0200

    testsuites: Fix main.cpp in all testsuites

--- testsuites/posix-tests/src/main.cpp
+++ testsuites/posix-tests/src/main.cpp
@@ -3,14 +3,17 @@
 
 #include "testsuite.hpp"
 
-static std::vector<abstract_test_case *> test_case_ptrs;
+std::vector<abstract_test_case *> &test_case_ptrs() {
+	static std::vector<abstract_test_case *> singleton;
+	return singleton;
+}
 
 void abstract_test_case::register_case(abstract_test_case *tcp) {
-	test_case_ptrs.push_back(tcp);
+	test_case_ptrs().push_back(tcp);
 }
 
 int main() {
-	for(abstract_test_case *tcp : test_case_ptrs) {
+	for(abstract_test_case *tcp : test_case_ptrs()) {
 		std::cout << "posix-tests: Running " << tcp->name() << std::endl;
 		tcp->run();
 	}
--- testsuites/posix-torture/src/main.cpp
+++ testsuites/posix-torture/src/main.cpp
@@ -3,16 +3,19 @@
 
 #include "testsuite.hpp"
 
-static std::vector<abstract_test_case *> test_case_ptrs;
+std::vector<abstract_test_case *> &test_case_ptrs() {
+	static std::vector<abstract_test_case *> singleton;
+	return singleton;
+}
 
 void abstract_test_case::register_case(abstract_test_case *tcp) {
-	test_case_ptrs.push_back(tcp);
+	test_case_ptrs().push_back(tcp);
 }
 
 int main() {
 	for(int s = 10; s < 24; s++) {
 		int n = 1 << s;
-		for(abstract_test_case *tcp : test_case_ptrs) {
+		for(abstract_test_case *tcp : test_case_ptrs()) {
 			std::cout << "posix-torture: Running " << tcp->name()
 					<< " for " << n << " iterations" << std::endl;
 			for(int i = 0; i < n; i++)
