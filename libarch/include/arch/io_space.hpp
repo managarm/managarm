369c72de24ef9b776d65d8090098506d8b7db8b4
Kacper Słomiński <kacper.slominski72@gmail.com>
2020-05-17 18:37:51 +0200

    Switch to managarm/libarch instead of in-tree libarch

--- libarch/include/arch/io_space.hpp
+++
@@ -1,7 +0,0 @@
-
-#if defined(__i386__) || defined(__x86_64__)
-#	include<arch/x86/io_space.hpp>
-#else
-#	error Unsupported architecture
-#endif
-
--- libarch/include/arch/mem_space.hpp
+++
@@ -1,7 +0,0 @@
-
-#if defined(__i386__) || defined(__x86_64__)
-#	include<arch/x86/mem_space.hpp>
-#else
-#	error Unsupported architecture
-#endif
-
