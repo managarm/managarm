a08bb98f2ff5783723f2e1584c9474a5121a84f8
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2020-05-27 18:38:18 +0200

    drivers/virtio-console: Dump kernel-profile ring

--- drivers/tty/virtio-console/src/console.cpp
+++ drivers/tty/virtio-console/src/console.cpp
@@ -8,12 +8,7 @@
 #include <protocols/mbus/client.hpp>
 #include <kerncfg.pb.h>
 
-namespace {
-	helix::UniqueLane kerncfgByteRingLane;
-	async::jump foundKerncfgByteRing;
-}
-
-async::result<void> enumerateKerncfgByteRing(const char *purpose) {
+async::result<helix::UniqueLane> enumerateKerncfgByteRing(const char *purpose) {
 	auto root = co_await mbus::Instance::global().getRoot();
 
 	auto filter = mbus::Conjunction({
@@ -21,28 +16,32 @@ async::result<void> enumerateKerncfgByteRing(const char *purpose) {
 		mbus::EqualsFilter("purpose", purpose)
 	});
 
-	auto handler = mbus::ObserverHandler{}
-	.withAttach([] (mbus::Entity entity, mbus::Properties properties) -> async::detached {
-		std::cout << "virtio-console: Found kerncfg" << std::endl;
+	// TODO: this is quite dirty and we are leaking this async::promise.
+	auto p = new async::promise<helix::UniqueLane>{};
 
-		kerncfgByteRingLane = helix::UniqueLane(co_await entity.bind());
-		foundKerncfgByteRing.trigger();
+	auto handler = mbus::ObserverHandler{}
+	.withAttach([p] (mbus::Entity entity,
+			mbus::Properties properties) mutable -> async::detached {
+		std::cout << "virtio-console: Found kerncfg" << std::endl;
+		p->set_value(helix::UniqueLane(co_await entity.bind()));
 	});
 
 	co_await root.linkObserver(std::move(filter), std::move(handler));
-	co_await foundKerncfgByteRing.async_wait();
+	co_return co_await p->async_get();
 }
 
 async::result<std::tuple<size_t, uint64_t, uint64_t>>
-getKerncfgByteRingPart(arch::dma_buffer_view chunk, uint64_t dequeue) {
+getKerncfgByteRingPart(helix::BorrowedLane lane,
+		arch::dma_buffer_view chunk, uint64_t dequeue, uint64_t watermark) {
 	managarm::kerncfg::CntRequest req;
 	req.set_req_type(managarm::kerncfg::CntReqType::GET_BUFFER_CONTENTS);
+	req.set_watermark(watermark);
 	req.set_size(chunk.size());
 	req.set_dequeue(dequeue);
 
 	auto ser = req.SerializeAsString();
 	auto [offer, send_req, recv_resp, recv_buffer] =
-		co_await helix_ng::exchangeMsgs(kerncfgByteRingLane,
+		co_await helix_ng::exchangeMsgs(lane,
 			helix_ng::offer(
 				helix_ng::sendBuffer(ser.data(), ser.size()),
 				helix_ng::recvInline(),
@@ -82,29 +81,36 @@ async::detached Device::runDevice() {
 
 	transport_->runDevice();
 
-	co_await enumerateKerncfgByteRing("heap-trace");
+	auto dumpKerncfgRing = [this] (const char *name, size_t watermark) -> async::result<void> {
+		auto lane = co_await enumerateKerncfgByteRing(name);
 
-	uint64_t dequeue = 0;
+		uint64_t dequeue = 0;
 
-	constexpr size_t chunk_size = 64 * 1024;
-	arch::dma_buffer chunk_buffer{&dmaPool_, chunk_size};
+		arch::dma_buffer chunkBuffer{&dmaPool_, 1 << 16};
 
-	while (true) {
-		auto [size, new_dequeue, enqueue] = co_await getKerncfgByteRingPart(chunk_buffer, dequeue);
-		if (!size)
-			continue;
+		while (true) {
+			auto [size, newDequeue, enqueue] = co_await getKerncfgByteRingPart(
+					lane, chunkBuffer, dequeue, watermark);
+			if (!size)
+				continue;
 
-		if (dequeue + (256 * 1024 * 1024) < enqueue)
-			std::cerr << "virtio-console: warning, we possibly missed "
-				<< (enqueue - dequeue + (256 * 1024 * 1024)) << " bytes" << std::endl;
+			// TODO: improve this by passing the "true" dequeue pointer to userspace.
+			if (dequeue + (256 * 1024 * 1024) < enqueue)
+				std::cerr << "virtio-console: warning, we possibly missed "
+					<< (enqueue - dequeue + (256 * 1024 * 1024)) << " bytes" << std::endl;
 
-		dequeue = new_dequeue;
+			dequeue = newDequeue;
 
-		virtio_core::Chain chain;
-		chain.append(co_await txQueue_->obtainDescriptor());
-		chain.setupBuffer(virtio_core::hostToDevice, chunk_buffer.subview(0, size));
-		co_await txQueue_->submitDescriptor(chain.front());
-	}
+			virtio_core::Chain chain;
+			chain.append(co_await txQueue_->obtainDescriptor());
+			chain.setupBuffer(virtio_core::hostToDevice, chunkBuffer.subview(0, size));
+			co_await txQueue_->submitDescriptor(chain.front());
+		}
+	};
+
+	async::detach(dumpKerncfgRing("heap-trace", 1024 * 1024));
+	async::detach(dumpKerncfgRing("kernel-profile", 1024));
+	co_return;
 }
 
 } } // namespace tty::virtio_console
