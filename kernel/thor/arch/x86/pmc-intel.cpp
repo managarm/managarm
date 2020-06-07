6fe2b29271f060cb6f7a251cc2d4ca8419969418
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2020-06-07 15:03:32 +0200

    thor: Count clock cycles (not isns) for Intel PMCs

--- kernel/thor/arch/x86/pmc-intel.cpp
+++ kernel/thor/arch/x86/pmc-intel.cpp
@@ -3,11 +3,25 @@
 
 namespace thor {
 
+enum class IntelCounter {
+	none,
+	fixed0, // Instructions retired.
+	fixed1 // Clock cycles.
+};
+
+static IntelCounter whichCounter = IntelCounter::fixed1;
+
 void initializeIntelPmc() {
+	// Disable all fixed performance counters.
+	frigg::arch_x86::wrmsr(0x38D, // PERF_FIXED_CTR_CTRL
+		0
+	);
+
 	// Counters first need to be enabled in the "global control" MSR.
 	frigg::arch_x86::wrmsr(0x38F, // PERF_GLOBAL_CTRL
 			frigg::arch_x86::rdmsr(0x38F)
-			| (UINT64_C(1) << 32));
+			| (UINT64_C(1) << 32)
+			| (UINT64_C(1) << 33));
 }
 
 void setIntelPmc() {
@@ -17,8 +31,15 @@ void setIntelPmc() {
 	);
 
 	// Program the initial value.
-	frigg::arch_x86::wrmsr(0x309, // PERF_FIXED_CTR0
-			(UINT64_C(1) << 48) - 0x1000);
+	// This is hardcoded to yield a fixed number of events per second on a 1 GHz machine for now.
+	if(whichCounter == IntelCounter::fixed0) {
+		frigg::arch_x86::wrmsr(0x309, // PERF_FIXED_CTR0
+				(UINT64_C(1) << 48) - 1'000'000'000/5000);
+	}else{
+		assert(whichCounter == IntelCounter::fixed1);
+		frigg::arch_x86::wrmsr(0x30A, // PERF_FIXED_CTR1
+				(UINT64_C(1) << 48) - 1'000'000'000/5000);
+	}
 
 	// TODO: Clear overflow. This is required for real hardware.
 	//frigg::arch_x86::wrmsr(0x390, // PERF_GLOBAL_OVF_CTRL
@@ -28,15 +49,31 @@ void setIntelPmc() {
 
 	// Enable the performance counter.
 	// KVM requires this MSR write to happen *after* the initial value is set.
-	frigg::arch_x86::wrmsr(0x38D, // PERF_FIXED_CTR_CTRL
-			UINT64_C(3) // User + supervisor mode.
-			| UINT64_C(8) // Enable PMI
-	);
+	if(whichCounter == IntelCounter::fixed0) {
+		frigg::arch_x86::wrmsr(0x38D, // PERF_FIXED_CTR_CTRL
+				(UINT64_C(3) << 0) // User + supervisor mode for PERF_FIXED_CTR0
+				| (UINT64_C(1) << 3) // Enable PMI for PREF_FIXED_CTR0
+		);
+	}else{
+		assert(whichCounter == IntelCounter::fixed1);
+		frigg::arch_x86::wrmsr(0x38D, // PERF_FIXED_CTR_CTRL
+				(UINT64_C(3) << 4) // User + supervisor mode for PERF_FIXED_CTR1
+				| (UINT64_C(1) << 7) // Enable PMI for PREF_FIXED_CTR1
+		);
+	}
 }
 
 bool checkIntelPmcOverflow() {
-	return frigg::arch_x86::rdmsr(0x38E) // PERF_GLOBAL_STATUS
-			& (UINT64_C(1) << 32);
+	if(whichCounter == IntelCounter::fixed0) {
+		frigg::arch_x86::wrmsr(0x309, // PERF_FIXED_CTR0
+				(UINT64_C(1) << 48) - 1'000'000'000/5000);
+		return frigg::arch_x86::rdmsr(0x38E) // PERF_GLOBAL_STATUS
+				& (UINT64_C(1) << 32); // Overflow of PERF_FIXED_CTR0
+	}else{
+		assert(whichCounter == IntelCounter::fixed1);
+		return frigg::arch_x86::rdmsr(0x38E) // PERF_GLOBAL_STATUS
+				& (UINT64_C(1) << 33); // Overflow of PERF_FIXED_CTR1
+	}
 }
 
 } // namespace thor
