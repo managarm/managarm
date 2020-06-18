36efcbd78076e3535cc19757c8b2b9a14f0e6032
Dennisbonke <admin@dennisbonke.com>
2020-06-18 21:53:35 +0200

    posix: Convert GET_EUID to bragi
    Signed-off-by: Dennisbonke <admin@dennisbonke.com>

--- posix/subsystem/src/main.cpp
+++ posix/subsystem/src/main.cpp
@@ -662,20 +662,26 @@ async::result<void> serveRequests(std::shared_ptr<Process> self,
 			} else {
 				co_await sendErrorResponse(managarm::posix::Errors::SUCCESS);
 			}
-		}else if(req.request_type() == managarm::posix::CntReqType::GET_EUID) {
+		}else if(preamble.id() == managarm::posix::GetEuidRequest::message_id) {
+			auto req = bragi::parse_head_only<managarm::posix::GetEuidRequest>(recv_head);
+
+			if (!req) {
+				std::cout << "posix: Rejecting request due to decoding failure" << std::endl;
+				break;
+			}
+
 			if(logRequests)
 				std::cout << "posix: GET_EUID" << std::endl;
-
-			helix::SendBuffer send_resp;
 
 			managarm::posix::SvrResponse resp;
 			resp.set_error(managarm::posix::Errors::SUCCESS);
 			resp.set_uid(self->euid());
 
-			auto ser = resp.SerializeAsString();
-			auto &&transmit = helix::submitAsync(conversation, helix::Dispatcher::global(),
-					helix::action(&send_resp, ser.data(), ser.size()));
-			co_await transmit.async_wait();
+			auto [send_resp] = co_await helix_ng::exchangeMsgs(
+					conversation,
+					helix_ng::sendBragiHeadOnly(resp, frg::stl_allocator{})
+				);
+
 			HEL_CHECK(send_resp.error());
 		}else if(req.request_type() == managarm::posix::CntReqType::SET_EUID) {
 			if(logRequests)
