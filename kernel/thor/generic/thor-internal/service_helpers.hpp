030010e122e2c81a59e6d7d001ac60f30fff0666
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2020-07-05 15:33:50 +0200

    thor: Remove obsolete fiber I/O functions

--- kernel/thor/generic/thor-internal/service_helpers.hpp
+++ kernel/thor/generic/thor-internal/service_helpers.hpp
@@ -8,15 +8,4 @@ namespace thor {
 void fiberCopyToBundle(MemoryView *bundle, ptrdiff_t offset, const void *pointer, size_t size);
 void fiberCopyFromBundle(MemoryView *bundle, ptrdiff_t offset, void *pointer, size_t size);
 
-void fiberSleep(uint64_t nanos);
-
-LaneHandle fiberOffer(LaneHandle lane);
-LaneHandle fiberAccept(LaneHandle lane);
-
-void fiberSend(LaneHandle lane, const void *buffer, size_t length);
-frigg::UniqueMemory<KernelAlloc> fiberRecv(LaneHandle lane);
-
-void fiberPushDescriptor(LaneHandle lane, AnyDescriptor descriptor);
-AnyDescriptor fiberPullDescriptor(LaneHandle lane);
-
 } // namespace thor
