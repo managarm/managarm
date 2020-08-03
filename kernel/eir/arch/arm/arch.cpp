cad42771fcb5454aa01c7967e27e88fb79a69c1c
Kacper Słomiński <kacper.slominski72@gmail.com>
2020-08-03 18:02:19 +0200

    kernel/eir: implement arm paging functions

--- kernel/eir/arch/arm/arch.cpp
+++ kernel/eir/arch/arm/arch.cpp
@@ -1,18 +1,179 @@
 #include <eir-internal/arch.hpp>
 #include <eir-internal/generic.hpp>
 #include <eir-internal/debug.hpp>
+#include <frigg/debug.hpp>
 
 namespace eir {
 
-void setupPaging() {}
+uintptr_t eirTTBR[2];
+
+void setupPaging() {
+	eirTTBR[0] = allocPage();
+	eirTTBR[1] = allocPage();
+
+	for (size_t i = 0; i < 512; i++) {
+		((uint64_t *)eirTTBR[0])[i] = 0;
+		((uint64_t *)eirTTBR[1])[i] = 0;
+	}
+}
+
+static inline constexpr uint64_t kPageValid = 1;
+static inline constexpr uint64_t kPageTable = (1 << 1);
+static inline constexpr uint64_t kPageL3Page = (1 << 1);
+static inline constexpr uint64_t kPageXN = (uint64_t(1) << 54);
+static inline constexpr uint64_t kPagePXN = (uint64_t(1) << 53);
+static inline constexpr uint64_t kPageNotGlobal = (1 << 11);
+static inline constexpr uint64_t kPageAccess = (1 << 10);
+static inline constexpr uint64_t kPageRO = (1 << 7);
+static inline constexpr uint64_t kPageInnerSh = (3 << 8);
+static inline constexpr uint64_t kPageOuterSh = (2 << 8);
+static inline constexpr uint64_t kPageWb = (0 << 2);
+static inline constexpr uint64_t kPageGRE = (1 << 2);
 
 void mapSingle4kPage(address_t address, address_t physical, uint32_t flags,
-		CachingMode caching_mode) {}
+		CachingMode caching_mode) {
+	auto ttbr = (address >> 63) & 1;
+	auto l0 = (address >> 39) & 0x1FF;
+	auto l1 = (address >> 30) & 0x1FF;
+	auto l2 = (address >> 21) & 0x1FF;
+	auto l3 = (address >> 12) & 0x1FF;
 
-address_t getSingle4kPage(address_t address) { return -1; }
+	auto l0_ent = ((uint64_t *)eirTTBR[ttbr])[l0];
+	auto l1_ptr = l0_ent & 0xFFFFFFFFF000;
+	if (!(l0_ent & kPageValid)) {
+		uint64_t addr = allocPage();
+
+		for(int i = 0; i < 512; i++)
+			((uint64_t *)addr)[i] = 0;
+
+		((uint64_t *)eirTTBR[ttbr])[l0] =
+			addr | kPageValid | kPageTable;
+
+		l1_ptr = addr;
+	}
+
+	auto l1_ent = ((uint64_t *)l1_ptr)[l1];
+	auto l2_ptr = l1_ent & 0xFFFFFFFFF000;
+	if (!(l1_ent & kPageValid)) {
+		uint64_t addr = allocPage();
+
+		for(int i = 0; i < 512; i++)
+			((uint64_t *)addr)[i] = 0;
+
+		((uint64_t *)l1_ptr)[l1] =
+			addr | kPageValid | kPageTable;
+
+		l2_ptr = addr;
+	}
+
+	auto l2_ent = ((uint64_t *)l2_ptr)[l2];
+	auto l3_ptr = l2_ent & 0xFFFFFFFFF000;
+	if (!(l2_ent & kPageValid)) {
+		uint64_t addr = allocPage();
+
+		for(int i = 0; i < 512; i++)
+			((uint64_t *)addr)[i] = 0;
+
+		((uint64_t *)l2_ptr)[l2] =
+			addr | kPageValid | kPageTable;
+
+		l3_ptr = addr;
+	}
+
+	auto l3_ent = ((uint64_t *)l3_ptr)[l3];
+
+	if (l3_ent & kPageValid)
+		eir::panicLogger() << "eir: Trying to map 0x" << frg::hex_fmt{address}
+				<< " twice!" << frg::endlog;
+
+	uint64_t new_entry = physical | kPageValid | kPageL3Page | kPageAccess;
+
+	if (!(flags & PageFlags::write))
+		new_entry |= kPageRO;
+	if (!(flags & PageFlags::execute))
+		new_entry |= kPageXN | kPagePXN;
+	if (!(flags & PageFlags::global))
+		new_entry |= kPageNotGlobal;
+
+	if (caching_mode == CachingMode::writeCombine)
+		new_entry |= kPageGRE | kPageOuterSh;
+	else {
+		assert(caching_mode == CachingMode::null);
+		new_entry |= kPageWb | kPageInnerSh;
+	}
+
+	if (new_entry & (0b111ULL << 48)) {
+		eir::infoLogger() << "Oops, reserved bits set when mapping 0x"
+			<< frg::hex_fmt{physical} << " to 0x" << frg::hex_fmt{address}
+			<< frg::endlog;
+
+		eir::panicLogger() << "New entry value: 0x" << frg::hex_fmt{new_entry} << frg::endlog;
+	}
+
+	((uint64_t *)l3_ptr)[l3] = new_entry;
+}
+
+address_t getSingle4kPage(address_t address) {
+	auto ttbr = (address >> 63) & 1;
+	auto l0 = (address >> 39) & 0x1FF;
+	auto l1 = (address >> 30) & 0x1FF;
+	auto l2 = (address >> 21) & 0x1FF;
+	auto l3 = (address >> 12) & 0x1FF;
+
+	auto l0_ent = ((uint64_t *)eirTTBR[ttbr])[l0];
+	auto l1_ptr = l0_ent & 0xFFFFFFFFF000;
+	if (!(l0_ent & kPageValid))
+		return -1;
+
+	auto l1_ent = ((uint64_t *)l1_ptr)[l1];
+	auto l2_ptr = l1_ent & 0xFFFFFFFFF000;
+	if (!(l1_ent & kPageValid))
+		return -1;
+
+	auto l2_ent = ((uint64_t *)l2_ptr)[l2];
+	auto l3_ptr = l2_ent & 0xFFFFFFFFF000;
+	if (!(l2_ent & kPageValid))
+		return -1;
+
+	auto l3_ent = ((uint64_t *)l3_ptr)[l3];
+	auto page_ptr = l3_ent & 0xFFFFFFFFF000;
+	if (!(l3_ent & kPageValid))
+		return -1;
+
+	return page_ptr;
+}
 
 void initProcessorEarly() {
 	eir::infoLogger() << "Starting Eir" << frg::endlog;
+
+	uint64_t aa64mmf0;
+	asm volatile ("mrs %0, id_aa64mmfr0_el1" : "=r" (aa64mmf0));
+
+	if (aa64mmf0 & (0xF << 28))
+		eir::panicLogger() << "PANIC! This CPU doesn't support 4K memory translation granules" << frg::endlog;
+
+	if (!((aa64mmf0 & 0xF) < 5))
+		eir::panicLogger() << "PANIC! This CPU doesn't support at least 48 bit physical addresses" << frg::endlog;
+
+	uint64_t mair =
+		0b11111111 | // Normal, Write-back RW-Allocate non-transient
+		(0b00001100 << 8); // Device, GRE
+
+	asm volatile ("msr mair_el1, %0" :: "r" (mair));
+
+	uint64_t tcr =
+		(16 << 0) | // T0SZ=16
+		(16 << 16) | // T1SZ=16
+		(1 << 8) | // TTRB0 Inner WB RW-Allocate
+		(1 << 10) | // TTRB0 Outer WB RW-Allocate
+		(1 << 24) | // TTRB1 Inner WB RW-Allocate
+		(1 << 26) | // TTRB1 Outer WB RW-Allocate
+		(3 << 12) | // TTRB0 Inner shareable
+		(3 << 28) | // TTRB1 Inner shareable
+		(uint64_t(5) << 32) | // 48-bit intermediate address
+		(2 << 30); // TTRB1 4K granule
+
+	asm volatile ("msr tcr_el1, %0" :: "r" (tcr));
 }
 
 // Returns Core region index
--- kernel/eir/arch/arm/eir-internal/arch/types.hpp
+++ kernel/eir/arch/arm/eir-internal/arch/types.hpp
@@ -6,5 +6,7 @@ namespace eir {
 
 using address_t = uint64_t;
 
+extern uintptr_t eirTTBR[2];
+
 } // namespace eir
 
