f0fc6c71922071385e73d7c2d0ef26152c613255
Dennis Bonke <admin@dennisbonke.com>
2021-05-01 22:41:12 +0200

    drivers/gfx/plainfb: Fix several unused parameter warnings
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- drivers/gfx/plainfb/src/main.cpp
+++ drivers/gfx/plainfb/src/main.cpp
@@ -85,7 +85,7 @@ std::unique_ptr<drm_core::Configuration> GfxDevice::createConfiguration() {
 }
 
 std::shared_ptr<drm_core::FrameBuffer> GfxDevice::createFrameBuffer(std::shared_ptr<drm_core::BufferObject> base_bo,
-		uint32_t width, uint32_t height, uint32_t format, uint32_t pitch) {
+		uint32_t width, uint32_t height, uint32_t, uint32_t pitch) {
 	auto bo = std::static_pointer_cast<GfxDevice::BufferObject>(base_bo);
 		
 	assert(pitch % 4 == 0);
@@ -387,7 +387,7 @@ async::detached observeControllers() {
 	});
 
 	auto handler = mbus::ObserverHandler{}
-	.withAttach([] (mbus::Entity entity, mbus::Properties properties) {
+	.withAttach([] (mbus::Entity entity, mbus::Properties) {
 		std::cout << "gfx/plainfb: Detected device" << std::endl;
 		bindController(std::move(entity));
 	});
