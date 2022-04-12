88c38b7ceb977b2df4d3aaccbdc4325e78b1d1df
YusufKhan-human <54283715+YusufKhan-gamedev@users.noreply.github.com>
2022-04-12 16:39:39 -0500

    drivers: rtl8139: remove LGPL Liscence

--- drivers/nic/rtl8139/src/rtl8139.cpp
+++ drivers/nic/rtl8139/src/rtl8139.cpp
@@ -1,23 +1,3 @@
-// SPDX-License-Identifier:  LGPL-3.0-or-later
-/*
- * Copyright 2022 Yusuf Khan
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You can find a copy of the GNU Lesser General Public License at
- * https://www.gnu.org/licenses/ or write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
- *
- */
-
 #include <nic/rtl8139/rtl8139.hpp>
 #include <arch/io_space.hpp>
 #include <arch/dma_pool.hpp>
