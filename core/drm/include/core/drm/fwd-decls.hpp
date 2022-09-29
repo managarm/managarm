57a3dccc265faa4bf100defea44f4444f8299ce3
no92 <no92.mail@gmail.com>
2022-09-29 20:45:33 +0200

    core/drm: move forward declarations to separate header

---
+++ core/drm/include/core/drm/fwd-decls.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace drm_core {
+	struct Blob;
+	struct BufferObject;
+	struct Connector;
+	struct ConnectorState;
+	struct Crtc;
+	struct CrtcState;
+	struct Encoder;
+	struct FrameBuffer;
+	struct ModeObject;
+	struct Plane;
+	struct PlaneState;
+	struct Configuration;
+	struct Device;
+	struct Assignment;
+	struct AtomicState;
+	struct Property;
+}
