62229934f3aecbb44497da2e1dea884cf563bb7c
no92 <no92.mail@gmail.com>
2023-01-24 11:41:30 +0100

    posix: catch invalid address lengths in UNIX socket's bind/connect

--- posix/subsystem/src/un-socket.cpp
+++ posix/subsystem/src/un-socket.cpp
@@ -1,5 +1,6 @@
 
-#include <string.h>
+#include <cstddef>
+#include <cstring>
 #include <sys/epoll.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
@@ -315,6 +316,10 @@ public:
 
 	async::result<protocols::fs::Error>
 	bind(Process *process, const void *addr_ptr, size_t addr_length) override {
+		if(addr_length <= offsetof(struct sockaddr_un, sun_path)) {
+			co_return protocols::fs::Error::illegalArguments;
+		}
+
 		// Create a new socket node in the FS.
 		struct sockaddr_un sa;
 		assert(addr_length <= sizeof(struct sockaddr_un));
@@ -374,7 +379,9 @@ public:
 		memcpy(&sa, addr_ptr, addr_length);
 		std::string path;
 
-		if(sa.sun_path[0] == '\0') {
+		if(addr_length <= offsetof(struct sockaddr_un, sun_path)) {
+			co_return protocols::fs::Error::illegalArguments;
+		} else if(sa.sun_path[0] == '\0') {
 			path.resize(addr_length - sizeof(sa.sun_family) - 1);
 			memcpy(path.data(), sa.sun_path + 1, addr_length - sizeof(sa.sun_family) - 1);
 		} else {
