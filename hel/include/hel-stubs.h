87a8bf83a9f9f50b11fbba0f50d189bc4efa9f4b
Alexander <electrodeyt@gmail.com>
2023-07-09 18:20:15 +0200

    First iteration of everything, technically links

--- hel/include/hel-stubs.h
+++ hel/include/hel-stubs.h
@@ -5,6 +5,8 @@
 #	include "hel-stubs-x86_64.h"
 #elif defined(__aarch64__)
 #	include "hel-stubs-aarch64.h"
+#elif defined(__riscv) && __riscv_xlen == 64
+#	include "hel-stubs-riscv64.h"
 #else
 #	error Unsupported architecture
 #endif
