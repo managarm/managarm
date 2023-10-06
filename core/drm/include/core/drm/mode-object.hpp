e67ea1c59c8fc80c960aa71f61adde19d0658b4a
no92 <leo@managarm.org>
2023-10-06 15:46:37 +0200

    core/drm: add function for updating the "IN_FORMATS" property of planes

--- core/drm/include/core/drm/mode-object.hpp
+++ core/drm/include/core/drm/mode-object.hpp
@@ -281,6 +281,9 @@ struct Plane : ModeObject {
 	std::shared_ptr<drm_core::PlaneState> drmState();
 	void setDrmState(std::shared_ptr<drm_core::PlaneState> new_state);
 
+	// commit formats to the "IN_FORMATS" blob
+	void updateInFormatsBlob(std::shared_ptr<drm_core::Plane> plane);
+
 private:
 	PlaneType _type;
 	drm_core::FrameBuffer *_fb = nullptr;
