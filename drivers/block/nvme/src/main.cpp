767985231fffa1ddc81e8ac314232a8ab11fdfe9
Kacper Słomiński <kacper.slominski72@gmail.com>
2024-02-08 23:10:17 +0100

    block/nvme: Switch to the new mbus API

--- drivers/block/nvme/src/main.cpp
+++ drivers/block/nvme/src/main.cpp
@@ -7,8 +7,8 @@
 
 std::vector<std::unique_ptr<Controller>> globalControllers;
 
-async::detached bindController(mbus::Entity entity) {
-	protocols::hw::Device device(co_await entity.bind());
+async::detached bindController(mbus_ng::Entity entity) {
+	protocols::hw::Device device((co_await entity.getRemoteLane()).unwrap());
 	auto info = co_await device.getPciInfo();
 
 	auto &barInfo = info.barInfo[0];
@@ -18,28 +18,32 @@ async::detached bindController(mbus::Entity entity) {
 
 	helix::Mapping mapping{bar0, barInfo.offset, barInfo.length};
 
-	auto controller = std::make_unique<Controller>(entity.getId(), std::move(device), std::move(mapping),
-			   std::move(bar0), std::move(irq));
+	auto controller = std::make_unique<Controller>(entity.id(), std::move(device), std::move(mapping),
+			std::move(bar0), std::move(irq));
 	controller->run();
 	globalControllers.push_back(std::move(controller));
 }
 
 async::detached observeControllers() {
-	auto root = co_await mbus::Instance::global().getRoot();
+	auto filter = mbus_ng::Conjunction{{
+		mbus_ng::EqualsFilter{"pci-class", "01"},
+		mbus_ng::EqualsFilter{"pci-subclass", "08"},
+		mbus_ng::EqualsFilter{"pci-interface", "02"}
+	}};
 
-	auto filter = mbus::Conjunction({
-            mbus::EqualsFilter("pci-class", "01"),
-            mbus::EqualsFilter("pci-subclass", "08"),
-            mbus::EqualsFilter("pci-interface", "02"),
-    });
+	auto enumerator = mbus_ng::Instance::global().enumerate(filter);
+	while (true) {
+		auto [_, events] = (co_await enumerator.nextEvents()).unwrap();
 
-	auto handler = mbus::ObserverHandler{}
-					   .withAttach([](mbus::Entity entity, mbus::Properties) {
-						   std::cout << "block/nvme: Detected controller\n";
-						   bindController(std::move(entity));
-					   });
+		for (auto &event : events) {
+			if (event.type != mbus_ng::EnumerationEvent::Type::created)
+				continue;
 
-	co_await root.linkObserver(std::move(filter), std::move(handler));
+			auto entity = co_await mbus_ng::Instance::global().getEntity(event.id);
+			std::cout << "block/nvme: Detected controller" << std::endl;
+			bindController(std::move(entity));
+		}
+	}
 }
 
 int main() {
