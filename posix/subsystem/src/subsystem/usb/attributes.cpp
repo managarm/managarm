f04f868494d7374a5297d5bbd82cdedd38f011d0
no92 <leo@managarm.org>
2024-05-31 22:44:31 +0200

    posix: replace uses of `snprintf` with `std::format` in USB sysfs

--- posix/subsystem/src/subsystem/usb/attributes.cpp
+++ posix/subsystem/src/subsystem/usb/attributes.cpp
@@ -6,67 +6,53 @@
 namespace usb_subsystem {
 
 async::result<frg::expected<Error, std::string>> VendorAttribute::show(sysfs::Object *object) {
-	char buffer[6]; // The format is 1234\n\0.
 	auto device = static_cast<UsbBase *>(object);
-	snprintf(buffer, 6, "%.4x\n", device->desc()->idVendor);
-	co_return std::string{buffer};
+	co_return std::format("{:0>4x}\n", device->desc()->idVendor);
 }
 
 async::result<frg::expected<Error, std::string>> DeviceAttribute::show(sysfs::Object *object) {
-	char buffer[6]; // The format is 1234\n\0.
 	auto device = static_cast<UsbBase *>(object);
-	snprintf(buffer, 6, "%.4x\n", device->desc()->idProduct);
-	co_return std::string{buffer};
+	co_return std::format("{:0>4x}\n", device->desc()->idProduct);
 }
 
 async::result<frg::expected<Error, std::string>> DeviceClassAttribute::show(sysfs::Object *object) {
-	char buffer[4]; // The format is 34\n\0.
 	auto device = static_cast<UsbBase *>(object);
-	snprintf(buffer, 4, "%.2x\n", device->desc()->deviceClass);
-	co_return std::string{buffer};
+	co_return std::format("{:0>2x}\n", device->desc()->deviceClass);
 }
 
 async::result<frg::expected<Error, std::string>> DeviceSubClassAttribute::show(sysfs::Object *object) {
-	char buffer[4]; // The format is 34\n\0.
 	auto device = static_cast<UsbBase *>(object);
-	snprintf(buffer, 4, "%.2x\n", device->desc()->deviceSubclass);
-	co_return std::string{buffer};
+	co_return std::format("{:0>2x}\n", device->desc()->deviceSubclass);
 }
 
 async::result<frg::expected<Error, std::string>> DeviceProtocolAttribute::show(sysfs::Object *object) {
-	char buffer[4]; // The format is 34\n\0.
 	auto device = static_cast<UsbBase *>(object);
-	snprintf(buffer, 4, "%.2x\n", device->desc()->deviceProtocol);
-	co_return std::string{buffer};
+	co_return std::format("{:0>2x}\n", device->desc()->deviceProtocol);
 }
 
 async::result<frg::expected<Error, std::string>> BcdDeviceAttribute::show(sysfs::Object *object) {
-	char buffer[6]; // The format is 34\n\0.
 	auto device = static_cast<UsbBase *>(object);
-	snprintf(buffer, 6, "%.4x\n", device->desc()->bcdDevice);
-	co_return std::string{buffer};
+	co_return std::format("{:0>4x}\n", device->desc()->bcdDevice);
 }
 
 async::result<frg::expected<Error, std::string>> VersionAttribute::show(sysfs::Object *object) {
-	char buffer[7]; // The format is " 2.00\n\0".
 	auto device = static_cast<UsbBase *>(object);
-	snprintf(buffer, 7, "%2x.%02x\n", device->desc()->bcdUsb >> 8, device->desc()->bcdUsb & 0xFF);
-	co_return std::string{buffer};
+	co_return std::format("{:>2x}.{:0>2x}\n", device->desc()->bcdUsb >> 8, device->desc()->bcdUsb & 0xFF);
 }
 
 async::result<frg::expected<Error, std::string>> SpeedAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbBase *>(object);
-	co_return device->speed + "\n";
+	co_return std::format("{}\n", device->speed);
 }
 
 async::result<frg::expected<Error, std::string>> DeviceMaxPowerAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbDevice *>(object);
-	co_return std::to_string(device->maxPower) + "mA\n";
+	co_return std::format("{}mA\n", device->maxPower);
 }
 
 async::result<frg::expected<Error, std::string>> ControllerMaxPowerAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbController *>(object);
-	co_return std::to_string(device->maxPower) + "mA\n";
+	co_return std::format("{}mA\n", device->maxPower);
 }
 
 async::result<frg::expected<Error, std::string>> MaxChildAttribute::show(sysfs::Object *object) {
@@ -76,19 +62,17 @@ async::result<frg::expected<Error, std::string>> MaxChildAttribute::show(sysfs::
 
 async::result<frg::expected<Error, std::string>> NumInterfacesAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbDevice *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%2x\n", device->numInterfaces);
-	co_return std::string{buf};
+	co_return std::format("{:>2}\n", device->numInterfaces);
 }
 
 async::result<frg::expected<Error, std::string>> BusNumAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbBase *>(object);
-	co_return std::to_string(device->busNum) + "\n";
+	co_return std::format("{}\n", device->busNum);
 }
 
 async::result<frg::expected<Error, std::string>> DevNumAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbBase *>(object);
-	co_return std::to_string(device->portNum) + "\n";
+	co_return std::format("{}\n", device->portNum);
 }
 
 async::result<frg::expected<Error, std::string>> DescriptorsAttribute::show(sysfs::Object *object) {
@@ -109,14 +93,12 @@ async::result<frg::expected<Error, std::string>> TxLanesAttribute::show(sysfs::O
 async::result<frg::expected<Error, std::string>> ConfigValueAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbDevice *>(object);
 	auto config_val = (co_await device->device().currentConfigurationValue()).value();
-
-	co_return std::to_string(config_val) + "\n";
+	co_return std::format("{}\n", config_val);
 }
 
 async::result<frg::expected<Error, std::string>> MaxPacketSize0Attribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbDevice *>(object);
-
-	co_return std::to_string(device->desc()->maxPacketSize) + "\n";
+	co_return std::format("{}\n", device->desc()->maxPacketSize);
 }
 
 async::result<frg::expected<Error, std::string>> ConfigurationAttribute::show(sysfs::Object *object) {
@@ -126,65 +108,47 @@ async::result<frg::expected<Error, std::string>> ConfigurationAttribute::show(sy
 
 async::result<frg::expected<Error, std::string>> BmAttributesAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbDevice *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->bmAttributes);
-	co_return std::string{buf};
+	co_return std::format("{:2x}\n", device->bmAttributes);
 }
 
 async::result<frg::expected<Error, std::string>> NumConfigurationsAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbDevice *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->desc()->numConfigs);
-	co_return std::string{buf};
+	co_return std::format("{}\n", device->desc()->numConfigs);
 }
 
 async::result<frg::expected<Error, std::string>> InterfaceClassAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbInterface *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->interfaceClass);
-	co_return std::string{buf};
+	co_return std::format("{:0>2x}\n", device->interfaceClass);
 }
 
 async::result<frg::expected<Error, std::string>> InterfaceSubClassAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbInterface *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->interfaceSubClass);
-	co_return std::string{buf};
+	co_return std::format("{:0>2x}\n", device->interfaceSubClass);
 }
 
 async::result<frg::expected<Error, std::string>> InterfaceProtocolAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbInterface *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->interfaceProtocol);
-	co_return std::string{buf};
+	co_return std::format("{:0>2x}\n", device->interfaceProtocol);
 }
 
 async::result<frg::expected<Error, std::string>> AlternateSettingAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbInterface *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%2x\n", device->alternateSetting);
-	co_return std::string{buf};
+	co_return std::format("{:>2x}\n", device->alternateSetting);
 }
 
 async::result<frg::expected<Error, std::string>> InterfaceNumberAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbInterface *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->interfaceNumber);
-	co_return std::string{buf};
+	co_return std::format("{:0>2x}\n", device->interfaceNumber);
 }
 
 async::result<frg::expected<Error, std::string>> EndpointNumAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbInterface *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->endpointCount);
-	co_return std::string{buf};
+	co_return std::format("{:0>2x}\n", device->endpointCount);
 }
 
 async::result<frg::expected<Error, std::string>> EndpointAddressAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbEndpoint *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->endpointAddress);
-	co_return std::string{buf};
+	co_return std::format("{:0>2x}\n", device->endpointAddress);
 }
 
 async::result<frg::expected<Error, std::string>> PrettyIntervalAttribute::show(sysfs::Object *object) {
@@ -194,30 +158,22 @@ async::result<frg::expected<Error, std::string>> PrettyIntervalAttribute::show(s
 
 async::result<frg::expected<Error, std::string>> IntervalAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbEndpoint *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->interval);
-	co_return std::string{buf};
+	co_return std::format("{:0>2x}\n", device->interval);
 }
 
 async::result<frg::expected<Error, std::string>> LengthAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbEndpoint *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->length);
-	co_return std::string{buf};
+	co_return std::format("{:0>2x}\n", device->length);
 }
 
 async::result<frg::expected<Error, std::string>> EpAttributesAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbEndpoint *>(object);
-	char buf[4];
-	snprintf(buf, 4, "%02x\n", device->attributes);
-	co_return std::string{buf};
+	co_return std::format("{:0>2x}\n", device->attributes);
 }
 
 async::result<frg::expected<Error, std::string>> EpMaxPacketSizeAttribute::show(sysfs::Object *object) {
 	auto device = static_cast<UsbEndpoint *>(object);
-	char buf[6];
-	snprintf(buf, 6, "%04x\n", device->maxPacketSize);
-	co_return std::string{buf};
+	co_return std::format("{:0>4x}\n", device->maxPacketSize);
 }
 
 async::result<frg::expected<Error, std::string>> EpTypeAttribute::show(sysfs::Object *object) {
