a4125601226720f52fba20d46f7a8d694cbab55e
no92 <leo@managarm.org>
2024-06-16 12:06:01 +0200

    posix: break `CTRL_CMD_GETFAMILY` handling out into a function

--- posix/subsystem/src/netlink/nlctrl.cpp
+++ posix/subsystem/src/netlink/nlctrl.cpp
@@ -37,6 +37,107 @@ std::map<uint16_t, GenericNetlinkFamily> families = {
 	{GENL_ID_CTRL, { "nlctrl", 2, genl_ctrl_ops, genl_ctrl_mcast_groups, 0x10 }},
 };
 
+namespace {
+
+async::result<protocols::fs::Error> handleGetFamily(nl_socket::OpenFile *f, struct nlmsghdr *nlh, struct sockaddr_nl *sa) {
+	auto req = core::netlink::netlinkAttr(nlh, core::netlink::nl::packets::genl{});
+
+	std::optional<uint16_t> filter_id;
+	std::optional<std::string> filter_name;
+
+	if(req.has_value()) {
+		for(auto val : *req) {
+			switch(val.type()) {
+				case CTRL_ATTR_FAMILY_ID:
+					filter_id = val.data<uint16_t>();
+					break;
+				case CTRL_ATTR_FAMILY_NAME:
+					filter_name = val.str();
+					break;
+				default:
+					std::cout << std::format("posix: unhandled CTRL_CMD_GETFAMILY attr 0x{:x}\n", val.type());
+					break;
+			}
+		}
+	}
+
+	size_t matches = 0;
+	core::netlink::NetlinkBuilder b;
+
+	for(auto const &[id, info] : families) {
+		if(filter_id && filter_id.value() != id)
+			continue;
+		else if(filter_name && filter_name.value() != info.name)
+			continue;
+
+		b.header(id, NLM_F_MULTI, nlh->nlmsg_seq, sa->nl_pid);
+		b.message<genlmsghdr>({
+			.cmd = CTRL_CMD_NEWFAMILY,
+			.version = 2,
+		});
+		b.nlattr<uint16_t>(CTRL_ATTR_FAMILY_ID, id);
+		b.nlattr<std::string>(CTRL_ATTR_FAMILY_NAME, info.name);
+		b.nlattr<uint32_t>(CTRL_ATTR_VERSION, info.version);
+		b.nlattr<uint32_t>(CTRL_ATTR_HDRSIZE, 0);
+		b.nlattr<uint32_t>(CTRL_ATTR_MAXATTR, 0);
+
+		if(!info.ops.empty()) {
+			b.nested_nlattr<const GenericNetlinkFamily *>(CTRL_ATTR_OPS,
+					[](core::netlink::NetlinkBuilder &b, auto info) {
+				for(size_t i = 0; i < info->ops.size(); i++) {
+					struct OpInfo {
+						uint8_t id;
+						uint8_t flags;
+					} const op_info = {
+						info->ops[i].cmd,
+						info->ops[i].flags,
+					};
+
+					b.nested_nlattr<const OpInfo *>(i + 1,
+							[](core::netlink::NetlinkBuilder &b, auto ctx) {
+						b.nlattr<uint32_t>(CTRL_ATTR_OP_ID, ctx->id);
+						b.nlattr<uint32_t>(CTRL_ATTR_OP_FLAGS, ctx->flags);
+					}, &op_info);
+				}
+			}, &info);
+		}
+
+		if(!info.mcast_groups.empty()) {
+			b.nested_nlattr<const GenericNetlinkFamily *>(CTRL_ATTR_MCAST_GROUPS,
+					[](core::netlink::NetlinkBuilder &b, auto info) {
+				for(uint32_t i = 0; i < info->mcast_groups.size(); i++) {
+					struct MulticastGroupInfo {
+						uint32_t id;
+						std::string name;
+					} const mcast_info = {
+						info->first_id + i,
+						info->mcast_groups[i].name,
+					};
+
+					b.nested_nlattr<const MulticastGroupInfo *>(i + 1,
+							[](core::netlink::NetlinkBuilder &b, auto ctx) {
+						b.nlattr<uint32_t>(CTRL_ATTR_MCAST_GRP_ID, ctx->id);
+						b.nlattr<std::string>(CTRL_ATTR_MCAST_GRP_NAME, ctx->name);
+					}, &mcast_info);
+				}
+			}, &info);
+		}
+
+		f->deliver(b.packet());
+		matches++;
+	}
+
+	if(matches) {
+		sendDone(f, nlh, sa);
+	} else {
+		sendError(f, nlh, ENOENT, sa);
+	}
+
+	co_return protocols::fs::Error::none;
+}
+
+} // namespace
+
 async::result<protocols::fs::Error> nlctrl::sendMsg(nl_socket::OpenFile *f, core::netlink::Packet packet, struct sockaddr_nl *sa) {
 	auto nlh = reinterpret_cast<struct nlmsghdr *>(packet.buffer.data());
 	auto genlh = reinterpret_cast<struct genlmsghdr *>(NLMSG_DATA(nlh));
@@ -48,100 +149,7 @@ async::result<protocols::fs::Error> nlctrl::sendMsg(nl_socket::OpenFile *f, core
 		case GENL_ID_CTRL: {
 			switch(genlh->cmd) {
 				case CTRL_CMD_GETFAMILY: {
-					auto req = core::netlink::netlinkAttr(nlh, core::netlink::nl::packets::genl{});
-
-					std::optional<uint16_t> filter_id;
-					std::optional<std::string> filter_name;
-
-					if(req.has_value()) {
-						for(auto val : *req) {
-							switch(val.type()) {
-								case CTRL_ATTR_FAMILY_ID:
-									filter_id = val.data<uint16_t>();
-									break;
-								case CTRL_ATTR_FAMILY_NAME:
-									filter_name = val.str();
-									break;
-								default:
-									std::cout << std::format("posix: unhandled CTRL_CMD_GETFAMILY attr 0x{:x}\n", val.type());
-									break;
-							}
-						}
-					}
-
-					size_t matches = 0;
-					core::netlink::NetlinkBuilder b;
-
-					for(auto const &[id, info] : families) {
-						if(filter_id && filter_id.value() != id)
-							continue;
-						else if(filter_name && filter_name.value() != info.name)
-							continue;
-
-						b.header(id, NLM_F_MULTI, nlh->nlmsg_seq, sa->nl_pid);
-						b.message<genlmsghdr>({
-							.cmd = CTRL_CMD_NEWFAMILY,
-							.version = 2,
-						});
-						b.nlattr<uint16_t>(CTRL_ATTR_FAMILY_ID, id);
-						b.nlattr<std::string>(CTRL_ATTR_FAMILY_NAME, info.name);
-						b.nlattr<uint32_t>(CTRL_ATTR_VERSION, info.version);
-						b.nlattr<uint32_t>(CTRL_ATTR_HDRSIZE, 0);
-						b.nlattr<uint32_t>(CTRL_ATTR_MAXATTR, 0);
-
-						if(!info.ops.empty()) {
-							b.nested_nlattr<const GenericNetlinkFamily *>(CTRL_ATTR_OPS,
-									[](core::netlink::NetlinkBuilder &b, auto info) {
-								for(size_t i = 0; i < info->ops.size(); i++) {
-									struct OpInfo {
-										uint8_t id;
-										uint8_t flags;
-									} const op_info = {
-										info->ops[i].cmd,
-										info->ops[i].flags,
-									};
-
-									b.nested_nlattr<const OpInfo *>(i + 1,
-											[](core::netlink::NetlinkBuilder &b, auto ctx) {
-										b.nlattr<uint32_t>(CTRL_ATTR_OP_ID, ctx->id);
-										b.nlattr<uint32_t>(CTRL_ATTR_OP_FLAGS, ctx->flags);
-									}, &op_info);
-								}
-							}, &info);
-						}
-
-						if(!info.mcast_groups.empty()) {
-							b.nested_nlattr<const GenericNetlinkFamily *>(CTRL_ATTR_MCAST_GROUPS,
-									[](core::netlink::NetlinkBuilder &b, auto info) {
-								for(uint32_t i = 0; i < info->mcast_groups.size(); i++) {
-									struct MulticastGroupInfo {
-										uint32_t id;
-										std::string name;
-									} const mcast_info = {
-										info->first_id + i,
-										info->mcast_groups[i].name,
-									};
-
-									b.nested_nlattr<const MulticastGroupInfo *>(i + 1,
-											[](core::netlink::NetlinkBuilder &b, auto ctx) {
-										b.nlattr<uint32_t>(CTRL_ATTR_MCAST_GRP_ID, ctx->id);
-										b.nlattr<std::string>(CTRL_ATTR_MCAST_GRP_NAME, ctx->name);
-									}, &mcast_info);
-								}
-							}, &info);
-						}
-
-						f->deliver(b.packet());
-						matches++;
-					}
-
-					if(matches) {
-						sendDone(f, nlh, sa);
-					} else {
-						sendError(f, nlh, ENOENT, sa);
-					}
-
-					co_return protocols::fs::Error::none;
+					co_return co_await handleGetFamily(f, nlh, sa);
 				}
 				default: {
 					std::cout << std::format("posix: unknown nlctrl cmd=0x{:x} version={}\n", genlh->cmd, genlh->version);
