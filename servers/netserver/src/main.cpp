6d4725dcebd854574ca1456ce23960c5b4666edb
no92 <leo@managarm.org>
2024-08-09 21:19:11 +0200

    netserver: stop requiring iMACAddress for USB CDC devices

--- servers/netserver/src/main.cpp
+++ servers/netserver/src/main.cpp
@@ -154,7 +154,7 @@ async::result<protocols::svrctl::Error> doBindUsb(mbus_ng::Entity baseEntity) {
 			}
 		});
 
-		if(usb_info.valid && usb_info.iMACAddress && usb_info.control_if && usb_info.data_if) {
+		if(usb_info.valid && usb_info.control_if && usb_info.data_if) {
 			usb_info.configuration_index = configuration;
 			usb_info.chosen_configuration = reinterpret_cast<protocols::usb::ConfigDescriptor *>(raw_descs.data())->configValue;
 			matched_usb_info = std::move(usb_info);
@@ -166,14 +166,22 @@ async::result<protocols::svrctl::Error> doBindUsb(mbus_ng::Entity baseEntity) {
 	|| matched_usb_info->subclass == protocols::usb::cdc_subclass::reserved
 	|| !matched_usb_info->valid
 	|| !matched_usb_info->chosen_configuration
-	|| !matched_usb_info->iMACAddress
 	|| !matched_usb_info->control_if
 	|| !matched_usb_info->data_if) {
-		std::cout << std::format("netserver: skipping device with mbus ID {}", baseEntity.id()) << std::endl;
+		std::cout << std::format("netserver: skipping device {:04x}:{:04x} with mbus ID {}\n",
+			dev_desc->idVendor, dev_desc->idProduct, baseEntity.id()
+		);
 		co_return protocols::svrctl::Error::deviceNotSupported;
 	}
 
-	auto str = (co_await dev.getString(*matched_usb_info->iMACAddress)).value();
+	std::cout << std::format("netserver: binding device {:04x}:{:04x} with mbus ID {}\n",
+		dev_desc->idVendor, dev_desc->idProduct, baseEntity.id()
+	);
+
+	std::string str = "000000000000";
+
+	if(matched_usb_info->iMACAddress)
+		str = (co_await dev.getString(*matched_usb_info->iMACAddress)).value();
 
 	auto decodeHexString = [](char c) -> uint8_t {
 		if(c >= 'A' && c <= 'F')
