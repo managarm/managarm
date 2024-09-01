d686e5fb8b1fca6a9ec14ba50628a36a1b3f9d11
Kacper Słomiński <kacper.slominski72@gmail.com>
2024-09-01 03:01:43 +0200

    init: Look for the root partition instead of hardcoding it
    The root partition now has a special sysfs attribute given based on
    the partition type, which we can use to find the partition instead of
    assuming that it's /dev/sda1.

--- posix/init/src/stage1.cpp
+++ posix/init/src/stage1.cpp
@@ -7,8 +7,149 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/sysmacros.h>
+#include <linux/netlink.h>
 #include <unistd.h>
+#include <string.h>
 #include <iostream>
+#include <string_view>
+#include <unordered_set>
+#include <ranges>
+#include <filesystem>
+#include <optional>
+#include <fstream>
+
+bool logDiscovery = false;
+
+std::string findRootDevice() {
+	std::cout << "init: Looking for the root partition" << std::endl;
+
+	int nlFd = socket(AF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
+	if(nlFd < 0) {
+		std::cout << "init: socket(AF_NETLINK) failed! errno: " << strerror(errno) << std::endl;
+		return "";
+	}
+
+	struct sockaddr_nl sa;
+	sa.nl_family = AF_NETLINK;
+	sa.nl_pid = getpid();
+	sa.nl_groups = 1;
+
+	int ret = bind(nlFd, reinterpret_cast<struct sockaddr *>(&sa), sizeof(sa));
+	if(ret < 0) {
+		std::cout << "init: bind(nlFd) failed! errno: " << strerror(errno) << std::endl;
+		return "";
+	}
+
+	std::unordered_set<std::string> checkedDevices;
+
+	auto checkDevice = [&] (std::string device) -> std::optional<std::string> {
+		if (checkedDevices.contains(device))
+			return std::nullopt;
+
+		checkedDevices.insert(device);
+
+		if(logDiscovery)
+			std::cout << "init: Considering device " << device << std::endl;
+
+		auto rootAttr = device + "/managarm-root";
+
+		// Check if the managarm-root attribute exists
+		if(access(rootAttr.data(), R_OK)) {
+			assert(errno == ENOENT);
+			if(logDiscovery)
+				std::cout << "init:  Not the root filesystem" << std::endl;
+			return std::nullopt;
+		}
+
+		// Figure out the device's major:minor
+		std::ifstream ifs{device + "/dev"};
+		std::string dev;
+		std::getline(ifs, dev);
+
+		auto split = dev | std::views::split(':');
+		auto it = split.begin();
+
+		// TODO(qookie): C++23 would let us do std::string_view{*it}
+		std::string majorStr{(*it).begin(), (*it).end()};
+		it++;
+		std::string minorStr{(*it).begin(), (*it).end()};
+
+		int major = std::stoi(majorStr), minor = std::stoi(minorStr);
+
+		// Find the /dev node with the right major:minor numbers
+		for(auto node : std::filesystem::directory_iterator{"/dev/"}) {
+			struct stat st;
+			stat(node.path().c_str(), &st);
+
+			if(st.st_rdev == makedev(major, minor)) {
+				return node.path();
+			}
+		}
+
+		// This major:minor is not in /dev? Bail out...
+		std::cout << "init: Device " << device << " (maj:min " << major << ":" << minor << ")"
+			<< "is the root filesystem, but has no corresponding /dev node?" << std::endl;
+		return "";
+	};
+
+	// Check existing devices in /sys/class/block
+	// that appeared before we started listening
+	for(auto dev : std::filesystem::directory_iterator{"/sys/class/block"}) {
+		auto linkTarget = "/sys/class/block/" / std::filesystem::read_symlink(dev);
+
+		if(logDiscovery)
+			std::cout << "init: Candidate device " << linkTarget
+				<< " found via /sys/class/block" << std::endl;
+		if(auto device = checkDevice(linkTarget.generic_string()))
+			return device.value();
+	}
+
+	while(true) {
+		std::string buf;
+		buf.resize(16384);
+
+		ret = read(nlFd, buf.data(), buf.size());
+		if(ret < 0) {
+			std::cout << "init: read(nlFd) failed! errno: " << strerror(errno) << std::endl;
+			return "";
+		} else {
+			// Parse the uevent message
+			std::string_view action, subsystem;
+			std::string devpath;
+
+			const char *cur = buf.data();
+			while(cur < buf.data() + ret) {
+				std::string_view line{cur};
+				auto split = line | std::views::split('=');
+				auto it = split.begin();
+
+				// TODO(qookie): C++23 would let us do std::string_view{*it}
+				std::string_view name{(*it).begin(), (*it).end()};
+				it++;
+				std::string_view value{(*it).begin(), (*it).end()};
+
+				if(name == "ACTION")
+					action = value;
+				else if(name == "SUBSYSTEM")
+					subsystem = value;
+				else if(name == "DEVPATH")
+					devpath = "/sys" + std::string{value};
+
+				cur += line.size() + 1;
+			}
+
+			if(action == "add" && subsystem == "block") {
+				if(logDiscovery)
+					std::cout << "init: Candidate device " << devpath
+						<< " found via uevent" << std::endl;
+				if(auto device = checkDevice(devpath))
+					return device.value();
+			}
+		}
+	}
+}
 
 int main() {
 	int fd = open("/dev/helout", O_WRONLY);
@@ -62,12 +203,14 @@ int main() {
 		execl("/bin/runsvr", "/bin/runsvr", "runsvr", "/sbin/storage", nullptr);
 	}else assert(block_usb != -1);
 
-	// Spin until /dev/sda0 becomes available. Then mount the rootfs and prepare it.
-	while(access("/dev/sda1", F_OK)) {
-		assert(errno == ENOENT);
-		std::cout << "Waiting for /dev/sda1" << std::endl;
-		sleep(1);
-	}
+	std::string rootPath = "";
+	// TODO(qookie): Query /proc/cmdline to see if the user
+	// requested a different device.
+
+	if (!rootPath.size())
+		rootPath = findRootDevice();
+	if (!rootPath.size())
+		throw std::runtime_error("Can't determine root device");
 
 #if defined (__x86_64__)
 	// Hack: Start UHCI only after EHCI devices are ready.
@@ -77,8 +220,8 @@ int main() {
 	}else assert(uhci != -1);
 #endif
 
-	std::cout << "init: Mounting /dev/sda1" << std::endl;
-	if(mount("/dev/sda1", "/realfs", "ext2", 0, ""))
+	std::cout << "init: Mounting " << rootPath << std::endl;
+	if(mount(rootPath.data(), "/realfs", "ext2", 0, ""))
 		throw std::runtime_error("mount() failed");
 
 	if(mount("", "/realfs/proc", "procfs", 0, ""))
