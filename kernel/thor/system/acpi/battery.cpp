2a596a7335a71d3a89fecc58bcbf796cfeda3ecd
no92 <leo@managarm.org>
2024-09-27 11:18:17 +0200

    thor: maintain and expose battery state

--- kernel/thor/system/acpi/battery.cpp
+++ kernel/thor/system/acpi/battery.cpp
@@ -1,8 +1,10 @@
 #include <bragi/helpers-all.hpp>
 #include <bragi/helpers-frigg.hpp>
+#include <frg/utility.hpp>
 #include <hw.frigg_bragi.hpp>
 #include <thor-internal/acpi/acpi.hpp>
 #include <thor-internal/acpi/battery.hpp>
+#include <thor-internal/acpi/helpers.hpp>
 #include <thor-internal/fiber.hpp>
 #include <thor-internal/mbus.hpp>
 #include <thor-internal/main.hpp>
@@ -22,9 +24,36 @@ private:
 	coroutine<frg::expected<Error>> handleRequest(LaneHandle lane) override;
 	static uacpi_status notification(uacpi_handle, uacpi_namespace_node *node, uacpi_u64 value);
 
+	void updateBIF();
+	void updateBST();
+	void updateState();
+
+	struct BatteryState {
+		// the units in which ACPI reports its data
+		frg::optional<int> acpi_units = frg::null_opt;
+		frg::optional<int> battery_technology = frg::null_opt;
+
+		bool charging;
+		// the rate of (dis)charge in mW
+		frg::optional<uint32_t> rate_milliwatt;
+		// the rate of (dis)charge in mA
+		frg::optional<uint32_t> rate_milliampere;
+		// the voltage across the battery terminals
+		frg::optional<uint32_t> voltage;
+		frg::optional<uint32_t> design_voltage;
+
+		frg::optional<uint32_t> remaining_capacity_milliwatthours;
+		frg::optional<uint32_t> remaining_capacity_milliamperehours;
+		frg::optional<uint32_t> design_capacity_milliwatthours;
+		frg::optional<uint32_t> design_capacity_milliamperehours;
+		frg::optional<uint32_t> last_full_charge_capacity_milliwatthours;
+		frg::optional<uint32_t> last_full_charge_capacity_milliamperehours;
+	};
+
 	size_t _id;
 	uacpi_namespace_node *_node;
 	async::recurring_event _irq;
+	BatteryState _state = {};
 };
 
 } // namespace thor::acpi
@@ -36,7 +65,52 @@ constexpr bool logBatteryUpdates = true;
 
 size_t next_battery_id = 0;
 
-}
+namespace BIF {
+	namespace PowerUnit {
+		constexpr uacpi_u32 MILLIWATT = 0;
+		constexpr uacpi_u32 MILLIAMPERE = 1;
+	} // namespace PowerUnit
+
+	namespace DesignCapacity {
+		constexpr uacpi_u32 UNKNOWN = 0xFFFFFFFF;
+	} // namespace DesignCapacity
+
+	namespace LastFullChargeCapacity {
+		constexpr uacpi_u32 UNKNOWN = 0xFFFFFFFF;
+	} // namespace LastFullChargeCapacity
+
+	namespace BatteryTechnology {
+		constexpr uacpi_u32 PRIMARY = 0;
+		constexpr uacpi_u32 SECONDARY = 1;
+	} // namespace BatteryTechnology
+
+	namespace DesignVoltage {
+		constexpr uacpi_u32 UNKNOWN = 0xFFFFFFFF;
+	} // namespace DesignVoltage
+} // namespace BIF
+
+namespace BST {
+	namespace State {
+		constexpr uacpi_u32 DISCHARGING = (1 << 0);
+		constexpr uacpi_u32 CHARGING = (1 << 1);
+		constexpr uacpi_u32 CRITICAL_ENERGY_STATE = (1 << 2);
+		constexpr uacpi_u32 CHARGE_LIMITING = (1 << 3);
+	} // namespace State
+
+	namespace Rate {
+		constexpr uacpi_u32 UNKNOWN = 0xFFFFFFFF;
+	} // namespace Rate
+
+	namespace Voltage {
+		constexpr uacpi_u32 UNKNOWN = 0xFFFFFFFF;
+	} // namespace Voltage
+
+	namespace Capacity {
+		constexpr uacpi_u32 UNKNOWN = 0xFFFFFFFF;
+	} // namespace Capacity
+} // namespace BST
+
+} // namespace
 
 namespace thor::acpi {
 
@@ -44,6 +118,8 @@ coroutine<void> BatteryBusObject::run() {
 	auto obj = frg::construct<AcpiObject>(*kernelAlloc, _node, ACPI_HID_BATTERY, _id);
 	co_await obj->run();
 	auto acpi_object = obj->mbus_id;
+
+	updateState();
 
 	Properties properties;
 	properties.stringProperty("class", frg::string<KernelAlloc>(*kernelAlloc, "power_supply"));
@@ -71,8 +147,51 @@ coroutine<frg::expected<Error>> BatteryBusObject::handleRequest(LaneHandle lane)
 	if(preamble.error())
 		co_return Error::protocolViolation;
 
-	infoLogger() << "thor: dismissing conversation due to illegal HW request." << frg::endlog;
-	co_await DismissSender{conversation};
+	if(preamble.id() == bragi::message_id<managarm::hw::BatteryStateRequest>) {
+		auto req = bragi::parse_head_only<managarm::hw::BatteryStateRequest>(reqBuffer, *kernelAlloc);
+
+		if(req->block_until_ready())
+			co_await _irq.async_wait();
+
+		managarm::hw::BatteryStateReply<KernelAlloc> resp(*kernelAlloc);
+
+		resp.set_charging(_state.charging);
+		if(_state.rate_milliampere)
+			resp.set_current_now(*_state.rate_milliampere * 1000);
+		if(_state.rate_milliwatt)
+			resp.set_power_now(*_state.rate_milliwatt * 1000);
+		if(_state.remaining_capacity_milliwatthours)
+			resp.set_energy_now(*_state.remaining_capacity_milliwatthours * 1000);
+		if(_state.last_full_charge_capacity_milliwatthours)
+			resp.set_energy_full(*_state.last_full_charge_capacity_milliwatthours * 1000);
+		if(_state.design_capacity_milliwatthours)
+			resp.set_energy_full_design(*_state.design_capacity_milliwatthours * 1000);
+		if(_state.voltage)
+			resp.set_voltage_now(*_state.voltage * 1000);
+		if(_state.design_voltage)
+			resp.set_voltage_min_design(*_state.design_voltage * 1000);
+
+		resp.set_error(managarm::hw::Errors::SUCCESS);
+
+		frg::unique_memory<KernelAlloc> respHeadBuffer{*kernelAlloc, resp.head_size};
+		frg::unique_memory<KernelAlloc> respTailBuffer{*kernelAlloc, resp.size_of_tail()};
+
+		bragi::write_head_tail(resp, respHeadBuffer, respTailBuffer);
+
+		auto respHeadError = co_await SendBufferSender{conversation, std::move(respHeadBuffer)};
+		if(respHeadError != Error::success)
+			co_return respHeadError;
+
+		auto respTailError = co_await SendBufferSender{conversation, std::move(respTailBuffer)};
+		if(respTailError != Error::success)
+			co_return respTailError;
+
+		co_return frg::success;
+	}else{
+		infoLogger() << "thor: dismissing conversation due to illegal HW request." << frg::endlog;
+		co_await DismissSender{conversation};
+	}
+
 	co_return frg::success;
 }
 
@@ -84,10 +203,220 @@ uacpi_status BatteryBusObject::notification(uacpi_handle context, uacpi_namespac
 	}
 
 	auto self = reinterpret_cast<BatteryBusObject *>(context);
+
+	self->updateState();
 	self->_irq.raise();
 
 	return UACPI_STATUS_OK;
 }
+
+void BatteryBusObject::updateBIF() {
+	uacpi_object *bif = UACPI_NULL;
+
+	auto ret = uacpi_eval_typed(_node, "_BIF", UACPI_NULL, UACPI_OBJECT_PACKAGE_BIT, &bif);
+	if(ret != UACPI_STATUS_OK) {
+		infoLogger() << "thor: _BIF error " << uacpi_status_to_string(ret) << frg::endlog;
+		return;
+	}
+
+	uacpi_package *pkg = bif->package;
+
+	auto power_unit = intFromPackage(pkg, 0);
+	auto design_capacity = intFromPackage(pkg, 1);
+	auto last_full_charge_capacity = intFromPackage(pkg, 2);
+	auto battery_technology = intFromPackage(pkg, 3);
+	auto design_voltage = intFromPackage(pkg, 4);
+
+	if(!power_unit) {
+		infoLogger() << "thor: battery power unit: invalid" << frg::endlog;
+	} else {
+		switch(*power_unit) {
+			case BIF::PowerUnit::MILLIWATT:
+				_state.acpi_units = BIF::PowerUnit::MILLIWATT;
+				break;
+			case BIF::PowerUnit::MILLIAMPERE:
+				_state.acpi_units = BIF::PowerUnit::MILLIAMPERE;
+				break;
+			default:
+				_state.acpi_units = frg::null_opt;
+				break;
+		}
+	}
+
+	if(_state.acpi_units && design_capacity && *design_capacity != BIF::DesignCapacity::UNKNOWN) {
+		if(*_state.acpi_units == BIF::PowerUnit::MILLIAMPERE) {
+			_state.design_capacity_milliamperehours = *design_capacity;
+
+			if(_state.voltage)
+				_state.design_capacity_milliwatthours = (*_state.design_capacity_milliamperehours) * (*_state.voltage);
+		} else if(*_state.acpi_units == BIF::PowerUnit::MILLIWATT) {
+			_state.design_capacity_milliwatthours = *design_capacity;
+
+			if(_state.voltage)
+				_state.design_capacity_milliamperehours = (*_state.design_capacity_milliwatthours) / (*_state.voltage);
+		} else {
+			panicLogger() << "thor: unhandled ACPI battery power unit " << *_state.acpi_units << frg::endlog;
+		}
+	} else {
+		_state.design_capacity_milliwatthours = frg::null_opt;
+		_state.design_capacity_milliamperehours = frg::null_opt;
+	}
+
+	if(_state.acpi_units && last_full_charge_capacity && *last_full_charge_capacity != BIF::LastFullChargeCapacity::UNKNOWN) {
+		if(*_state.acpi_units == BIF::PowerUnit::MILLIAMPERE) {
+			_state.last_full_charge_capacity_milliamperehours = *last_full_charge_capacity;
+
+			if(_state.voltage)
+				_state.last_full_charge_capacity_milliwatthours = (*_state.last_full_charge_capacity_milliamperehours) * (*_state.voltage);
+		} else if(*_state.acpi_units == BIF::PowerUnit::MILLIWATT) {
+			_state.last_full_charge_capacity_milliwatthours = *last_full_charge_capacity;
+
+			if(_state.voltage)
+				_state.last_full_charge_capacity_milliamperehours = (*_state.last_full_charge_capacity_milliwatthours) / (*_state.voltage);
+		} else {
+			panicLogger() << "thor: unhandled ACPI battery power unit " << *_state.acpi_units << frg::endlog;
+		}
+	} else {
+		_state.last_full_charge_capacity_milliwatthours = frg::null_opt;
+		_state.last_full_charge_capacity_milliamperehours = frg::null_opt;
+	}
+
+	if(battery_technology) {
+		switch(*battery_technology) {
+			case BIF::BatteryTechnology::PRIMARY:
+			case BIF::BatteryTechnology::SECONDARY:
+				_state.battery_technology = *battery_technology;
+				break;
+			default:
+				_state.battery_technology = frg::null_opt;
+				break;
+		}
+	} else {
+		_state.battery_technology = frg::null_opt;
+	}
+
+	if(design_voltage && *design_voltage != BIF::DesignVoltage::UNKNOWN)
+		_state.design_voltage = *design_voltage;
+	else
+		_state.design_voltage = frg::null_opt;
+
+	uacpi_object_unref(bif);
+}
+
+void BatteryBusObject::updateBST() {
+	uacpi_object *bst = UACPI_NULL;
+
+	auto ret = uacpi_eval_typed(_node, "_BST", UACPI_NULL, UACPI_OBJECT_PACKAGE_BIT, &bst);
+	if(ret != UACPI_STATUS_OK) {
+		infoLogger() << "thor: _BST error " << uacpi_status_to_string(ret) << frg::endlog;
+		return;
+	}
+
+	uacpi_package *pkg = bst->package;
+
+	auto battery_state = intFromPackage(pkg, 0);
+	auto present_rate = intFromPackage(pkg, 1);
+	auto remaining_capacity = intFromPackage(pkg, 2);
+	auto present_voltage = intFromPackage(pkg, 3);
+
+	if(!battery_state) {
+		infoLogger() << "thor: battery state: invalid" << frg::endlog;
+	} else {
+		if(*battery_state & BST::State::DISCHARGING)
+			_state.charging = false;
+		if(*battery_state & BST::State::CHARGING)
+			_state.charging = true;
+		if(*battery_state & BST::State::CRITICAL_ENERGY_STATE)
+			if(logBatteryUpdates)
+				infoLogger() << "thor: battery state: critical energy" << frg::endlog;
+		if(*battery_state & BST::State::CHARGE_LIMITING)
+			if(logBatteryUpdates)
+				infoLogger() << "thor: battery state: charge limiting" << frg::endlog;
+	}
+
+	if(present_voltage && *present_voltage != BST::Voltage::UNKNOWN)
+		_state.voltage = *present_voltage;
+	else
+		_state.voltage = frg::null_opt;
+
+	if(_state.acpi_units && present_rate && *present_rate != BST::Rate::UNKNOWN) {
+		if(*_state.acpi_units == BIF::PowerUnit::MILLIAMPERE) {
+			_state.rate_milliampere = frg::abs(uacpi_i32(*present_rate));
+
+			if(_state.voltage)
+				_state.rate_milliwatt = (*_state.rate_milliampere) * (*_state.voltage);
+			else
+				_state.rate_milliwatt = frg::null_opt;
+		} else if(*_state.acpi_units == BIF::PowerUnit::MILLIWATT) {
+			_state.rate_milliwatt = *present_rate;
+
+			if(_state.voltage)
+				_state.rate_milliampere = (*_state.rate_milliwatt) / (*_state.voltage);
+			else
+				_state.rate_milliampere = frg::null_opt;
+		} else {
+			panicLogger() << "thor: unhandled ACPI battery power unit " << *_state.acpi_units << frg::endlog;
+		}
+	} else {
+		_state.rate_milliwatt = frg::null_opt;
+		_state.rate_milliampere = frg::null_opt;
+	}
+
+	if(_state.acpi_units && remaining_capacity && remaining_capacity != BST::Capacity::UNKNOWN) {
+		if(*_state.acpi_units == BIF::PowerUnit::MILLIAMPERE) {
+			_state.remaining_capacity_milliamperehours = *remaining_capacity;
+
+			if(_state.voltage)
+				_state.remaining_capacity_milliwatthours = (*_state.remaining_capacity_milliamperehours) * (*_state.voltage);
+		} else if(*_state.acpi_units == BIF::PowerUnit::MILLIWATT) {
+			_state.remaining_capacity_milliwatthours = *remaining_capacity;
+
+			if(_state.voltage)
+				_state.remaining_capacity_milliamperehours = (*_state.remaining_capacity_milliwatthours) / (*_state.voltage);
+		} else {
+			panicLogger() << "thor: unhandled ACPI battery power unit " << *_state.acpi_units << frg::endlog;
+		}
+	} else {
+		_state.remaining_capacity_milliwatthours = frg::null_opt;
+		_state.remaining_capacity_milliamperehours = frg::null_opt;
+	}
+
+	uacpi_object_unref(bst);
+}
+
+void BatteryBusObject::updateState() {
+	updateBIF();
+	// _BST depends on unit information from _BIF
+	updateBST();
+
+	if(logBatteryUpdates) {
+		infoLogger() << "thor: battery " << _id << " update:" << frg::endlog;
+		infoLogger() << "\tState: " << (_state.charging ? "charging" : "discharging") << frg::endlog;
+		if(_state.battery_technology)
+			infoLogger() << "\tBattery Technology: " << (*_state.battery_technology == BIF::BatteryTechnology::PRIMARY ? "Primary" : "Secondary") << frg::endlog;
+		if(_state.design_voltage)
+			infoLogger() << "\tDesign Voltage: " << *_state.design_voltage << " mV" << frg::endlog;
+		if(_state.voltage)
+			infoLogger() << "\tVoltage: " << *_state.voltage << " mV" << frg::endlog;
+		if(_state.rate_milliwatt)
+			infoLogger() << "\tRate: " << *_state.rate_milliwatt << " mW" << frg::endlog;
+		if(_state.rate_milliampere)
+			infoLogger() << "\tRate: " << *_state.rate_milliampere << " mA" << frg::endlog;
+		if(_state.remaining_capacity_milliwatthours)
+			infoLogger() << "\tRemaining Capacity: " << *_state.remaining_capacity_milliwatthours << " mWh" << frg::endlog;
+		if(_state.remaining_capacity_milliamperehours)
+			infoLogger() << "\tRemaining Capacity: " << *_state.remaining_capacity_milliamperehours << " mAh" << frg::endlog;
+		if(_state.design_capacity_milliwatthours)
+			infoLogger() << "\tDesign Capacity: " << *_state.design_capacity_milliwatthours << " mWh" << frg::endlog;
+		if(_state.design_capacity_milliamperehours)
+			infoLogger() << "\tDesign Capacity: " << *_state.design_capacity_milliamperehours << " mAh" << frg::endlog;
+		if(_state.last_full_charge_capacity_milliwatthours)
+			infoLogger() << "\tLast Full Charge Capacity: " << *_state.last_full_charge_capacity_milliwatthours << " mWh" << frg::endlog;
+		if(_state.last_full_charge_capacity_milliamperehours)
+			infoLogger() << "\tLast Full Charge Capacity: " << *_state.last_full_charge_capacity_milliamperehours << " mAh" << frg::endlog;
+	}
+}
+
 void initializeBatteries() {
 	async::detach_with_allocator(*kernelAlloc, []() -> coroutine<void> {
 		co_await acpiFiber->associatedWorkQueue()->schedule();
---
+++ kernel/thor/system/acpi/thor-internal/acpi/helpers.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <frg/optional.hpp>
+#include <uacpi/types.h>
+
+namespace thor::acpi {
+
+frg::optional<uacpi_u64> intFromPackage(uacpi_package *pkg, size_t index) {
+	if(pkg->count <= index)
+		return frg::null_opt;
+
+	if(pkg->objects[index]->type != UACPI_OBJECT_INTEGER)
+		return frg::null_opt;
+
+	return pkg->objects[index]->integer;
+}
+
+} // namespace thor::acpi
