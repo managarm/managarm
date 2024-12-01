c650c3697b80a730376db38ba6132b52df78a541
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2024-12-01 21:59:33 +0100

    kernel/thor: Determine frequency of RISC-V timer

--- kernel/thor/arch/riscv/timer.cpp
+++ kernel/thor/arch/riscv/timer.cpp
@@ -1,6 +1,8 @@
 #include <thor-internal/arch-generic/cpu.hpp>
+#include <thor-internal/dtb/dtb.hpp>
 #include <thor-internal/main.hpp>
 #include <thor-internal/timer.hpp>
+#include <thor-internal/util.hpp>
 
 namespace thor {
 
@@ -9,8 +11,12 @@ extern PrecisionTimerEngine *globalTimerEngine;
 
 namespace {
 
+// Frequency and inverse frequency of the CPU timer in nHz and ns, respectively.
+constinit FreqFraction freq;
+constinit FreqFraction inverseFreq;
+
 struct RiscvClockSource : ClockSource {
-	uint64_t currentNanos() override { return getRawTimestampCounter(); }
+	uint64_t currentNanos() override { return inverseFreq * getRawTimestampCounter(); }
 };
 
 struct RiscvTimer : AlarmTracker {
@@ -23,7 +29,29 @@ constinit frg::manual_box<RiscvTimer> riscvTimer;
 } // namespace
 
 static initgraph::Task initTimer{
-    &globalInitEngine, "riscv.init-timer", initgraph::Entails{getTaskingAvailableStage()}, [] {
+    &globalInitEngine,
+    "riscv.init-timer",
+    initgraph::Requires{getDeviceTreeParsedStage()},
+    initgraph::Entails{getTaskingAvailableStage()},
+    [] {
+	    // Get the timebase-frequency property in /cpus.
+	    auto *dtCpus = getDeviceTreeNodeByPath("/cpus");
+	    if (!dtCpus)
+		    panicLogger() << "Device tree node /cpus is not available" << frg::endlog;
+	    auto maybeFreqProp = dtCpus->dtNode().findProperty("timebase-frequency");
+	    if (!maybeFreqProp)
+		    panicLogger() << "Device tree property timebase-frequency is missing from /cpus"
+		                  << frg::endlog;
+	    if (maybeFreqProp->size() != 4)
+		    panicLogger() << "Expected exactly one u32 in timebase-frequency" << frg::endlog;
+	    auto freqSeconds = maybeFreqProp->asU32();
+	    infoLogger() << "thor: Timer frequency is " << freqSeconds << " Hz" << frg::endlog;
+
+	    // Frequency is given in Hz. Hence, we need to divide by 10^9 to convert to nHz.
+	    uint64_t divisor = 1'000'000'000;
+	    freq = computeFreqFraction(freqSeconds, divisor);
+	    inverseFreq = computeFreqFraction(divisor, freqSeconds);
+
 	    riscvClockSource.initialize();
 	    riscvTimer.initialize();
 
