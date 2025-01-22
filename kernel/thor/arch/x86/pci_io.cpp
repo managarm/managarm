98811050010e03dbc51ba0383365feea33fa913a
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2025-01-22 19:30:45 +0100

    thor: Add locks around PCI legacy config access

--- kernel/thor/arch/x86/pci_io.cpp
+++ kernel/thor/arch/x86/pci_io.cpp
@@ -3,14 +3,19 @@
 
 namespace thor::pci {
 
+constinit IrqSpinlock legacyPciMutex;
+
 uint32_t readLegacyPciConfigWord(uint32_t bus, uint32_t slot, uint32_t function, uint32_t offset) {
 	assert(bus < 256 && slot < 32 && function < 8 && offset < 256);
 	assert(!(offset & 3));
 	uint32_t result;
 	auto address = (bus << 16) | (slot << 11) | (function << 8)
 			| (offset & ~uint32_t(3)) | 0x80000000;
-	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
-	asm volatile ( "inl %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC + (offset & 3))) );
+	{
+		auto lock = frg::guard(&legacyPciMutex);
+		asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
+		asm volatile ( "inl %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC + (offset & 3))) );
+	}
 	return result;
 }
 
@@ -20,8 +25,11 @@ uint16_t readLegacyPciConfigHalf(uint32_t bus, uint32_t slot, uint32_t function,
 	uint16_t result;
 	auto address = (bus << 16) | (slot << 11) | (function << 8)
 			| (offset & ~uint32_t(3)) | 0x80000000;
-	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
-	asm volatile ( "inw %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC + (offset & 3))) );
+	{
+		auto lock = frg::guard(&legacyPciMutex);
+		asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
+		asm volatile ( "inw %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC + (offset & 3))) );
+	}
 	return result;
 }
 
@@ -30,8 +38,11 @@ uint8_t readLegacyPciConfigByte(uint32_t bus, uint32_t slot, uint32_t function, 
 	uint8_t result;
 	auto address = (bus << 16) | (slot << 11) | (function << 8)
 			| (offset & ~uint32_t(3)) | 0x80000000;
-	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
-	asm volatile ( "inb %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC + (offset & 3))) );
+	{
+		auto lock = frg::guard(&legacyPciMutex);
+		asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
+		asm volatile ( "inb %1, %0" : "=a" (result) : "d" (uint16_t(0xCFC + (offset & 3))) );
+	}
 	return result;
 }
 
@@ -40,8 +51,11 @@ void writeLegacyPciConfigWord(uint32_t bus, uint32_t slot, uint32_t function, ui
 	assert(!(offset & 3));
 	auto address = (bus << 16) | (slot << 11) | (function << 8)
 			| (offset & ~uint32_t(3)) | 0x80000000;
-	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
-	asm volatile ( "outl %0, %1" : : "a" (value), "d" (uint16_t(0xCFC + (offset & 3))) );
+	{
+		auto lock = frg::guard(&legacyPciMutex);
+		asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
+		asm volatile ( "outl %0, %1" : : "a" (value), "d" (uint16_t(0xCFC + (offset & 3))) );
+	}
 }
 
 void writeLegacyPciConfigHalf(uint32_t bus, uint32_t slot, uint32_t function, uint32_t offset, uint16_t value) {
@@ -49,16 +63,22 @@ void writeLegacyPciConfigHalf(uint32_t bus, uint32_t slot, uint32_t function, ui
 	assert(!(offset & 1));
 	auto address = (bus << 16) | (slot << 11) | (function << 8)
 			| (offset & ~uint32_t(3)) | 0x80000000;
-	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
-	asm volatile ( "outw %0, %1" : : "a" (value), "d" (uint16_t(0xCFC + (offset & 3))) );
+	{
+		auto lock = frg::guard(&legacyPciMutex);
+		asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
+		asm volatile ( "outw %0, %1" : : "a" (value), "d" (uint16_t(0xCFC + (offset & 3))) );
+	}
 }
 
 void writeLegacyPciConfigByte(uint32_t bus, uint32_t slot, uint32_t function, uint32_t offset, uint8_t value) {
 	assert(bus < 256 && slot < 32 && function < 8 && offset < 256);
 	auto address = (bus << 16) | (slot << 11) | (function << 8)
 			| (offset & ~uint32_t(3)) | 0x80000000;
-	asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
-	asm volatile ( "outb %0, %1" : : "a" (value), "d" (uint16_t(0xCFC + (offset & 3))) );
+	{
+		auto lock = frg::guard(&legacyPciMutex);
+		asm volatile ( "outl %0, %1" : : "a" (address), "d" (uint16_t(0xCF8)) );
+		asm volatile ( "outb %0, %1" : : "a" (value), "d" (uint16_t(0xCFC + (offset & 3))) );
+	}
 }
 
-}
+} // namespace thor::pci
