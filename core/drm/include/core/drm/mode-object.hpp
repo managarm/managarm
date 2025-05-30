85291d3ddc2fe22a82d599fe294af4b15bd274bd
no92 <leo@managarm.org>
2025-05-30 11:59:16 +0200

    core/drm: correctly handle format modifiers

--- core/drm/include/core/drm/mode-object.hpp
+++ core/drm/include/core/drm/mode-object.hpp
@@ -250,6 +250,7 @@ public:
 	virtual void notifyDirty() = 0;
 	virtual uint32_t getWidth() = 0;
 	virtual uint32_t getHeight() = 0;
+	virtual uint32_t getModifier() = 0;
 };
 
 struct Plane : ModeObject {
