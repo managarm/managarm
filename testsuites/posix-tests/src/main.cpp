e23527e5b6dcc06a27bd08280695a73fbb7d21d2
no92 <leo@managarm.org>
2025-07-28 18:30:06 +0200

    testsuites/posix-tests: allow specifying by globs which tests to run

--- testsuites/posix-tests/src/main.cpp
+++ testsuites/posix-tests/src/main.cpp
@@ -1,5 +1,9 @@
+#include <fnmatch.h>
 #include <iostream>
+#include <string>
 #include <vector>
+
+#include <CLI/CLI.hpp>
 
 #include "testsuite.hpp"
 
@@ -12,9 +16,29 @@ void abstract_test_case::register_case(abstract_test_case *tcp) {
 	test_case_ptrs().push_back(tcp);
 }
 
-int main() {
-	for(abstract_test_case *tcp : test_case_ptrs()) {
-		std::cout << "posix-tests: Running " << tcp->name() << std::endl;
-		tcp->run();
+int main(int argc, char **argv) {
+	CLI::App app{"POSIX testsuite for managarm"};
+
+	std::vector<std::string> globs;
+	app.add_option("globs", globs, "tests to run");
+
+	CLI11_PARSE(app, argc, argv);
+
+	if (globs.empty()) {
+		for(abstract_test_case *tcp : test_case_ptrs()) {
+			std::cout << "posix-tests: Running " << tcp->name() << std::endl;
+			tcp->run();
+		}
+	} else {
+		for(abstract_test_case *tcp : test_case_ptrs()) {
+			for (const auto &glob : globs) {
+				if (fnmatch(glob.c_str(), tcp->name(), 0) == 0) {
+					std::cout << "posix-tests: Running " << tcp->name() << std::endl;
+					tcp->run();
+				}
+			}
+		}
 	}
+
+	return EXIT_SUCCESS;
 }
