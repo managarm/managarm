8dde40ba8927acd52bbbaa339cfbed1a28b44d78
Kacper Słomiński <kacper.slominski72@gmail.com>
2025-08-10 01:37:53 +0200

    eir, thor: Use boot UART config note on ARM

--- kernel/eir/arch/arm/platform/virt/virt.cpp
+++ kernel/eir/arch/arm/platform/virt/virt.cpp
@@ -2,6 +2,8 @@
 #include <eir-internal/arch/pl011.hpp>
 #include <eir-internal/generic.hpp>
 #include <eir-internal/main.hpp>
+#include <eir/interface.hpp>
+#include <eir-internal/memory-layout.hpp>
 #include <frg/manual_box.hpp>
 
 namespace eir {
@@ -9,17 +11,6 @@ namespace eir {
 frg::manual_box<PL011> debugUart;
 
 void debugPrintChar(char c) { debugUart->send(c); }
-
-static initgraph::Task prepareSerialForThor{
-    &globalInitEngine,
-    "virt.prepare-serial-for-thor",
-    initgraph::Requires{getEirDoneStage()},
-    []() {
-	    mapSingle4kPage(0xFFFF'0000'0000'0000, 0x9000000, PageFlags::write, CachingMode::mmio);
-	    mapKasanShadow(0xFFFF'0000'0000'0000, 0x1000);
-	    unpoisonKasanShadow(0xFFFF'0000'0000'0000, 0x1000);
-    }
-};
 
 void initPlatform() {
 	debugUart.initialize(0x9000000, 24000000);
@@ -35,5 +26,34 @@ extern "C" [[noreturn]] void eirVirtMain() {
 	GenericInfo info{.cmdline = nullptr, .fb{}, .debugFlags = eirDebugSerial, .hasFb = false};
 	eirGenericMain(info);
 }
+
+// UART setup for Thor
+
+static initgraph::Task reserveBootUartMmio{
+    &globalInitEngine,
+    "virt.reserve-boot-uart-mmio",
+    initgraph::Entails{getMemoryRegionsKnownStage()},
+    [] {
+	    reserveEarlyMmio(1);
+    }
+};
+
+static initgraph::Task setupBootUartMmio{
+    &globalInitEngine,
+    "virt.setup-boot-uart-mmio",
+    initgraph::Requires{getAllocationAvailableStage()},
+    initgraph::Entails{getKernelLoadableStage()},
+    [] {
+	    auto addr = allocateEarlyMmio(1);
+
+	    mapSingle4kPage(addr, 0x9000000, PageFlags::write, CachingMode::mmio);
+	    mapKasanShadow(addr, 0x1000);
+	    unpoisonKasanShadow(addr, 0x1000);
+
+	    extern BootUartConfig bootUartConfig;
+	    bootUartConfig.address = addr;
+	    bootUartConfig.type = BootUartType::pl011;
+    }
+};
 
 } // namespace eir
