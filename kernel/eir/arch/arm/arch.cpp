eb08c22dd953f6fd77b4855a13adc0af827a3905
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2025-09-18 17:26:43 +0200

    eir: Reprogram MMU before entering kernel

--- kernel/eir/arch/arm/arch.cpp
+++ kernel/eir/arch/arm/arch.cpp
@@ -4,7 +4,138 @@
 #include <eir-internal/generic.hpp>
 #include <eir-internal/memory-layout.hpp>
 
+extern "C" [[noreturn]] void eirEnterKernel(uint64_t entry, uint64_t stack);
+
 namespace eir {
+
+namespace {
+
+void enterKernelPaging() {
+	uint64_t aa64mmfr0;
+	asm volatile("mrs %0, id_aa64mmfr0_el1" : "=r"(aa64mmfr0));
+
+	// Setup system registers for paging (MAIR and TCR).
+	if (aa64mmfr0 & (uint64_t(0xF) << 28))
+		eir::panicLogger() << "PANIC! This CPU doesn't support 4K memory translation granules"
+		                   << frg::endlog;
+
+	auto pa = frg::min(uint64_t(5), aa64mmfr0 & 0xF);
+
+	uint64_t mair = UINT64_C(0b11111111) |         // Normal, Write-back RW-Allocate non-transient
+	                (UINT64_C(0b00001100) << 8) |  // Device, GRE
+	                (UINT64_C(0b00000000) << 16) | // Device, nGnRnE
+	                (UINT64_C(0b00000100) << 24) | // Device, nGnRE
+	                (UINT64_C(0b01000100) << 32);  // Normal Non-cacheable
+
+	uint64_t tcr = (16 << 0) |            // T0SZ=16
+	               (16 << 16) |           // T1SZ=16
+	               (1 << 8) |             // TTBR0 Inner WB RW-Allocate
+	               (1 << 10) |            // TTBR0 Outer WB RW-Allocate
+	               (1 << 24) |            // TTBR1 Inner WB RW-Allocate
+	               (1 << 26) |            // TTBR1 Outer WB RW-Allocate
+	               (2 << 12) |            // TTBR0 Inner shareable
+	               (2 << 28) |            // TTBR1 Inner shareable
+	               (uint64_t(pa) << 32) | // 48-bit intermediate address
+	               (uint64_t(2) << 30);   // TTBR1 4K granule
+
+	if (physOffset) {
+		// Running from non-identity mapping with paging enabled.
+		// We cannot reconfigure paging. Switch to the correct page tables.
+		infoLogger() << "eir: Switching to page tables and entering kernel" << frg::endlog;
+
+		// TODO: We should not overwrite MAIR and TCR with potentially conflicting values here.
+		//       Instead, ensure that the current values are sane and error out if they are not.
+		asm volatile("msr mair_el1, %0" : : "r"(mair));
+		asm volatile("msr tcr_el1, %0" : : "r"(mair));
+
+		asm volatile(
+		    // clang-format off
+			// Reload page table registers.
+			"\r" "msr ttbr0_el1, %0" "\n"
+			"\r" "msr ttbr1_el1, %1" "\n"
+			"\r" "isb" "\n"
+			// Invalidate TLB to clear old mappings.
+			"\r" "tlbi vmalle1" "\n"
+			"\r" "dsb ish" "\n"
+			"\r" "isb" "\n"
+		    // clang-format on
+		    :
+		    : "r"(eirTTBR[0] + 1), "r"(eirTTBR[1] + 1)
+		    : "x0", "memory"
+		);
+	} else {
+		// Running from identity mapping. Paging may or may not be enabled.
+		// Reconfigure paging and ensure that it is enabled.
+		infoLogger() << "eir: Reprogramming MMU and entering kernel" << frg::endlog;
+
+		uint64_t sctlr;
+		asm volatile("mrs %0, sctlr_el1" : "=r"(sctlr));
+
+		uint64_t ctr;
+		asm("mrs %0, ctr_el0" : "=r"(ctr));
+		auto dcLineSize = ((ctr >> 16) & 0b1111) << 4;
+
+		// Note since disabling the MMU also disables the dcache, we must not access
+		// any data while the MMU is off in the code below. More specifically:
+		// * Cache lines that contain data may be dirty (and thus must not be accessed).
+		// * Cache lines that contain Eir's code are known to be flushed to PoU,
+		//   but not necessarily to PoC. Hence, we explicitly flush the region that runs
+		//   with MMU disabled in the code below.
+		asm volatile(
+		    // clang-format off
+		    // x0 = Address of label 2.
+		         "adrp x0, 2f" "\n"
+		    "\r" "add x0, x0, :lo12:2f" "\n"
+		    // Align x0 down to dcLineSize.
+		    "\r" "sub x1, %5, #1" "\n"
+		    "\r" "bic x0, x0, x1" "\n"
+		    // x1 = Address of label 3.
+		    "\r" "adrp x1, 3f" "\n"
+		    "\r" "add x1, x1, :lo12:3f" "\n"
+		    // Clear the dcache in range [x0, x1) to PoC.
+		    "1:" "\n"
+		    "\r" "dc cvac, x0" "\n"
+		    "\r" "add x0, x0, %5" "\n"
+		    "\r" "cmp x0, x1" "\n"
+		    "\r" "b.lo 1b" "\n"
+		    // MMU regprogramming logic starts here.
+		    // First, ensure that the cache flush is finished.
+		    "2:" "\n"
+		    "\r" "dsb ish" "\n"
+		    "\r" "isb" "\n"
+		    // Disable paging.
+		    "\r" "bic x0, %2, #1" "\n"
+		    "\r" "msr sctlr_el1, x0" "\n"
+		    "\r" "isb" "\n"
+		    // Load MMU-related system registers.
+		    "\r" "msr mair_el1, %3" "\n"
+		    "\r" "msr tcr_el1, %4" "\n"
+		    "\r" "msr ttbr0_el1, %0" "\n"
+		    "\r" "msr ttbr1_el1, %1" "\n"
+		    "\r" "isb" "\n"
+		    // Invalidate TLB to clear old mappings.
+		    "\r" "tlbi vmalle1" "\n"
+		    "\r" "dsb ish" "\n"
+		    "\r" "isb" "\n"
+		    // Enable paging.
+		    "\r" "orr x0, %2, #1" "\n"
+		    "\r" "msr sctlr_el1, x0" "\n"
+		    "\r" "isb" "\n"
+		    "3:"
+		    // clang-format on
+		    :
+		    : "r"(eirTTBR[0] + 1), // %0
+		      "r"(eirTTBR[1] + 1), // %1
+		      "r"(sctlr),          // %2
+		      "r"(mair),           // %3
+		      "r"(tcr),            // %4
+		      "r"(dcLineSize)      // %5
+		    : "x0", "x1", "memory"
+		);
+	}
+}
+
+} // anonymous namespace
 
 uintptr_t eirTTBR[2];
 
@@ -144,39 +275,7 @@ address_t getSingle4kPage(address_t address) {
 
 int getKernelVirtualBits() { return 49; }
 
-void initProcessorEarly() {
-	eir::infoLogger() << "Starting Eir" << frg::endlog;
-
-	uint64_t aa64mmfr0;
-	asm volatile("mrs %0, id_aa64mmfr0_el1" : "=r"(aa64mmfr0));
-
-	if (aa64mmfr0 & (uint64_t(0xF) << 28))
-		eir::panicLogger() << "PANIC! This CPU doesn't support 4K memory translation granules"
-		                   << frg::endlog;
-
-	auto pa = frg::min(uint64_t(5), aa64mmfr0 & 0xF);
-
-	uint64_t mair = UINT64_C(0b11111111) |         // Normal, Write-back RW-Allocate non-transient
-	                (UINT64_C(0b00001100) << 8) |  // Device, GRE
-	                (UINT64_C(0b00000000) << 16) | // Device, nGnRnE
-	                (UINT64_C(0b00000100) << 24) | // Device, nGnRE
-	                (UINT64_C(0b01000100) << 32);  // Normal Non-cacheable
-
-	asm volatile("msr mair_el1, %0" ::"r"(mair));
-
-	uint64_t tcr = (16 << 0) |            // T0SZ=16
-	               (16 << 16) |           // T1SZ=16
-	               (1 << 8) |             // TTBR0 Inner WB RW-Allocate
-	               (1 << 10) |            // TTBR0 Outer WB RW-Allocate
-	               (1 << 24) |            // TTBR1 Inner WB RW-Allocate
-	               (1 << 26) |            // TTBR1 Outer WB RW-Allocate
-	               (2 << 12) |            // TTBR0 Inner shareable
-	               (2 << 28) |            // TTBR1 Inner shareable
-	               (uint64_t(pa) << 32) | // 48-bit intermediate address
-	               (uint64_t(2) << 30);   // TTBR1 4K granule
-
-	asm volatile("msr tcr_el1, %0" ::"r"(tcr));
-}
+void initProcessorEarly() { eir::infoLogger() << "Starting Eir" << frg::endlog; }
 
 void initProcessorPaging() {
 	setupPaging();
@@ -203,7 +302,8 @@ void initProcessorPaging() {
 bool patchArchSpecificManagarmElfNote(unsigned int, frg::span<char>) { return false; }
 
 [[noreturn]] void enterKernel() {
-	eirEnterKernel(eirTTBR[0] + 1, eirTTBR[1] + 1, kernelEntry, getKernelStackPtr());
+	enterKernelPaging();
+	eirEnterKernel(kernelEntry, getKernelStackPtr());
 }
 
 } // namespace eir
