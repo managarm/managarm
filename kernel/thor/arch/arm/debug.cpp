bff49fb6c4a6fdd43885a3149a08553d89314fa5
iretq <me@iretq.dev>
2025-09-26 23:02:16 +0200

    thor: Use the common UART code

--- kernel/thor/arch/arm/debug.cpp
+++ kernel/thor/arch/arm/debug.cpp
@@ -1,64 +1,67 @@
-#include <thor-internal/arch/debug.hpp>
-#include <thor-internal/elf-notes.hpp>
-#include <eir/interface.hpp>
 #include <arch/mem_space.hpp>
 #include <arch/register.hpp>
+#include <eir/interface.hpp>
+#include <thor-internal/arch/debug.hpp>
+#include <thor-internal/elf-notes.hpp>
+#include <uart/uart.hpp>
 
 namespace thor {
 
-constinit UartLogHandler uartLogHandler;
+constinit common::uart::AnyUart bootUart;
+constinit UartLogHandler uartLogHandler{&bootUart};
 
 extern bool debugToSerial;
-
-void setupDebugging() {
-	if (debugToSerial)
-		enableLogHandler(&uartLogHandler);
-}
-
-namespace {
-	namespace reg {
-		static constexpr arch::scalar_register<uint32_t> data{0x00};
-		static constexpr arch::bit_register<uint32_t> status{0x18};
-	}
-
-	namespace status {
-		static constexpr arch::field<uint32_t, bool> tx_full{5, 1};
-	};
-} // namespace anonymous
 
 extern ManagarmElfNote<BootUartConfig> bootUartConfig;
 THOR_DEFINE_ELF_NOTE(bootUartConfig){elf_note_type::bootUartConfig, {}};
 
+void setupDebugging() {
+	if (debugToSerial) {
+		if (bootUartConfig->type == BootUartType::pl011) {
+			bootUart.emplace<common::uart::PL011>(bootUartConfig->window, 0);
+		} else if (bootUartConfig->type == BootUartType::samsung) {
+			bootUart.emplace<common::uart::Samsung>(bootUartConfig->window);
+		}
+
+		if (!std::holds_alternative<std::monostate>(bootUart)) {
+			enableLogHandler(&uartLogHandler);
+		}
+	}
+}
+
 void UartLogHandler::emit(frg::string_view record) {
 	auto [md, msg] = destructureLogRecord(record);
-	for (size_t i = 0; i < msg.size(); ++i)
-		printChar(msg[i]);
-	printChar('\n');
+	print(msg);
 }
 
 void UartLogHandler::emitUrgent(frg::string_view record) {
 	auto [md, msg] = destructureLogRecord(record);
-	const char *prefix = "URGENT: ";
-	while(*prefix)
-		printChar(*(prefix++));
-	for (size_t i = 0; i < msg.size(); ++i)
-		printChar(msg[i]);
-	printChar('\n');
+	print("URGENT: ", false);
+	print(msg);
 }
 
-void UartLogHandler::printChar(char c) {
-	if (bootUartConfig->type != BootUartType::pl011)
-		return;
-
-	arch::mem_space space{bootUartConfig->window};
-
-	// We assume here that Eir has mapped the UART as device memory, and
-	// configured the UART to some sensible settings (115200 8N1).
-
-	while (space.load(reg::status) & status::tx_full)
-		;
-
-	space.store(reg::data, c);
+void UartLogHandler::print(frg::string_view line, bool printNL) {
+	std::visit(
+	    frg::overloaded{
+	        [](std::monostate) { __builtin_trap(); },
+	        [&](auto &inner) {
+		        for (size_t i = 0; i < line.size(); ++i) {
+			        auto c = line[i];
+			        if (c == '\n') {
+				        inner.write('\r');
+				        inner.write('\n');
+			        } else {
+				        inner.write(c);
+			        }
+		        }
+		        if (printNL) {
+			        inner.write('\r');
+			        inner.write('\n');
+		        }
+	        }
+	    },
+	    *uart_
+	);
 }
 
 } // namespace thor
