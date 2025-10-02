529c05d66f14bbd0cef4fa8cefe9d4d3a42edc6d
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2025-10-02 23:53:32 +0200

    eir: Support loading initrd from UKI

---
+++ kernel/eir/boot/uefi/pe.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace eir {
+
+struct PeFileHeader {
+	uint16_t Machine;
+	uint16_t NumberOfSections;
+	uint32_t TimeDateStamp;
+	uint32_t PointerToSymbolTable;
+	uint32_t NumberOfSymbols;
+	uint16_t SizeOfOptionalHeader;
+	uint16_t Characteristics;
+};
+
+struct PeSectionHeader {
+	char Name[8];
+	uint32_t VirtualSize;
+	uint32_t VirtualAddress;
+	uint32_t SizeOfRawData;
+	uint32_t PointerToRawData;
+	uint32_t PointerToRelocations;
+	uint32_t PointerToLinenumbers;
+	uint16_t NumberOfRelocations;
+	uint16_t NumberOfLinenumbers;
+	uint32_t Characteristics;
+};
+
+} // namespace eir
