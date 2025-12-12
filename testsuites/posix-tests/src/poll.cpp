437ad99dc77346b04694bb3b4e352f89d92bcdb2
no92 <leo@managarm.org>
2025-12-12 01:42:30 +0100

    testsuites: test poll()ing with duplicated fds in `struct pollfd`

--- testsuites/posix-tests/src/poll.cpp
+++ testsuites/posix-tests/src/poll.cpp
@@ -3,6 +3,7 @@
 #include <poll.h>
 #include <signal.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -50,4 +51,58 @@ DEFINE_TEST(poll_signal, ([] {
 	ret = ppoll(&pfd, 1, NULL, &empty);
 	assert(ret == 1);
 	assert(pfd.revents == POLLNVAL);
+}))
+
+DEFINE_TEST(poll_same_fd_pollfd, ([] {
+	int sv[2];
+
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, sv) == -1) {
+		perror("socketpair");
+		exit(1);
+	}
+
+	struct pollfd fds[2];
+	fds[0].fd = sv[0];
+	fds[0].events = POLLIN;
+	fds[0].revents = 0;
+
+	fds[1].fd = sv[0];
+	fds[1].events = POLLOUT;
+	fds[1].revents = 0;
+
+	int ret = poll(fds, 2, 1000); // 1 second timeout
+	assert(ret == 1);
+	assert(fds[0].revents == 0);
+	assert(fds[1].revents == POLLOUT);
+
+	fds[0].events = POLLIN | POLLOUT;
+
+	ret = poll(fds, 2, 1000); // 1 second timeout
+	assert(ret == 2);
+	assert(fds[0].revents == POLLOUT);
+	assert(fds[1].revents == POLLOUT);
+
+	char buffer = 'X';
+	if (write(sv[1], &buffer, 1) != 1) {
+		perror("write");
+		exit(1);
+	}
+
+	fds[0].fd = sv[0];
+	fds[0].events = POLLIN;
+	fds[0].revents = 0;
+
+	fds[1].fd = sv[0];
+	fds[1].events = POLLOUT;
+	fds[1].revents = 0;
+
+	ret = poll(fds, 2, 1000); // 1 second timeout
+	assert(ret == 2);
+	assert(fds[0].revents == POLLIN);
+	assert(fds[1].revents == POLLOUT);
+
+	close(sv[0]);
+	close(sv[1]);
+
+	return 0;
 }))
