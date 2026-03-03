f9669b887e32d874768c72589b3901646bb81c90
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2026-03-03 23:31:25 +0100

    thor: Move RcuEngine declaration to header

--- kernel/thor/generic/thor-internal/rcu.hpp
+++ kernel/thor/generic/thor-internal/rcu.hpp
@@ -2,10 +2,31 @@
 
 #include <optional>
 
+#include <async/recurring-event.hpp>
+#include <async/wait-group.hpp>
 #include <frg/list.hpp>
+#include <thor-internal/coroutine.hpp>
 #include <thor-internal/cpu-data.hpp>
 
 namespace thor {
+
+// RcuEngine implements an RCU mechanism where disabling scheduling acts as an RCU read-side lock.
+struct RcuEngine {
+	coroutine<void> barrier();
+
+private:
+	// Sequence number of the RCU state transition.
+	static constexpr uint64_t stateSeq = (UINT64_C(1) << 63) - 1;
+	// This bit is set when there is an ongoing state transition.
+	static constexpr uint64_t stateBusy = UINT64_C(1) << 63;
+
+	std::atomic<uint64_t> state_{0};
+	// This event is raised whenever the busy bit transitions to clear.
+	async::recurring_event seqEvent_;
+
+	// Used to wait until the transition is done on all CPUs.
+	async::wait_group transitionWg_{0};
+};
 
 struct RcuCallable {
 	friend struct RcuDispatcher;
