e290d91c8de0ede13e9087f785ffd2695ae163fb
no92 <leo@managarm.org>
2026-03-10 23:18:33 +0100

    core/bpf: fail validation for unsupported instructions

--- core/lib/bpf/bpf.cpp
+++ core/lib/bpf/bpf.cpp
@@ -5,6 +5,8 @@ bool Bpf::validate() {
 	for(size_t pc = 0; pc < prog_.size(); pc++) {
 		auto inst = prog_[pc];
 
+		// exhaustive switch to cover all Op values to make omissions a compile-time error
+		// as we use -Werror=switch
 		switch(Op(inst.code)) {
 			case Op::JMP_JEQ_K:
 			// case Op::JMP_JEQ_X:
@@ -16,10 +18,24 @@ bool Bpf::validate() {
 			// case Op::JMP_JSET_X:
 				if(pc + inst.jt + 1 >= prog_.size() || pc + inst.jf + 1 >= prog_.size())
 					return false;
-				break;
-			default:
-				break;
+				continue;
+			case Op::ALU_ADD_X:
+			case Op::ALU_AND_K:
+			case Op::ALU_MUL_K:
+			case Op::LDX_W_IMM:
+			case Op::LD_B_IND:
+			case Op::LD_H_ABS:
+			case Op::LD_H_IND:
+			case Op::LD_W_ABS:
+			case Op::LD_W_IND:
+			case Op::MISC_TAX:
+			case Op::RET_A:
+			case Op::RET_K:
+				continue;
 		}
+
+		// unrecognized instruction
+		return false;
 	}
 
 	Op last = Op(prog_.back().code);
@@ -52,6 +68,8 @@ uint32_t Bpf::run(arch::dma_buffer_view buffer) {
 			return arch::convert_endian<arch::endian::big>(*reinterpret_cast<T *>(buffer.subview(offset, sizeof(T)).data()));
 		};
 
+		// exhaustive switch to cover all Op values to make omissions a compile-time error
+		// as we use -Werror=switch
 		switch(Op(inst.code)) {
 			case Op::ALU_ADD_X: {
 				bpf_log_op("A (0x%x) += X (0x%x) = 0x%x", A, X, A * X);
@@ -126,16 +144,10 @@ uint32_t Bpf::run(arch::dma_buffer_view buffer) {
 				bpf_log_op("RET A (0x%02x)", A);
 				return A;
 			}
-			default:
-				// TODO: for now, an unknown BPF instruction is a hard failure as our coverage of
-				// the instruction set is quite incomplete. In the future, once that doesn't hold
-				// any more, we should do proper error handling here instead (e.g. return EINVAL).
-				printf("core: unhandled BPF instruction 0x%02x\n", inst.code);
-				bpf_log_op("{ 0x%02x, %.2u, %.2u, 0x%08x }\n", inst.code, inst.jt, inst.jf, inst.k);
-				assert(!"unhandled BPF instruction");
 		}
 
 	}
 
+	// prevented by validating that the last instruction is a return
 	assert(!"invalid BPF filter with no return!");
 }
