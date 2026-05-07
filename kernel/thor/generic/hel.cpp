9eeb2c83b362f170a40107af719ad7b23c4e34a2
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2026-05-07 23:05:11 +0200

    thor: Snapshot HelSgItem structs in kernel

--- kernel/thor/generic/hel.cpp
+++ kernel/thor/generic/hel.cpp
@@ -2791,6 +2791,7 @@ HelError doSubmitExchangeMsgs(HelHandle laneHandle, smarter::shared_ptr<IpcQueue
 				break;
 			case kHelActionSendFromBufferSg: {
 				size_t length = 0;
+				frg::small_vector<HelSgItem, 8, KernelAlloc> sgItems{*kernelAlloc};
 				auto sglist = reinterpret_cast<HelSgItem *>(recipe->buffer);
 				for(size_t j = 0; j < recipe->length; j++) {
 					HelSgItem item;
@@ -2798,14 +2799,13 @@ HelError doSubmitExchangeMsgs(HelHandle laneHandle, smarter::shared_ptr<IpcQueue
 						return kHelErrFault;
 					if (!(frg::safe_int{length} + frg::safe_int{item.length}).into(length))
 						return kHelErrIllegalArgs;
+					sgItems.push_back(item);
 				}
 
 				frg::unique_memory<KernelAlloc> buffer(*kernelAlloc, length);
 				size_t offset = 0;
 				for(size_t j = 0; j < recipe->length; j++) {
-					HelSgItem item;
-					if(!readUserObject(sglist + j, item))
-						return kHelErrFault;
+					const auto &item = sgItems[j];
 					if(!readUserMemory(reinterpret_cast<char *>(buffer.data()) + offset,
 							reinterpret_cast<char *>(item.buffer), item.length))
 						return kHelErrFault;
