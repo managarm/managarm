f4c3fb0b21da648df0e641eb64b6750d6eeb0e31
no92 <leo@managarm.org>
2026-05-31 19:08:07 +0200

    thor: add QueueIndex utility

--- kernel/thor/generic/thor-internal/util.hpp
+++ kernel/thor/generic/thor-internal/util.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <assert.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <bit>
+#include <span>
+#include <type_traits>
 
 namespace thor {
 
@@ -42,5 +46,111 @@ inline FreqFraction computeFreqFraction(uint64_t num, uint64_t denom) {
 	auto f = (num << s) / denom;
 	return FreqFraction{f, s};
 }
+
+template <size_t Mod = std::dynamic_extent>
+struct QueueIndex {
+private:
+	struct StaticStorage {
+		constexpr StaticStorage() noexcept = default;
+		constexpr size_t get() const noexcept { return Mod; }
+	};
+
+	struct DynamicStorage {
+		constexpr explicit DynamicStorage(size_t mod) noexcept : mod_(mod) {}
+		constexpr size_t get() const noexcept { return mod_; }
+
+	private:
+		size_t mod_;
+	};
+
+	using StorageType =
+	    std::conditional_t<Mod == std::dynamic_extent, DynamicStorage, StaticStorage>;
+
+public:
+	constexpr explicit QueueIndex(size_t value) noexcept
+	    requires(Mod != std::dynamic_extent)
+	: index_(value % Mod),
+	  modStorage_() {
+		[[assume(Mod > 0)]];
+		[[assume(index_ < Mod)]];
+	}
+
+	constexpr QueueIndex(size_t value, size_t mod) noexcept
+	    requires(Mod == std::dynamic_extent)
+	: index_(value % mod),
+	  modStorage_(mod) {
+		[[assume(mod > 0)]];
+		[[assume(index_ < mod)]];
+	}
+
+	constexpr operator size_t() const noexcept { return index_; }
+
+	constexpr size_t operator()() const noexcept { return index_; }
+
+	constexpr QueueIndex operator+(int v) const noexcept {
+		size_t current_mod = modStorage_.get();
+		[[assume(current_mod > 0)]];
+
+		QueueIndex tmp = *this;
+
+		if constexpr (Mod != std::dynamic_extent && std::has_single_bit(Mod)) {
+			tmp.index_ = (tmp.index_ + static_cast<size_t>(v)) & (Mod - 1uz);
+		} else {
+			if (v >= 0) {
+				tmp.index_ = (tmp.index_ + (static_cast<size_t>(v) % current_mod)) % current_mod;
+			} else {
+				size_t abs_v = static_cast<size_t>(-v) % current_mod;
+				tmp.index_ = (current_mod + tmp.index_ - abs_v) % current_mod;
+			}
+		}
+		return tmp;
+	}
+
+	constexpr QueueIndex &operator++() noexcept {
+		size_t current_mod = modStorage_.get();
+		[[assume(current_mod > 0)]];
+
+		if constexpr (Mod != std::dynamic_extent && std::has_single_bit(Mod)) {
+			index_ = (index_ + 1uz) & (Mod - 1uz);
+		} else {
+			index_ = (index_ + 1uz) % current_mod;
+		}
+
+		return *this;
+	}
+
+	constexpr QueueIndex operator++(int) noexcept {
+		auto temp = *this;
+		++*this;
+		return temp;
+	}
+
+	constexpr QueueIndex &operator--() noexcept {
+		size_t current_mod = modStorage_.get();
+		[[assume(current_mod > 0)]];
+
+		if constexpr (Mod != std::dynamic_extent && std::has_single_bit(Mod)) {
+			index_ = (index_ - 1uz) & (Mod - 1uz);
+		} else {
+			index_ = (current_mod + index_ - 1uz) % current_mod;
+		}
+
+		return *this;
+	}
+
+	constexpr QueueIndex operator--(int) noexcept {
+		auto temp = *this;
+		--(*this);
+		return temp;
+	}
+
+	constexpr bool operator==(const QueueIndex &other) const noexcept {
+		return modStorage_.get() == other.modStorage_.get() && index_ == other.index_;
+	}
+
+private:
+	size_t index_;
+	[[no_unique_address]] StorageType modStorage_;
+};
 
 } // namespace thor
