1cbaeee3dfbc05f8c45e9cda724e8a83815a9a92
Qwinci <qwinci222@gmail.com>
2026-06-02 00:28:51 +0300

    posix: Fix sound device hierarchy and add card device

--- posix/subsystem/src/subsystem/sound.cpp
+++ posix/subsystem/src/subsystem/sound.cpp
@@ -21,10 +21,23 @@ id_allocator<uint32_t> minorAllocator{0};
 
 }
 
+struct CardDevice final : drvcore::ClassDevice {
+	CardDevice(int index)
+	: drvcore::ClassDevice{sysfsSubsystem, nullptr, std::format("card{}", index), nullptr} { }
+
+	void composeUevent(drvcore::UeventProperties &ue) override {
+		ue.set("SUBSYSTEM", "sound");
+	}
+
+	std::optional<std::string> getClassPath() override {
+		return "sound";
+	}
+};
+
 struct ControlDevice final : UnixDevice, drvcore::ClassDevice {
-	ControlDevice(int index, helix::UniqueLane lane)
+	ControlDevice(int index, helix::UniqueLane lane, std::shared_ptr<CardDevice> parent)
 	: UnixDevice{VfsType::charDevice},
-			drvcore::ClassDevice{sysfsSubsystem, nullptr, std::format("controlC{}", index), this},
+			drvcore::ClassDevice{sysfsSubsystem, std::move(parent), std::format("controlC{}", index), this},
 			index_{index}, lane_{std::move(lane)} { }
 
 	std::string nodePath() override {
@@ -57,9 +70,9 @@ private:
 };
 
 struct PcmDevice final : UnixDevice, drvcore::ClassDevice {
-	PcmDevice(int cardIndex, int index, bool playback, helix::UniqueLane lane)
+	PcmDevice(int cardIndex, int index, bool playback, helix::UniqueLane lane, std::shared_ptr<CardDevice> parent)
 	: UnixDevice{VfsType::charDevice},
-			drvcore::ClassDevice{sysfsSubsystem, nullptr,
+			drvcore::ClassDevice{sysfsSubsystem, std::move(parent),
 						std::format("pcmC{}D{}{}", cardIndex, index, playback ? 'p' : 'c'), this},
 			cardIndex_{cardIndex}, index_{index}, playback_{playback}, lane_{std::move(lane)} { }
 
@@ -88,6 +101,41 @@ private:
 	helix::UniqueLane lane_;
 };
 
+namespace {
+
+struct CardDevices {
+	std::shared_ptr<CardDevice> card;
+	std::shared_ptr<ControlDevice> control;
+	uint64_t remainingChildDevices;
+};
+
+std::unordered_map<mbus_ng::EntityId, CardDevices> cardMbusMap;
+
+}
+
+async::result<void> installPcmDevice(mbus_ng::Entity entity, const std::string &soundDeviceType,
+		ControlDevice *controlDevice, std::shared_ptr<CardDevice> cardDevice) {
+	auto lane = (co_await entity.getRemoteLane()).unwrap();
+
+	int cardIndex = controlDevice->index();
+	auto deviceIndex = controlDevice->deviceIdAllocator.allocate();
+
+	std::shared_ptr<PcmDevice> pcmDevice;
+	if (soundDeviceType == "playback") {
+		pcmDevice = std::make_shared<PcmDevice>(cardIndex, deviceIndex, true, std::move(lane), cardDevice);
+	} else if (soundDeviceType == "capture") {
+		pcmDevice = std::make_shared<PcmDevice>(cardIndex, deviceIndex, false, std::move(lane), cardDevice);
+	} else {
+		logPanic("posix: unsupported sound device type '{}' (mbus ID {})", soundDeviceType, entity.id());
+	}
+
+	pcmDevice->assignId({116, minorAllocator.allocate()});
+
+	charRegistry.install(pcmDevice);
+	drvcore::registerMbusDevice(entity.id(), pcmDevice);
+	drvcore::installDevice(pcmDevice);
+}
+
 async::detached run() {
 	sysfsSubsystem = new drvcore::ClassSubsystem{"sound"};
 
@@ -111,40 +159,38 @@ async::detached run() {
 				int cardIndex = cardAllocator.allocate();
 				std::println(std::cout, "posix: Installing sound card snd/card{}", cardIndex);
 
+				auto cardDevice = std::make_shared<CardDevice>(cardIndex);
+				drvcore::installDevice(cardDevice);
+
 				auto lane = (co_await entity.getRemoteLane()).unwrap();
-				auto controlDevice = std::make_shared<ControlDevice>(cardIndex, std::move(lane));
+				auto controlDevice = std::make_shared<ControlDevice>(cardIndex, std::move(lane), cardDevice);
 
 				controlDevice->assignId({116, minorAllocator.allocate()});
 
-				charRegistry.install(controlDevice);
-				drvcore::registerMbusDevice(event.id, controlDevice);
-				drvcore::installDevice(controlDevice);
-			} else if (type == "sound-device") {
-				auto &parentId = std::get<mbus_ng::StringItem>(event.properties.at("drvcore.mbus-parent")).value;
-				auto parent = drvcore::getMbusDevice(mbus_ng::EntityId{std::stoi(parentId)});
+				auto &remainingChildDevicesStr =
+						std::get<mbus_ng::StringItem>(event.properties.at("sound.num-devices")).value;
 
-				auto control = std::static_pointer_cast<ControlDevice>(parent);
-				auto lane = (co_await entity.getRemoteLane()).unwrap();
+				CardDevices cardDevices{
+					.card{cardDevice},
+					.control{controlDevice},
+					.remainingChildDevices = std::stoull(remainingChildDevicesStr),
+				};
+				cardMbusMap.insert({entity.id(), std::move(cardDevices)});
+			} else if (type == "sound-device") {
+				auto &parentIdStr = std::get<mbus_ng::StringItem>(event.properties.at("drvcore.mbus-parent")).value;
+				mbus_ng::EntityId parentId = std::stoi(parentIdStr);
+
+				auto &cardDevice = cardMbusMap.at(parentId);
 
 				auto &soundDeviceType = std::get<mbus_ng::StringItem>(event.properties.at("sound.type")).value;
+				co_await installPcmDevice(std::move(entity), soundDeviceType, cardDevice.control.get(), cardDevice.card);
+				cardDevice.remainingChildDevices--;
 
-				int cardIndex = control->index();
-				auto deviceIndex = control->deviceIdAllocator.allocate();
-
-				std::shared_ptr<PcmDevice> pcmDevice;
-				if (soundDeviceType == "playback") {
-					pcmDevice = std::make_shared<PcmDevice>(cardIndex, deviceIndex, true, std::move(lane));
-				} else if (soundDeviceType == "capture") {
-					pcmDevice = std::make_shared<PcmDevice>(cardIndex, deviceIndex, false, std::move(lane));
-				} else {
-					logPanic("posix: unsupported sound device type '{}' (mbus ID {})", soundDeviceType, entity.id());
+				if (cardDevice.remainingChildDevices == 0) {
+					charRegistry.install(cardDevice.control);
+					drvcore::registerMbusDevice(parentId, cardDevice.control);
+					drvcore::installDevice(cardDevice.control);
 				}
-
-				pcmDevice->assignId({116, minorAllocator.allocate()});
-
-				charRegistry.install(pcmDevice);
-				drvcore::registerMbusDevice(event.id, pcmDevice);
-				drvcore::installDevice(pcmDevice);
 			} else {
 				std::println(std::cout, "posix: unsupported sound device class '{}' (mbus ID {})", type, entity.id());
 			}
