65dc9da57e933b9d9c7c33f6d40a83320cb550cb
Marvin Friedrich <contact@marvinf.com>
2026-06-28 16:45:18 +0200

    thor: Fix x86 AP startup hangs on some devices

--- kernel/thor/arch/x86/cpu.cpp
+++ kernel/thor/arch/x86/cpu.cpp
@@ -842,14 +842,18 @@ void bootSecondary(unsigned int apic_id, size_t cpuIndex) {
 	// The BIOS is not involved in this process at all.
 	infoLogger() << "thor: Booting AP " << apic_id << "." << frg::endlog;
 	raiseInitAssertIpi(apic_id);
-	KernelFiber::asyncBlockCurrent(generalTimerEngine()->sleepFor(10'000'000)); // Wait for 10ms.
+	pollSleepNano(10'000'000); // Wait for 10ms.
+
+	// De-assert the INIT IPI.
+	raiseInitDeassertIpi(apic_id);
+	pollSleepNano(200'000); // Wait for 200us.
 
 	// SIPI causes the processor to resume execution and resets CS:IP.
 	// Intel suggets to send two SIPIs (probably for redundancy reasons).
 	raiseStartupIpi(apic_id, pma);
-	KernelFiber::asyncBlockCurrent(generalTimerEngine()->sleepFor(200'000)); // Wait for 200us.
+	pollSleepNano(200'000); // Wait for 200us.
 	raiseStartupIpi(apic_id, pma);
-	KernelFiber::asyncBlockCurrent(generalTimerEngine()->sleepFor(200'000)); // Wait for 200us.
+	pollSleepNano(200'000); // Wait for 200us.
 
 	// Wait until the AP wakes up.
 	while(__atomic_load_n(&statusBlock->targetStage, __ATOMIC_ACQUIRE) < 1) {
