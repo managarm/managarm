b2c1675678cea245de2fd11d44e44878bba8d374
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2026-08-04 22:46:11 +0200

    thor: Fix rights laundering in helAlterMemoryIndirection()

--- kernel/thor/generic/hel.cpp
+++ kernel/thor/generic/hel.cpp
@@ -662,7 +662,9 @@ HelError helCreateIndirectMemory(size_t numSlots, HelHandle *handle) {
 	*handle = this_universe->attachDescriptor(
 		AnyDescriptor::make<DescriptorType::memoryView>(
 			std::move(*memoryOutcome),
-			kHelRightRead | kHelRightWrite | kHelRightExecute | kHelRightAssign | kHelRightProvision | kHelRightFence | kHelRightManage
+			// No kHelRightExecute: we only require kHelRightRead | kHelRightWrite in helAlterMemoryIndirection(),
+			// so allowing kHelRightExecute would allow users to launder rights.
+			kHelRightRead | kHelRightWrite | kHelRightAssign | kHelRightProvision | kHelRightFence | kHelRightManage
 		)
 	);
 
@@ -684,12 +686,12 @@ HelError helAlterMemoryIndirection(HelHandle indirectHandle, size_t slot,
 	auto memoryOutcome = thisUniverse->inspectDescriptor(memoryHandle,
 			[&](AnyDescriptor &desc) -> std::expected<void, Error> {
 		if(desc.is<DescriptorType::memoryView>()) {
-			auto viewOutcome = desc.resolveObject<DescriptorType::memoryView>(kHelRightAssign);
+			auto viewOutcome = desc.resolveObject<DescriptorType::memoryView>(kHelRightRead | kHelRightWrite | kHelRightAssign);
 			if(!viewOutcome)
 				return std::unexpected{viewOutcome.error()};
 			memoryView = std::move(*viewOutcome);
 		} else if(desc.is<DescriptorType::memorySlice>()) {
-			auto sliceOutcome = desc.resolveObject<DescriptorType::memorySlice>(kHelRightAssign);
+			auto sliceOutcome = desc.resolveObject<DescriptorType::memorySlice>(kHelRightRead | kHelRightWrite | kHelRightAssign);
 			if(!sliceOutcome)
 				return std::unexpected{sliceOutcome.error()};
 			memoryView = (*sliceOutcome)->getView();
