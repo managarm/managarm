d57b81bd63e8701a653e93a270cb636f420bd2e0
Alexander Richards <electrodeyt@gmail.com>
2026-09-09 22:03:27 +0200

    thor: collect dirty mappings in writeback fences

--- hel/include/hel.h
+++ hel/include/hel.h
@@ -972,6 +972,7 @@ struct HelSqForkMemory {
 };
 
 //! SQ data for kHelSubmitWritebackFence.
+//! Collects dirty pages from mappings of the range and waits for writeback to complete.
 struct HelSqWritebackFence {
 	//! Handle to the memory object.
 	HelHandle handle;
